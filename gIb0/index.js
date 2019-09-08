7a6671d9ebe2cd36e85e582c44af7dcf1ed6611e
cx20 <y-shimano@hondarer-soft.com>
2019-09-08 18:08:07 +0900

    [簡易版] WebGL で球体を描いてみるテスト

---
+++ gIb0/index.js
@@ -0,0 +1,107 @@
+// forked from cx20's "[簡易版] WebGL でリサージュ図形を描いてみるテスト" http://jsdo.it/cx20/9Dru
+// forked from cx20's "[簡易版] WebGL で円を角度をつけて回転させてみるテスト" http://jsdo.it/cx20/5vCG
+// forked from cx20's "[簡易版] WebGL で円を描いてみるテスト" http://jsdo.it/cx20/dz02
+// forked from cx20's "[簡易版] WebGL でサインカーブを描いてみるテスト" http://jsdo.it/cx20/fPok
+// forked from cx20's "[簡易版] WebGL で点をプロットしてみるテスト" http://jsdo.it/cx20/puXG
+// forked from cx20's "[簡易版] WebGL で四角形を描いてみるテスト" http://jsdo.it/cx20/vwnxi
+// forked from cx20's "[簡易版 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+var c, gl;
+var aLoc = [];
+var uLoc = [];
+
+function initWebGL() {
+    c = document.getElementById("c");
+    gl = c.getContext("experimental-webgl");
+}
+
+function initShaders() {
+    var p = gl.createProgram();
+    var vs = gl.createShader(gl.VERTEX_SHADER);
+    var fs = gl.createShader(gl.FRAGMENT_SHADER);
+    var v = document.getElementById("vs").textContent;
+    var f = document.getElementById("fs").textContent;
+    gl.shaderSource(vs, v);
+    gl.shaderSource(fs, f);
+    gl.compileShader(vs);
+    gl.compileShader(fs);
+    gl.attachShader(p, vs);
+    gl.attachShader(p, fs);
+    gl.linkProgram(p);
+    gl.useProgram(p);
+    aLoc[0] = gl.getAttribLocation(p, "position");
+    uLoc[0] = gl.getUniformLocation(p, 'matAxisX');
+    uLoc[1] = gl.getUniformLocation(p, 'matAxisY');
+    gl.enableVertexAttribArray(aLoc[0]);
+}
+
+function draw() {
+    // 正弦波×余弦波の座標データを用意
+    //             1.0 y 
+    //              ^  -1.0 
+    //              | / z
+    //              |/       x
+    // -1.0 -----------------> +1.0
+    //            / |
+    //      +1.0 /  |
+    //           -1.0
+    // 
+    //             [3]
+    //         [4]     [2]
+    //      [5]            [1]
+    //      *                *
+    //     [6]               [0]
+    //      *                *
+    //      [7]            [11]
+    //         [8]     [10]
+    //             [9]
+    //
+    var data = [];
+    var MAX = 144;
+    var A = 7.0;
+    var B = 8.0;
+    for ( var i = 0; i <= MAX; i++ ) {
+        var x = 0.5 * Math.sin(2 * Math.PI * i / MAX * A) * Math.cos(2 * Math.PI * i / MAX * B);
+        var y = 0.5 * Math.sin(2 * Math.PI * i / MAX * A) * Math.sin(2 * Math.PI * i / MAX * B);
+        var z = 0.5 * Math.cos(2 * Math.PI * i / MAX * A);
+        data = data.concat([x, y, z]);
+    }
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
+    gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);
+
+    var rad = 0;
+    (function(){
+        rad += Math.PI * 1.0 / 180.0;
+
+        var c = Math.cos(rad);
+        var s = Math.sin(rad);
+
+        // 変換行列を用意
+        // x軸で回転
+        var matAxisX = [
+            1.0, 0.0, 0.0, 0.0,
+            0.0,   c,  -s, 0.0,
+            0.0,   s,   c, 0.0,
+            0.0, 0.0, 0.0, 1.0
+        ];
+        // y軸で回転
+        var matAxisY = [
+              c, 0.0,   s, 0.0,
+            0.0, 1.0, 0.0, 0.0,
+             -s, 0.0,   c, 0.0,
+            0.0, 0.0, 0.0, 1.0
+        ];
+        gl.uniformMatrix4fv(uLoc[0], false, new Float32Array(matAxisX));
+        gl.uniformMatrix4fv(uLoc[1], false, new Float32Array(matAxisY));
+
+        gl.drawArrays(gl.LINE_STRIP, 0, data.length / 3);
+        gl.drawArrays(gl.POINTS, 0, data.length / 3);
+        gl.flush();
+        requestAnimationFrame(arguments.callee);
+    })();
+}
+
+initWebGL();
+initShaders();
+draw();
---
+++ gIb0/readme.txt
@@ -0,0 +1,16 @@
+[簡易版] WebGL で球体を描いてみるテスト
+※ 初期化処理やエラー処理を省いた簡易版のコードになります。ご注意ください。
+
+＜対応した点＞
+・球座標の公式を用いて座標をプロットするよう対応。
+
+　■ 極座標系 - Wikipedia
+　http://ja.wikipedia.org/wiki/%E6%A5%B5%E5%BA%A7%E6%A8%99%E7%B3%BB
+　
+　　x = r sinθcosφ
+　　y = r sinθsinφ
+　　z = r cosθ
+
+＜参考＞
+■ [GLSL] Three.js + ParticleSystem で球体にドットを配置してみるテスト（その３改）
+http://jsdo.it/cx20/eUz7
