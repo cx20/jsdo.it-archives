48d36d015e8418ac3ef61b0837f41fff5ce0f7b5
cx20 <y-shimano@hondarer-soft.com>
2019-09-08 19:10:07 +0900

    [簡易版] WebGL で四角形を分割してみるテスト（その２）

---
+++ vtc5/index.js
@@ -0,0 +1,83 @@
+// forked from cx20's "[簡易版] WebGL で四角形を分割してみるテスト" http://jsdo.it/cx20/fT6P
+// forked from cx20's "[簡易版] WebGL で四角形を描いてみるテスト" http://jsdo.it/cx20/vwnxi
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+var c, gl;
+var aLoc = [];
+
+function initWebGL() {
+    c = document.getElementById("c");
+    gl = c.getContext("experimental-webgl");
+}
+
+function initShaders() {
+    var p = gl.createProgram();
+    var v = document.getElementById("vs").textContent;
+    var f = document.getElementById("fs").textContent;
+    var vs = gl.createShader(gl.VERTEX_SHADER);
+    var fs = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(vs, v);
+    gl.shaderSource(fs, f);
+    gl.compileShader(vs);
+    gl.compileShader(fs);
+    gl.attachShader(p, vs);
+    gl.attachShader(p, fs);
+    gl.linkProgram(p);
+    gl.useProgram(p);
+    aLoc[0] = gl.getAttribLocation(p, "position");
+    gl.enableVertexAttribArray(aLoc[0]);
+}
+
+function draw() {
+    // グリッドのデータを用意
+    //             1.0 y 
+    //              ^  -1.0 
+    //              | / z
+    //              |/       x
+    // -1.0 -----------------> +1.0
+    //            / |
+    //      +1.0 /  |
+    //           -1.0
+    // 
+    //    [0]------[1]------[2]
+    //     |     /  |     /  |
+    //     |   /    |   /    |
+    //     | /      | /      |
+    //    [3]------[4]------[5]
+    //     |     /  |     /  |
+    //     |   /    |   /    |
+    //     | /      | /      |
+    //    [6]------[7]------[8]
+    //
+    var data = [ 
+        -0.5,  0.5,  0.0, // v0
+         0.0,  0.5,  0.0, // v1
+         0.5,  0.5,  0.0, // v2
+        -0.5,  0.0,  0.0, // v3
+         0.0,  0.0,  0.0, // v4
+         0.5,  0.0,  0.0, // v5
+        -0.5, -0.5,  0.0, // v6
+         0.0, -0.5,  0.0, // v7
+         0.5, -0.5,  0.0, // v8
+    ];
+    gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
+    gl.vertexAttribPointer(aLoc[0], 3, gl.FLOAT, false, 0, 0);
+
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, gl.createBuffer());
+    var indices = [
+        3, 0, 1, 3, 4, 1,  // v3-v0-v1 v3-v4-v1
+        4, 1, 2, 4, 5, 2,  // v4-v1-v2 v4-v5-v2
+        6, 3, 4, 6, 7, 4,  // v6-v3-v4 v6-v7-v4
+        7, 4, 5, 7, 8, 5   // v7-v4-v5 v7-v8-v5
+    ];
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+
+    gl.drawElements(gl.LINE_STRIP, indices.length, gl.UNSIGNED_SHORT, 0);
+    gl.drawElements(gl.POINTS, indices.length, gl.UNSIGNED_SHORT, 0);
+
+    gl.flush();
+}
+
+initWebGL();
+initShaders();
+draw();
