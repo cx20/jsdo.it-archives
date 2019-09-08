9b77989109c409c31b9dff77d5e391273f211844
cx20 <y-shimano@hondarer-soft.com>
2019-09-08 19:00:20 +0900

    [簡易版] WebGL でテクスチャを試してみるテスト

---
+++ y4vz/index.js
@@ -0,0 +1,120 @@
+// forked from cx20's "[簡易版] WebGL で四角形に色を付けてみるテスト" http://jsdo.it/cx20/veHj
+// forked from cx20's "[簡易版] WebGL で四角形を描いてみるテスト" http://jsdo.it/cx20/vwnxi
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+// forked from cx20's "[簡易版] WebGL で四角形に色を付けてみるテスト" http://jsdo.it/cx20/veHj
+// forked from cx20's "[簡易版] WebGL で四角形を描いてみるテスト" http://jsdo.it/cx20/vwnxi
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+var c, gl;
+var aLoc = [];
+var uLoc = [];
+
+function initWebGL() {
+    c = document.getElementById("c");
+    gl = c.getContext("experimental-webgl");
+}
+
+function initShaders() {
+    var p = gl.createProgram();
+    var v = document.getElementById("vs").textContent;
+    var f = document.getElementById("fs").textContent;
+    var vs = gl.createShader(gl.VERTEX_SHADER);
+    var fs = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(vs, v);
+    gl.shaderSource(fs, f);
+    gl.compileShader(vs);
+    gl.compileShader(fs);
+    gl.attachShader(p, vs);
+    gl.attachShader(p, fs);
+    gl.linkProgram(p);
+    gl.useProgram(p);
+    aLoc[0] = gl.getAttribLocation(p, "position");
+    gl.enableVertexAttribArray(aLoc[0]);
+    aLoc[1] = gl.getAttribLocation(p, "color");
+    gl.enableVertexAttribArray(aLoc[1]);
+    aLoc[2] = gl.getAttribLocation(p, "textureCoord");
+    gl.enableVertexAttribArray(aLoc[2]);
+
+    uLoc[0]  = gl.getUniformLocation(p, 'mvpMatrix');
+    uLoc[1]  = gl.getUniformLocation(p, 'texture');
+}
+
+function draw() {
+    // 正方形の座標データを用意
+    //             1.0 y 
+    //              ^  -1.0 
+    //              | / z
+    //              |/       x
+    // -1.0 -----------------> +1.0
+    //            / |
+    //      +1.0 /  |
+    //           -1.0
+    // 
+    //        [0]------[1]
+    //         |        |
+    //         |        |
+    //         |        |
+    //        [2]------[3]
+    //
+    var data = [ 
+        -0.5, 0.5, 0.0, // v0
+         0.5, 0.5, 0.0, // v1 
+        -0.5,-0.5, 0.0, // v2
+         0.5,-0.5, 0.0  // v3
+    ];
+    
+    var color = [ 
+         1.0, 1.0, 1.0, 1.0, // v0
+         1.0, 1.0, 1.0, 1.0, // v1
+         1.0, 1.0, 1.0, 1.0, // v2
+         1.0, 1.0, 1.0, 1.0  // v3
+    ];
+
+    var textureCoord = [
+        0.0, 0.0,
+        1.0, 0.0,
+        0.0, 1.0,
+        1.0, 1.0
+    ];
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
+    gl.vertexAttribPointer(aLoc[0], 3, gl.FLOAT, false, 0, 0);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(color), gl.STATIC_DRAW);
+    gl.vertexAttribPointer(aLoc[1], 4, gl.FLOAT, false, 0, 0);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoord), gl.STATIC_DRAW);
+    gl.vertexAttribPointer(aLoc[2], 2, gl.FLOAT, false, 0, 0);
+    
+    // 変換行列を用意
+    var mvpMatrix = [
+        1.0, 0.0, 0.0, 0.5,
+        0.0, 1.0, 0.0, 0.0,
+        0.0, 0.0, 1.0, 0.0,
+        0.0, 0.0, 0.0, 1.0
+    ];
+    gl.uniformMatrix4fv(uLoc[0], false, new Float32Array(mvpMatrix));
+    gl.uniform1i(uLoc[1], 0);
+
+    // 画像データの読み込み
+    var img = new Image();
+    img.onload = function(){
+        // テクスチャを用意
+        gl.bindTexture(gl.TEXTURE_2D, gl.createTexture());
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
+        gl.generateMipmap(gl.TEXTURE_2D);
+
+        gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+        //gl.drawArrays(gl.LINE_STRIP, 0, 4);
+        gl.drawArrays(gl.POINTS, 0, 4);
+        gl.flush();
+    };
+    //img.src = "frog.jpg";
+    img.src = "../assets/A/k/w/j/AkwjW.jpg";
+}
+
+initWebGL();
+initShaders();
+draw();
