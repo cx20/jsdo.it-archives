f2918616bed948388c3c7eb692f7c967800e1019
cx20 <y-shimano@hondarer-soft.com>
2019-09-12 05:36:27 +0900

    [WebGL] Grimoire.js でサッカーボールを表示させてみるテスト

---
+++ qgAL/index.js
@@ -0,0 +1,26 @@
+// forked from cx20's "[WebGL] Grimoire.js を試してみるテスト（その４）" http://jsdo.it/cx20/yNCh
+// forked from cx20's "[WebGL] Grimoire.js を試してみるテスト（その３）" http://jsdo.it/cx20/a1kX
+// forked from cx20's "[WebGL] Grimoire.js を試してみるテスト（その２）" http://jsdo.it/cx20/mjC6
+// forked from cx20's "[WebGL] Grimoire.js を試してみるテスト" http://jsdo.it/cx20/4ZEB
+// forked from cx20's "[WebGL] jThree を試してみるテスト" http://jsdo.it/cx20/Go5s
+
+gr.registerComponent('Rotate', {
+  attributes: {
+    speed: {
+      default: '1',
+      converter: 'Number',
+    },
+  },
+  $mount: function() {
+    this.phi = 0;
+  },
+  $update: function() {
+    this.phi += this.getAttribute('speed');
+    this.node.setAttribute('rotation', this.phi + ',' + this.phi + ',' + 0);
+  },
+});
+
+gr(function() {
+  var $$ = gr('#canvas');
+  $$('mesh').addComponent('Rotate');
+});
---
+++ qgAL/readme.txt
@@ -0,0 +1,11 @@
+[WebGL] Grimoire.js でサッカーボールを表示させてみるテスト
+
+＜対応した点＞
+・立方体→球体に変更
+・サッカーボールのテクスチャに変更
+
+＜参考＞
+■ GrimoireGL/GrimoireJS
+https://github.com/GrimoireGL/GrimoireJS
+■ Grimoire.js
+https://grimoire.gl/
