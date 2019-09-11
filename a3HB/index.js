299952cea8ba713b48d73e5a0ded8e8ad69e8a06
cx20 <y-shimano@hondarer-soft.com>
2019-09-12 00:37:00 +0900

    [WebGL] Grimoire.js + glTF ファイルを試してみるテスト（その８）

---
+++ a3HB/index.js
@@ -0,0 +1,33 @@
+// forked from cx20's "[WebGL] Grimoire.js + glTF ファイルを試してみるテスト（その７）（調整中）" http://jsdo.it/cx20/Mq2h
+// forked from cx20's "[WebGL] Grimoire.js + glTF ファイルを試してみるテスト（その６）（調整中）" http://jsdo.it/cx20/oCiQ
+// forked from cx20's "[WebGL] Grimoire.js + glTF ファイルを試してみるテスト（その５）（調整中）" http://jsdo.it/cx20/2PYW
+// forked from cx20's "[WebGL] Grimoire.js + glTF ファイルを試してみるテスト（その４）（調整中）" http://jsdo.it/cx20/iE9t
+// forked from cx20's "[WebGL] Grimoire.js + glTF ファイルを試してみるテスト（その３）（調整中）" http://jsdo.it/cx20/sV4J
+// forked from cx20's "[WebGL] Grimoire.js + glTF ファイルを試してみるテスト（その２）" http://jsdo.it/cx20/irCY
+// forked from cx20's "[WebGL] Grimoire.js + glTF ファイルを試してみるテスト（調整中）" http://jsdo.it/cx20/GJUm
+// forked from cx20's "[WebGL] Grimoire.js を試してみるテスト（その４）" http://jsdo.it/cx20/yNCh
+// forked from cx20's "[WebGL] Grimoire.js を試してみるテスト（その３）" http://jsdo.it/cx20/a1kX
+// forked from cx20's "[WebGL] Grimoire.js を試してみるテスト（その２）" http://jsdo.it/cx20/mjC6
+// forked from cx20's "[WebGL] Grimoire.js を試してみるテスト" http://jsdo.it/cx20/4ZEB
+// forked from cx20's "[WebGL] jThree を試してみるテスト" http://jsdo.it/cx20/Go5s
+
+gr.registerComponent('Rotate', {
+  attributes: {
+    speed: {
+      default: '1',
+      converter: 'Number',
+    },
+  },
+  $mount: function () {
+    this.phi = 0;
+  },
+  $update: function () {
+    this.phi += this.getAttribute('speed');
+    this.node.setAttribute('rotation', 0 + ',' + this.phi + ',' + 0);
+  },
+});
+
+gr(function () {
+  var $$ = gr('#canvas');
+  $$('model').addComponent('Rotate');
+});
---
+++ a3HB/readme.txt
@@ -0,0 +1,38 @@
+[WebGL] Grimoire.js + glTF ファイルを試してみるテスト（その８）
+
+＜対応した点＞
+・表示モデルをお化けカボチャに変更。
+・obj → glTF 変換したファイルを読み込めるかテスト
+・スケール指定に対応。
+
+＜変更履歴＞
+2017/03/12 grimoirejs-preset-basic＠1.9.7 + grimoirejs-gltf＠1.8.8。テクスチャのリサイズの問題解消。
+2017/02/11 grimoirejs-preset-basic＠1.8.7 + grimoirejs-gltf＠1.8.3。
+2017/01/28 grimoirejs-preset-basic＠1.8.5 + grimoirejs-gltf＠1.8.2。
+2016/12/17 grimoirejs-preset-basic＠1.4.4 + grimoirejs-gltf＠1.3.1。
+2016/12/10 grimoirejs-gltf＠1.2.10
+2016/12/06 grimoirejs-gltf＠1.2.4。モデルにテクスチャが正しく適用されていない？
+
+＜参考＞
+■ GrimoireGL/GrimoireJS
+https://github.com/GrimoireGL/GrimoireJS
+■ GrimoireGL/grimoirejs-gltf
+https://github.com/GrimoireGL/grimoirejs-gltf
+■ Grimoire.js - Examples
+https://cdn.rawgit.com/GrimoireGL/grimoire.gl-example/master/index.html#t90-01
+■ AnalyticalGraphicsInc/obj2gltf
+https://github.com/AnalyticalGraphicsInc/obj2gltf
+■ Q-BLOCK Create 3D Pixel Art Online
+http://kyucon.com/qblock/#/34778
+
+＜他ライブラリとの比較＞
+■ three.js で glTF 形式のデータを表示してみるテスト（その８）（改）
+http://jsdo.it/cx20/gYyo
+■ [WebGL] Babylon.js + glTFFileLoader を試してみるテスト（その８）（改）
+http://jsdo.it/cx20/20ju
+■ Cesium.js で3Dモデル表示してみるテスト（その８）
+http://jsdo.it/cx20/Gtr7
+■ [WebGL] xeogl + glTF ファイルを試してみるテスト（その８）
+http://jsdo.it/cx20/GvRE
+■ [WebGL] GLBoost + glTF Loader を試してみるテスト（その８）（改）
+http://jsdo.it/cx20/ocv5
