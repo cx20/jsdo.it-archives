31d1bee2fcb20f4e5f2e505b249763d31f82b1c6
cx20 <y-shimano@hondarer-soft.com>
2019-09-13 00:28:12 +0900

    Grimoire.js で地球を回してみるテスト

---
+++ uQit/index.js
@@ -0,0 +1,23 @@
+// forked from cx20's "WebGL で地球を回してみるテスト" http://jsdo.it/cx20/cI8t
+
+
+gr.registerComponent('Rotate', {
+  attributes: {
+    speed: {
+      default: '1',
+      converter: 'Number',
+    },
+  },
+  $mount: function () {
+    this.phi = 0;
+  },
+  $update: function () {
+    this.phi += this.getAttribute('speed');
+    this.node.setAttribute('rotation',0 + ',' + this.phi + ',' + 0);
+  },
+});
+
+gr(function () {
+  var $$ = gr('#canvas');
+  $$('mesh').addComponent('Rotate');
+});
---
+++ uQit/readme.txt
@@ -0,0 +1,15 @@
+Grimoire.js で地球を回してみるテスト
+
+＜対応した点＞
+・Grimoire.js で地球を回転させるよう対応。
+
+＜変更履歴＞
+2017/03/25 grimoire-preset-basic.js v1.9.8 テクスチャの向きの問題解消。
+2017/03/24 grimoire-preset-basic.js v1.9.7 テクスチャの向きに誤り。
+
+＜参考＞
+■ Texture/Earth
+https://grimoire.gl/example/example-003.html
+
+■ [WebGL] Grimoire.js でサッカーボールを表示させてみるテスト
+http://jsdo.it/cx20/qgAL
