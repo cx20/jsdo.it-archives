17e86e8f6b22af593e5ccc0244c4728bf37ca61b
cx20 <y-shimano@hondarer-soft.com>
2019-09-12 23:52:41 +0900

    [WebGL] Grimoire.js でマウスクリックした位置に消しゴムを落下させてみるテスト

---
+++ gTNn/index.js
@@ -0,0 +1,208 @@
+// forked from cx20's "[WebGL] Grimoire.js + Oimo.js で消しゴムを落下させてみるテスト（改）" http://jsdo.it/cx20/edAK
+// forked from cx20's "[WebGL] Grimoire.js + Oimo.js で消しゴムを落下させてみるテスト" http://jsdo.it/cx20/eDWo
+// forked from cx20's "[WebGL] Grimoire.js で Oimo.js を試してみるテスト（その２）" http://jsdo.it/cx20/sVc4
+// forked from cx20's "[WebGL] Grimoire.js で Oimo.js を試してみるテスト" http://jsdo.it/cx20/kQ0F
+// forked from cx20's "[WebGL] Grimoire.js で Cannon.js を試してみるテスト" http://jsdo.it/cx20/4xwo
+// forked from cx20's "[WebGL] Grimoire.js を試してみるテスト（その４）（VBO編）" http://jsdo.it/cx20/swSy
+// forked from cx20's "[WebGL] Grimoire.js を試してみるテスト（その３）（VBO編）" http://jsdo.it/cx20/YiRx
+// forked from cx20's "[WebGL] Grimoire.js を試してみるテスト（その２）（VBO編）" http://jsdo.it/cx20/e3YN
+// forked from cx20's "[WebGL] Grimoire.js を試してみるテスト（VBO編）" http://jsdo.it/cx20/iUdQ
+// forked from cx20's "[WebGL] Grimoire.js を試してみるテスト" http://jsdo.it/cx20/4ZEB
+// forked from cx20's "[WebGL] jThree を試してみるテスト" http://jsdo.it/cx20/Go5s
+
+const Quaternion = gr.lib.math.Quaternion;
+gr(function() {
+    const scene = gr("#main")("scene").single();
+/*
+    var timer = setInterval(function() {
+        const n = scene.addChildByName("rigid-eraser", {
+            //material: ["#red", "#green", "#blue"][Math.floor(Math.random() * 3)],
+            position: [Math.random() * 3 - 1.5, Math.random() * 5 + 5, Math.random() * 3 - 1.5]
+        });
+    }, 50);
+    setTimeout(function(){
+        clearInterval(timer);
+    }, 10000 );
+*/
+    const canvas = document.querySelector("canvas");
+    canvas.addEventListener("click", onClick, false );
+    let x = 0;
+    let y = 0;
+    function onClick(e) {
+        x = e.clientX - canvas.offsetLeft;
+        y = e.clientY - canvas.offsetTop;
+        console.log("x, y = " + x + ", " + y  );
+        const n = scene.addChildByName("rigid-eraser", {
+            //material: ["#red", "#green", "#blue"][Math.floor(Math.random() * 3)],
+            position: [(x/465 - 0.5)*10, ((1-y/465) - 0.5)*10, Math.random() * 3 - 1.5]
+        });
+    }
+});
+
+var GeometryFactory = gr.lib.fundamental.Geometry.GeometryFactory;
+var Geometry = gr.lib.fundamental.Geometry.Geometry;
+GeometryFactory.addType("custom", {}, function(gl,attrs){
+  var geometry = new Geometry(gl);
+  var positions = new Float32Array([
+    // Front face
+    -0.5*2, -0.5*2,  0.5*2, // v0
+     0.5*2, -0.5*2,  0.5*2, // v1
+     0.5*2,  0.5*2,  0.5*2, // v2
+    -0.5*2,  0.5*2,  0.5*2, // v3
+    // Back face
+    -0.5*2, -0.5*2, -0.5*2, // v4
+     0.5*2, -0.5*2, -0.5*2, // v5
+     0.5*2,  0.5*2, -0.5*2, // v6
+    -0.5*2,  0.5*2, -0.5*2, // v7
+    // Top face
+     0.5*2,  0.5*2,  0.5*2, // v2
+    -0.5*2,  0.5*2,  0.5*2, // v3
+    -0.5*2,  0.5*2, -0.5*2, // v7
+     0.5*2,  0.5*2, -0.5*2, // v6
+    // Bottom face
+    -0.5*2, -0.5*2,  0.5*2, // v0
+     0.5*2, -0.5*2,  0.5*2, // v1
+     0.5*2, -0.5*2, -0.5*2, // v5
+    -0.5*2, -0.5*2, -0.5*2, // v4
+    // Right face
+     0.5*2, -0.5*2,  0.5*2, // v1
+     0.5*2,  0.5*2,  0.5*2, // v2
+     0.5*2,  0.5*2, -0.5*2, // v6
+     0.5*2, -0.5*2, -0.5*2, // v5
+    // Left face
+    -0.5*2, -0.5*2,  0.5*2, // v0
+    -0.5*2,  0.5*2,  0.5*2, // v3
+    -0.5*2,  0.5*2, -0.5*2, // v7
+    -0.5*2, -0.5*2, -0.5*2  // v4
+  ]);
+  // テクスチャ座標データを用意
+  //  (0, 1)               (1, 1)
+  //   t +----+----+----+----+
+  //     | ① | ② | ③ |    |
+  //     +----+----+----+----+
+  //     | ④ | ⑤ | ⑥ |    |
+  //     +----+----+----+----+ -> s
+  //  (0, 0)               (1, 0)
+  //
+  var texcoords = new Float32Array([
+    // Front face
+    0.5,  0.5, // v0
+    0.75, 0.5, // v1
+    0.75, 1.0, // v2
+    0.5,  1.0, // v3
+
+    // Back face
+    0.25, 0.5, // v4
+    0.5,  0.5, // v5
+    0.5,  1.0, // v6
+    0.25, 1.0, // v7
+
+    // Top face
+    0.75, 0.5, // v2
+    0.5,  0.5, // v3
+    0.5,  0.0, // v7
+    0.75, 0.0, // v6
+
+    // Bottom face
+    0.0,  0.5, // v0
+    0.25, 0.5, // v1
+    0.25, 1.0, // v5
+    0.0,  1.0, // v4
+
+    // Right face
+    0.0,  0.5, // v1
+    0.0,  0.0, // v2
+    0.25, 0.0, // v6
+    0.25, 0.5, // v5
+
+    // Left face
+    0.5,  0.5, // v0
+    0.5,  0.0, // v3
+    0.25, 0.0, // v7
+    0.25, 0.5  // v4
+  ]);
+  geometry.addAttributes(positions, {
+    POSITION:{
+      size: 3
+    }
+  });
+  geometry.addAttributes(texcoords, {
+    TEXCOORD:{
+      size: 2
+    }
+  });
+  var indices = [
+     0,  1,  2,    0,  2 , 3,  // Front face
+     4,  5,  6,    4,  6 , 7,  // Back face
+     8,  9, 10,    8, 10, 11,  // Top face
+    12, 13, 14,   12, 14, 15,  // Bottom face
+    16, 17, 18,   16, 18, 19,  // Right face
+    20, 21, 22,   20, 22, 23   // Left face
+  ];
+  geometry.addIndex("default", indices, WebGLRenderingContext.TRIANGLES);
+  return geometry;
+});
+
+gr.register(() => {
+    gr.registerComponent("OimoScene", {
+        $awake: function() {
+            this.world = new OIMO.World();
+        },
+        $update: function() {
+            this.world.step();
+        }
+    });
+    gr.overrideDeclaration("scene", ["OimoScene"]);
+    gr.registerComponent("Rigid", {
+        attributes: {
+            shape: {
+                default: "box",
+                converter: "String"
+            },
+            move: {
+                converter: "Boolean",
+                default: true
+            }
+        },
+        $mount: function() {
+            this.__bindAttributes();
+            this.transform = this.node.getComponent("Transform");
+            const p = this.transform.position;
+            const r = this.transform.rotation;
+            const s = this.transform.scale;
+            const oimoScene = this.node.getComponentInAncestor("OimoScene");
+            this.body = oimoScene.world.add({
+                type: this.shape,
+                size: [s.X * 2, s.Y * 2, s.Z * 2],
+                pos: [p.X, p.Y, p.Z],
+                rot: [r.X, r.Y, r.Z],
+                move: this.move,
+                density: 1
+            });
+        },
+        $update: function() {
+            const p = this.body.getPosition();
+            this.transform.setAttribute("position", [p.x, p.y, p.z]);
+            const r = this.body.getQuaternion();
+            this.transform.setAttribute("rotation", new Quaternion([r.x, r.y, r.z, r.w]));
+            if ( p.y < -10 ) {
+                x = -5 + Math.random() * 10;
+                y = 10 + Math.random() * 2;
+                z = -5 + Math.random() * 10;
+                this.body.resetPosition(x, y, z);
+            }
+        }
+    });
+    gr.registerNode("rigid-cube", ["Rigid"], {
+        material:"#green",
+        geometry: "cube",
+        scale: 0.5
+    }, "mesh");
+    gr.registerNode("rigid-eraser", ["Rigid"], {
+        material: "new(textureShader)",
+        geometry: "c1",
+        texture: "../assets/7/5/o/P/75oPt.png",
+        //scale: [1.0, 0.2, 0.5]
+        scale: [1.0, 0.2, 0.5]
+    }, "mesh");
+});
