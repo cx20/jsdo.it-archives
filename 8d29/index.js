18ee709cb4496f14d2dbc9a6ddcaf1b078066ec6
cx20 <y-shimano@hondarer-soft.com>
2019-09-12 23:08:15 +0900

    [WebGL] Grimoire.js + Cannon.js でモデルを落下させてみるテスト

---
+++ 8d29/index.js
@@ -0,0 +1,97 @@
+// forked from cx20's "[WebGL] Grimoire.js で Cannon.js を試してみるテスト（その２）" http://jsdo.it/cx20/Kzgm
+// forked from cx20's "[WebGL] Grimoire.js で Cannon.js を試してみるテスト" http://jsdo.it/cx20/4xwo
+// forked from cx20's "[WebGL] Grimoire.js を試してみるテスト（その４）（VBO編）" http://jsdo.it/cx20/swSy
+// forked from cx20's "[WebGL] Grimoire.js を試してみるテスト（その３）（VBO編）" http://jsdo.it/cx20/YiRx
+// forked from cx20's "[WebGL] Grimoire.js を試してみるテスト（その２）（VBO編）" http://jsdo.it/cx20/e3YN
+// forked from cx20's "[WebGL] Grimoire.js を試してみるテスト（VBO編）" http://jsdo.it/cx20/iUdQ
+// forked from cx20's "[WebGL] Grimoire.js を試してみるテスト" http://jsdo.it/cx20/4ZEB
+// forked from cx20's "[WebGL] jThree を試してみるテスト" http://jsdo.it/cx20/Go5s
+
+var timeStep = 1 / 60
+
+const Quaternion = gr.lib.math.Quaternion;
+gr(function () {
+    const scene = gr("#main")("scene").single();
+/*
+    setInterval(function () {
+        const n = scene.addChildByName("rigid-cube", {
+            material: ["#red", "#green", "#blue"][Math.floor(Math.random() * 3)],
+            position: [Math.random() * 3 - 1.5, Math.random() * 5 + 5, Math.random() * 3 - 1.5]
+        });
+    }, 200);
+*/
+    setInterval(function () {
+        const n = scene.addChildByName("rigid-gltf", {
+            position: [Math.random() * 3 - 1.5, Math.random() * 5 + 5, Math.random() * 3 - 1.5]
+        });
+        gr("#main")("object.LOD3sp").setAttribute('position', "-0.2, -0.8, 0");
+  }, 200);
+});
+
+gr.register(() => {
+    gr.registerComponent("CannonScene", {
+        $awake: function () {
+            this.world = new CANNON.World();
+            this.world.gravity.set(0, -9.82, 0);
+            this.world.broadphase = new CANNON.NaiveBroadphase();
+            this.world.solver.iterations = 10;
+            this.world.solver.tolerance = 0.1;
+        },
+        $update: function () {
+            this.world.step(timeStep);
+        }
+    });
+    gr.overrideDeclaration("scene", ["CannonScene"]);
+    gr.registerComponent("Rigid", {
+        attributes: {
+            shape: {
+                default: "box",
+                converter: "String"
+            },
+            move: {
+                converter: "Boolean",
+                default: true
+            }
+        },
+        $mount: function () {
+            this.__bindAttributes();
+            this.transform = this.node.getComponent("Transform");
+            const p = this.transform.localPosition;
+            const r = this.transform.localRotation;
+            const s = this.transform.localScale;
+            var mass = 1.0;
+            var shape;
+            if (this.shape == "box") {
+                shape = new CANNON.Box(new CANNON.Vec3(s.X, s.Y, s.Z));
+            }
+            else if (this.shape == "duck") {
+                shape = new CANNON.Sphere(s.X*0.5);
+            }
+            body = new CANNON.Body({
+                mass: this.move ? 1.0 : 0.0,
+                position: new CANNON.Vec3(p.X, p.Y, p.Z),
+                shape: shape
+            });
+            this.body = body;
+            const cannonScene = this.node.getComponentInAncestor("CannonScene");
+            cannonScene.world.addBody(body);
+        },
+        $update: function () {
+            const p = this.body.position;
+            this.transform.setAttribute("position", [p.x, p.y, p.z]);
+            const r = this.body.quaternion;
+            this.transform.setAttribute("rotation", new Quaternion([r.x, r.y, r.z, r.w]));
+        }
+    });
+    gr.registerNode("rigid-cube", ["Rigid"], {
+        material: "#green",
+        geometry: "cube",
+        scale: 0.5
+    }, "mesh");
+    gr.registerNode("rigid-gltf", ["Rigid"], {
+        src: "../assets/g/s/5/8/gs58O.gltf", // glTF-Embedded/Duck.gltf
+        //geometry: "cube",
+        shape: "duck",
+        scale: 1.0
+    }, "model");
+});
---
+++ 8d29/readme.txt
@@ -0,0 +1,14 @@
+[WebGL] Grimoire.js + Cannon.js でモデルを落下させてみるテスト
+
+＜対応した点＞
+・キューブを glTF モデルに変更
+・物理演算の剛体とモデルの位置のズレを調整。
+　gr("#main")("object.LOD3sp").setAttribute('position', "-0.2, -0.8, 0");
+
+＜参考＞
+■ [WebGL] Grimoire.js で Oimo.js でモデルを落下させてみるテスト
+http://jsdo.it/cx20/IUCm
+
+■ WebGLと3D物理演算ライブラリの組み合わせを試してみる
+http://qiita.com/cx20/items/3ebed669fb9c9e797935
+
