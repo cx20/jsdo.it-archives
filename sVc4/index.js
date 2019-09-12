9216c97a204902e3e97e87a4c54b4aae73720fe5
cx20 <y-shimano@hondarer-soft.com>
2019-09-12 23:14:16 +0900

    [WebGL] Grimoire.js で Oimo.js を試してみるテスト（その２）

---
+++ sVc4/index.js
@@ -0,0 +1,69 @@
+// forked from cx20's "[WebGL] Grimoire.js で Oimo.js を試してみるテスト" http://jsdo.it/cx20/kQ0F
+// forked from cx20's "[WebGL] Grimoire.js で Cannon.js を試してみるテスト" http://jsdo.it/cx20/4xwo
+// forked from cx20's "[WebGL] Grimoire.js を試してみるテスト（その４）（VBO編）" http://jsdo.it/cx20/swSy
+// forked from cx20's "[WebGL] Grimoire.js を試してみるテスト（その３）（VBO編）" http://jsdo.it/cx20/YiRx
+// forked from cx20's "[WebGL] Grimoire.js を試してみるテスト（その２）（VBO編）" http://jsdo.it/cx20/e3YN
+// forked from cx20's "[WebGL] Grimoire.js を試してみるテスト（VBO編）" http://jsdo.it/cx20/iUdQ
+// forked from cx20's "[WebGL] Grimoire.js を試してみるテスト" http://jsdo.it/cx20/4ZEB
+// forked from cx20's "[WebGL] jThree を試してみるテスト" http://jsdo.it/cx20/Go5s
+
+const Quaternion = gr.lib.math.Quaternion;
+gr(function() {
+    const scene = gr("#main")("scene").single();
+    setInterval(function() {
+        const n = scene.addChildByName("rigid-cube", {
+            material: ["#red", "#green", "#blue"][Math.floor(Math.random() * 3)],
+            position: [Math.random() * 3 - 1.5, Math.random() * 5 + 5, Math.random() * 3 - 1.5]
+        });
+    }, 200);
+});
+gr.register(() => {
+    gr.registerComponent("OimoScene", {
+        $awake: function() {
+            this.world = new OIMO.World();
+        },
+        $update: function() {
+            this.world.step();
+        }
+    });
+    gr.overrideDeclaration("scene", ["OimoScene"]);
+    gr.registerComponent("Rigid", {
+        attributes: {
+            shape: {
+                default: "box",
+                converter: "String"
+            },
+            move: {
+                converter: "Boolean",
+                default: true
+            }
+        },
+        $mount: function() {
+            this.__bindAttributes();
+            this.transform = this.node.getComponent("Transform");
+            const p = this.transform.localPosition;
+            const r = this.transform.localRotation;
+            const s = this.transform.localScale;
+            const oimoScene = this.node.getComponentInAncestor("OimoScene");
+            this.body = oimoScene.world.add({
+                type: this.shape,
+                size: [s.X * 2, s.Y * 2, s.Z * 2],
+                pos: [p.X, p.Y, p.Z],
+                rot: [r.X, r.Y, r.Z],
+                move: this.move,
+                density: 1
+            });
+        },
+        $update: function() {
+            const p = this.body.getPosition();
+            this.transform.setAttribute("position", [p.x, p.y, p.z]);
+            const r = this.body.getQuaternion();
+            this.transform.setAttribute("rotation", new Quaternion([r.x, r.y, r.z, r.w]));
+        }
+    });
+    gr.registerNode("rigid-cube", ["Rigid"], {
+        material:"#green",
+        geometry: "cube",
+        scale: 0.5
+    }, "mesh");
+});
---
+++ sVc4/readme.txt
@@ -0,0 +1,11 @@
+[WebGL] Grimoire.js で Oimo.js を試してみるテスト（その２）
+
+＜対応した点＞
+・ボールを箱に変更
+・落下させる箱の色を変更するよう対応
+
+＜参考＞
+■ grimoirejs-handson/Session2/
+https://github.com/kyasbal-1994/grimoirejs-handson/tree/master/Session2
+■ grimoirejs-handson/Session2.pdf
+https://github.com/kyasbal-1994/grimoirejs-handson/blob/master/Session2.pdf
