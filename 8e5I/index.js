c1ae0415cce8ea860ae85b70869f9f122a4b4aee
cx20 <y-shimano@hondarer-soft.com>
2019-09-13 01:14:48 +0900

    [WebGL] Grimoire.js + ポリゴンで３次元関数を表示してみるテスト

---
+++ 8e5I/index.js
@@ -0,0 +1,83 @@
+// forked from cx20's "[WebGL] Grimoire.js で３次元リサージュ図形を描いてみるテスト" http://jsdo.it/cx20/SM1B
+// forked from cx20's "[WebGL] Grimoire.js でリサージュ図形を描いてみるテスト" http://jsdo.it/cx20/8Fx0
+// forked from cx20's "[WebGL] Grimoire.js を試してみるテスト（VBO編）" http://jsdo.it/cx20/iUdQ
+// forked from cx20's "[WebGL] Grimoire.js を試してみるテスト" http://jsdo.it/cx20/4ZEB
+// forked from cx20's "[WebGL] jThree を試してみるテスト" http://jsdo.it/cx20/Go5s
+
+var GeometryFactory = gr.lib.fundamental.Geometry.GeometryFactory;
+var Geometry = gr.lib.fundamental.Geometry.Geometry;
+GeometryFactory.addType("custom", {}, function (gl, attrs) {
+  var geometry = new Geometry(gl);
+  // 3次元リサージュの座標データを用意
+  //             1.0 y 
+  //              ^  -1.0 
+  //              | / z
+  //              |/       x
+  // -1.0 -----------------> +1.0
+  //            / |
+  //      +1.0 /  |
+  //           -1.0
+  // 
+  var WIDTH_SEGMENT = 100;
+  var HEIGHT_SEGMENT = 100;
+  var WIDTH_SIZE = 1.0 * 2 / WIDTH_SEGMENT;
+  var HEIGHT_SIZE = 1.0 * 2 / WIDTH_SEGMENT;
+
+  var positions = new Float32Array((WIDTH_SEGMENT + 1) * (HEIGHT_SEGMENT + 1) * 3);
+  var colors = new Float32Array((WIDTH_SEGMENT + 1) * (HEIGHT_SEGMENT + 1) * 4);
+
+  var theta = 0;
+  var k = 0;
+  for (var j = -10; j < 10; j += 0.2) {
+    for (var i = -10; i < 10; i += 0.2) {
+      var x = i;
+      var y = j;
+      var z = Math.sin(Math.sqrt(x * x + y * y) + theta) / Math.sqrt(x * x + y * y);
+      var x2 = x / 10;
+      var y2 = y / 10;
+      var z2 = z / 2;
+      //var z2 = 0;
+      positions[k * 3 + 0] = x2;
+      positions[k * 3 + 1] = y2;
+      positions[k * 3 + 2] = z2;
+
+      colors[k * 4 + 0] = x2 + 0.5;
+      colors[k * 4 + 1] = y2 + 0.5;
+      colors[k * 4 + 2] = z2 + 0.5;
+      colors[k * 4 + 3] = 1.0;
+
+      k++;
+    }
+  }
+
+  var indices = new Uint16Array(WIDTH_SEGMENT * HEIGHT_SEGMENT * 6);
+  k = 0;
+  for (var row = 0; row < HEIGHT_SEGMENT; row++) {
+    for (var col = 0; col < WIDTH_SEGMENT; col++) {
+      var a = (row + 1) * (WIDTH_SEGMENT + 1) + col;
+      var b = (row + 0) * (WIDTH_SEGMENT + 1) + col;
+      var c = (row + 0) * (WIDTH_SEGMENT + 1) + col + 1;
+      var d = (row + 1) * (WIDTH_SEGMENT + 1) + col + 1;
+      indices[k * 6 + 0] = b;
+      indices[k * 6 + 1] = a;
+      indices[k * 6 + 2] = c;
+      indices[k * 6 + 3] = a;
+      indices[k * 6 + 4] = d;
+      indices[k * 6 + 5] = c;
+      k++;
+    }
+  }
+  geometry.addAttributes(positions, {
+    POSITION: {
+      size: 3
+    }
+  });
+  geometry.addAttributes(colors, {
+    COLOR: {
+      size: 4
+    }
+  });
+  geometry.addIndex("default", indices, WebGLRenderingContext.TRIANGLES);
+  //geometry.addIndex("default", indices, WebGLRenderingContext.LINES);
+  return geometry;
+});
---
+++ 8e5I/readme.txt
@@ -0,0 +1,13 @@
+[WebGL] Grimoire.js + ポリゴンで３次元関数を表示してみるテスト
+
+＜対応した点＞
+・GeometryFactoryでポリゴンを作成し３次元関数を表示するよう対応。
+
+＜参考＞
+■ [WebGL] three.js + ポリゴンで３次元関数を表示してみるテスト（BufferGeometry編）
+http://jsdo.it/cx20/ylYQ
+
+■ 各種 WebGL ライブラリで基本図形を表示してみる - Qiita
+http://qiita.com/cx20/items/0fa19c96aa6470d98807
+■ パラメトリック曲面を描くのに適した WebGL ライブラリについて - Qiita
+http://qiita.com/cx20/items/e983572d5dde2fa9a85b
