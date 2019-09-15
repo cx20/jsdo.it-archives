34a6d1e263d29bd9a89bfbe6acd8705892eb660e
cx20 <y-shimano@hondarer-soft.com>
2019-09-16 00:46:23 +0900

    GLBoost + Oimo.js で雛(ひよこ)祭

---
+++ cx20/QkuW/index.js
@@ -0,0 +1,228 @@
+// forked from cx20's "GLBoost + Oimo.js で坂道にボール転がしてみるテスト（その２）" http://jsdo.it/cx20/45Wj
+// forked from cx20's "GLBoost + Oimo.js で坂道にボール転がしてみるテスト" http://jsdo.it/cx20/iegp
+// forked from cx20's "GLBoost + Oimo.js で箱にボールを入れてみるテスト" http://jsdo.it/cx20/Y91w
+// forked from cx20's "GLBoost + Oimo.js でサッカーボールを落下させてみるテスト" http://jsdo.it/cx20/GIhN
+// forked from cx20's "GLBoost + Oimo.js でドット絵を落下させてみるテスト" http://jsdo.it/cx20/wUhx
+// forked from cx20's "Three.js + Oimo.js でドット絵を落下させるテスト" http://jsdo.it/cx20/voHQ
+// forked from Lo-Th's "oimo basic" http://jsdo.it/Lo-Th/frXo
+
+// glboost var
+var canvas = document.getElementById("world");
+var renderer;
+var camera;
+var scene;
+var groupMeshs = [];
+var wireMeshs = [];
+
+//oimo var
+var world;
+var G = -10, nG = -10;
+var wakeup = false;
+var bodys = [];
+var stats;
+
+init();
+
+function init() {
+    stats = new Stats();
+    stats.setMode( 0 ); // 0: fps, 1: ms, 2: mb
+    stats.domElement.style.position = "fixed";
+    stats.domElement.style.left     = "5px";
+    stats.domElement.style.top      = "5px";
+    document.body.appendChild(stats.domElement);
+
+    scene = new GLBoost.Scene();
+    renderer = new GLBoost.Renderer({
+        canvas: canvas,
+        clearColor: {red: 0.8, green: 0.8, blue: 0.9, alpha: 1}
+    });
+    camera = new GLBoost.Camera({
+        eye: new GLBoost.Vector3(0, 300, 400),
+        center: new GLBoost.Vector3(0.0, 0.0, 0.0),
+        up: new GLBoost.Vector3(0.0, 1.0, 0.0)
+    }, {
+        fovy: 70.0,
+        aspect: 1.0,
+        zNear: 1,
+        zFar: 1000.0
+    });
+    scene.add(camera);
+
+    var directionalLight1 = new GLBoost.DirectionalLight(new GLBoost.Vector3(1.2, 1.2, 1.2), new GLBoost.Vector3(-1, -1, -1));
+    scene.add( directionalLight1 );
+    var directionalLight2 = new GLBoost.DirectionalLight(new GLBoost.Vector3(0.5, 0.5, 0.5), new GLBoost.Vector3(1, 1, 1));
+    scene.add( directionalLight2 );
+
+    // oimo init
+    world = new OIMO.World();
+    populate();
+
+}
+
+function populate() {
+    
+    // The Bit of a collision group
+    var group1 = 1 << 0;  // 00000000 00000000 00000000 00000001
+    var group2 = 1 << 1;  // 00000000 00000000 00000000 00000010
+    var group3 = 1 << 2;  // 00000000 00000000 00000000 00000100
+    var all = 0xffffffff; // 11111111 11111111 11111111 11111111
+
+    //var max = 256;
+    var max = 100;
+
+    // reset old
+    clearMesh();
+    world.clear();
+
+    // Is all the physics setting for rigidbody
+    var config = [
+        1,   // 密度
+        0.4, // 摩擦係数
+        0.6, // 反発係数
+        1,   // 所属する衝突グループのビット
+        all  // 衝突する衝突グループのビット
+    ];
+    
+    var ground2 = new OIMO.Body({size:[400, 40, 400], pos:[0,-20,0], world:world, config:config});
+    addStaticBox([400, 40, 400], [0,-20,0], [0,0,0]);
+    
+    for ( var i = 0; i < 20; i++ ) {
+        var ground2 = new OIMO.Body({size:[40, 40, 400], pos:[i * 20,i * 10,0], world:world, config:config});
+        addStaticBox([40, 40, 400], [i * 20,i * 10,0], [0,0,0]);
+    }
+
+    var w;
+    var h;
+    var d;
+    //var shader = new GLBoost.SimpleShader();
+    var shader = new GLBoost.HalfLambertShader();
+    //var shader = new GLBoost.LambertShader();
+    //var shader = new GLBoost.PhongShader();
+
+    var i = max;
+    
+    var glTFLoader = GLBoost.GLTFLoader.getInstance();
+/*
+    Copyright 2006 Sony Computer Entertainment Inc.
+    
+    Licensed under the SCEA Shared Source License, Version 1.0 (the "License"); 
+    you may not use this file except in compliance with the License. 
+    You may obtain a copy of the License at:
+    
+    http://research.scea.com/scea_shared_source_license.html
+    
+    Unless required by applicable law or agreed to in writing, 
+    software distributed under the License is distributed on an "AS IS" BASIS, 
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+    See the License for the specific language governing permissions and 
+    limitations under the License.
+*/
+    //var promise = glTFLoader.loadGLTF('duck.gltf', 30, shader); // duck.gltf
+    var promise = glTFLoader.loadGLTF('../../assets/g/s/5/8/gs58O.gltf', 30, shader); // duck.gltf
+
+    promise.then(function(mesh) {
+        var mesh0 = mesh.searchElement("LOD3spShape-lib");
+        
+        while (i--){
+            x = 250 + Math.random() * 20;
+            y = 100 + i * 20;
+            z = Math.random() * 20;
+
+            w = 20 + Math.random()*10;
+            h = 10 + Math.random()*10;
+            d = 10 + Math.random()*10;
+            w = 100;
+            bodys[i] = new OIMO.Body({
+                type: 'sphere',
+                size: [w*0.3],
+                pos: [x, y, z],
+                move: true,
+                world: world
+            });
+            var geometry = new GLBoost.Sphere(w*0.3, 12, 12);
+            geometry._primitiveType = GLBoost.LINE_STRIP;
+            wireMeshs[i] = new GLBoost.Mesh(geometry, mesh0.material);
+            wireMeshs[i].opacity = 0.5;
+            var mesh = new GLBoost.Mesh(mesh0.geometry, mesh0.material);
+            mesh.translate = new GLBoost.Vector3(-w*0.06, -w*0.21, 0);
+            groupMeshs[i] = new GLBoost.Group();
+            groupMeshs[i].addChild(mesh);
+            scene.add(groupMeshs[i]);
+            //scene.add(wireMeshs[i]);
+        }
+
+        // loop
+        scene.prepareForRender();
+        loop();
+    });
+}
+
+function addStaticBox(size, position, rotation, spec) {
+    var geo1 = new GLBoost.Cube(new GLBoost.Vector3(size[0], size[1], size[2]), new GLBoost.Vector3(1, 0, 0));
+    var shader = new GLBoost.LambertShader();    
+    var material = new GLBoost.ClassicMaterial();
+    material.shader = shader;
+    var mground1 = new GLBoost.Mesh(geo1, material);
+    mground1.translate = new GLBoost.Vector3(position[0], position[1], position[2]);
+    if ( spec ) {
+        mground1.opacity = 0.5;
+    }
+    mground1.dirty = true;
+    scene.add( mground1 );
+}
+
+function clearMesh(){
+/*
+    var i=meshs.length;
+    while (i--){scene.remove(meshs[ i ]);}
+*/
+}
+
+// MAIN LOOP
+
+function loop() {
+    renderer.clearCanvas();
+    renderer.draw(scene);
+    
+    world.step();
+    
+    var p, r, m, x, y, z;
+    var i = bodys.length;
+    var mesh;
+    var wireMesh;
+    wakeup = false;
+
+    if (G !== nG) {
+        wakeup = true;
+        G = nG;
+    }
+
+    while (i--) {
+        var body = bodys[i].body;
+        mesh = groupMeshs[i];
+        wireMesh = wireMeshs[i];
+        if (wakeup) bodys[i].body.awake();
+        if (!body.sleeping) {
+            var p = body.getPosition();
+            mesh.translate = new GLBoost.Vector3(p.x, p.y, p.z);
+            wireMesh.translate = new GLBoost.Vector3(p.x, p.y, p.z);
+            var q = body.getQuaternion();
+            mesh.quaternion = new GLBoost.Quaternion(q.x, q.y, q.z, q.w);
+            wireMesh.quaternion = new GLBoost.Quaternion(q.x, q.y, q.z, q.w);
+            if ( p.y < -300 ) {
+                x = 250 + Math.random() * 20;
+                y = 100 + i * 20;
+                z = Math.random() * 20;
+                bodys[i].resetPosition(x, y, z);
+            }
+        }
+    }
+
+    var rotateMatrixY = GLBoost.Matrix33.rotateY(1);
+    var rotatedVector = rotateMatrixY.multiplyVector(camera.eye);
+    camera.eye = rotatedVector;
+
+    stats.update();
+
+    requestAnimationFrame(loop);
+}
---
+++ cx20/QkuW/readme.txt
@@ -0,0 +1,8 @@
+GLBoost + Oimo.js で雛(ひよこ)祭
+
+＜対応した点＞
+・ボールをアヒルのひよこに変更。
+
+＜参考＞
+■ forked: Three.js + Oimo.js で雛(ひよこ)祭
+http://jsdo.it/cx20/iaLl
