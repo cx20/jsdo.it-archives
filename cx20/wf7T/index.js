08ca017293bd0f1c7603e646153c4cd84f0b180a
cx20 <y-shimano@hondarer-soft.com>
2019-09-16 20:56:18 +0900

    [WebGL] GLBoost を試してみるテスト（standalone編）（その４）

--- cx20/wf7T/index.js
+++ cx20/wf7T/index.js
@@ -5,11 +5,15 @@
 // forked from cx20's "[WebGL] GLBoost を試してみるテスト" http://jsdo.it/cx20/SWec
 // forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
 
-var canvas = document.getElementById("world");
-var glBoostContext = new GLBoost.GLBoostMiddleContext(canvas);
-var renderer = glBoostContext.createRenderer({ canvas: canvas, clearColor: {red:1, green:1, blue:1, alpha:1}});
+let width = window.innerWidth;
+let height = window.innerHeight;
 
-var scene = glBoostContext.createScene();
+let canvas = document.getElementById("world");
+let glBoostContext = new GLBoost.GLBoostMiddleContext(canvas);
+let renderer = glBoostContext.createRenderer({ canvas: canvas, clearColor: {red:1, green:1, blue:1, alpha:1}});
+renderer.resize(width, height);
+
+let scene = glBoostContext.createScene();
 
 // 立方体の座標データを用意
 //             1.0 y 
@@ -29,81 +33,9 @@ var scene = glBoostContext.createScene();
 //       |/       |/
 //      [0]------[1]
 //
-/*
-// 頂点データを Vector で指定する場合
-var positions = [
-    // Front face
-    new GLBoost.Vector3(-0.5, -0.5,  0.5), // v0
-    new GLBoost.Vector3( 0.5, -0.5,  0.5), // v1
-    new GLBoost.Vector3( 0.5,  0.5,  0.5), // v2
-    new GLBoost.Vector3(-0.5,  0.5,  0.5), // v3
-    // Back face
-    new GLBoost.Vector3(-0.5, -0.5, -0.5), // v4
-    new GLBoost.Vector3( 0.5, -0.5, -0.5), // v5
-    new GLBoost.Vector3( 0.5,  0.5, -0.5), // v6
-    new GLBoost.Vector3(-0.5,  0.5, -0.5), // v7
-    // Top face
-    new GLBoost.Vector3( 0.5,  0.5,  0.5), // v2
-    new GLBoost.Vector3(-0.5,  0.5,  0.5), // v3
-    new GLBoost.Vector3(-0.5,  0.5, -0.5), // v7
-    new GLBoost.Vector3( 0.5,  0.5, -0.5), // v6
-    // Bottom face
-    new GLBoost.Vector3(-0.5, -0.5,  0.5), // v0
-    new GLBoost.Vector3( 0.5, -0.5,  0.5), // v1
-    new GLBoost.Vector3( 0.5, -0.5, -0.5), // v5
-    new GLBoost.Vector3(-0.5, -0.5, -0.5), // v4
-    // Right face
-    new GLBoost.Vector3( 0.5, -0.5,  0.5), // v1
-    new GLBoost.Vector3( 0.5,  0.5,  0.5), // v2
-    new GLBoost.Vector3( 0.5,  0.5, -0.5), // v6
-    new GLBoost.Vector3( 0.5, -0.5, -0.5), // v5
-    // Left face
-    new GLBoost.Vector3(-0.5, -0.5,  0.5), // v0
-    new GLBoost.Vector3(-0.5,  0.5,  0.5), // v3
-    new GLBoost.Vector3(-0.5,  0.5, -0.5), // v7
-    new GLBoost.Vector3(-0.5, -0.5, -0.5)  // v4
-];
 
-var texcoords = [
-    // Front face
-    new GLBoost.Vector2(0.0, 0.0),
-    new GLBoost.Vector2(1.0, 0.0),
-    new GLBoost.Vector2(1.0, 1.0),
-    new GLBoost.Vector2(0.0, 1.0),
-    
-    // Back face
-    new GLBoost.Vector2(1.0, 0.0),
-    new GLBoost.Vector2(1.0, 1.0),
-    new GLBoost.Vector2(0.0, 1.0),
-    new GLBoost.Vector2(0.0, 0.0),
-    
-    // Top face
-    new GLBoost.Vector2(0.0, 1.0),
-    new GLBoost.Vector2(0.0, 0.0),
-    new GLBoost.Vector2(1.0, 0.0),
-    new GLBoost.Vector2(1.0, 1.0),
-    
-    // Bottom face
-    new GLBoost.Vector2(1.0, 1.0),
-    new GLBoost.Vector2(0.0, 1.0),
-    new GLBoost.Vector2(0.0, 0.0),
-    new GLBoost.Vector2(1.0, 0.0),
-    
-    // Right face
-    new GLBoost.Vector2(1.0, 0.0),
-    new GLBoost.Vector2(1.0, 1.0),
-    new GLBoost.Vector2(0.0, 1.0),
-    new GLBoost.Vector2(0.0, 0.0),
-    
-    // Left face
-    new GLBoost.Vector2(0.0, 0.0),
-    new GLBoost.Vector2(1.0, 0.0),
-    new GLBoost.Vector2(1.0, 1.0),
-    new GLBoost.Vector2(0.0, 1.0)
-];
-*/
 // 頂点データを配列で指定する場合
-var positions = [
+let positions = [
     // Front face
     [-0.5, -0.5,  0.5], // v0
     [ 0.5, -0.5,  0.5], // v1
@@ -136,7 +68,7 @@ var positions = [
     [-0.5, -0.5, -0.5]  // v4
 ];
 
-var texcoords = [
+let texcoords = [
     // Front face
     [0.0, 0.0],
     [1.0, 0.0],
@@ -174,7 +106,7 @@ var texcoords = [
     [0.0, 1.0]
 ];
 
-var indices = [
+let indices = [
      0,  1,  2,    0,  2 , 3,  // Front face
      4,  5,  6,    4,  6 , 7,  // Back face
      8,  9, 10,    8, 10, 11,  // Top face
@@ -183,41 +115,41 @@ var indices = [
     20, 21, 22,   20, 22, 23   // Left face
 ];
 
-var geometry = glBoostContext.createGeometry();
-var texture = glBoostContext.createTexture('../../assets/A/k/w/j/AkwjW.jpg'); // frog.jpg
-var material = glBoostContext.createClassicMaterial();
+let geometry = glBoostContext.createGeometry();
+let texture = glBoostContext.createTexture('../../assets/A/k/w/j/AkwjW.jpg'); // frog.jpg
+let material = glBoostContext.createClassicMaterial();
 material.setTexture(texture);
 geometry.setVerticesData({
     position: positions,
     texcoord: texcoords
 }, [indices], GLBoost.TRIANGLE);
 
-var mesh = glBoostContext.createMesh(geometry, material);
+let mesh = glBoostContext.createMesh(geometry, material);
 scene.addChild(mesh);
 
-var camera = glBoostContext.createPerspectiveCamera({
+let camera = glBoostContext.createPerspectiveCamera({
     eye: new GLBoost.Vector3(0.0, 0.0, 3.0),
     center: new GLBoost.Vector3(0.0, 0.0, 0.0),
     up: new GLBoost.Vector3(0.0, 1.0, 0.0)
 }, {
     fovy: 45.0,
-    aspect: 1.0,
+    aspect: width/height,
     zNear: 0.1,
     zFar: 10.0
 });
 
 scene.addChild(camera);
 
-var expression = glBoostContext.createExpressionAndRenderPasses(1);
+let expression = glBoostContext.createExpressionAndRenderPasses(1);
 expression.renderPasses[0].scene = scene;
 expression.prepareToRender();
 
 (function(){
     renderer.clearCanvas();
     renderer.draw(expression);
-    var rotateMatrixX = GLBoost.Matrix33.rotateX(1);
-    var rotateMatrixY = GLBoost.Matrix33.rotateY(1);
-    var rotatedVector = rotateMatrixX.multiplyVector(camera.eye);
+    let rotateMatrixX = GLBoost.Matrix33.rotateX(1);
+    let rotateMatrixY = GLBoost.Matrix33.rotateY(1);
+    let rotatedVector = rotateMatrixX.multiplyVector(camera.eye);
     rotatedVector = rotateMatrixY.multiplyVector(rotatedVector);
     camera.eye = rotatedVector;
 
@@ -225,6 +157,6 @@ expression.prepareToRender();
 })();
 
 function draw(canvas) {
-    var domElement = canvas;
+    let domElement = canvas;
     canvas.context.drawImage(domElement, 0, 0, domElement.width, domElement.height);
 }
