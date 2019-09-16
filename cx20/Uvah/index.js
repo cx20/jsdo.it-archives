b922bae04ef320b3fe99459e3b4cc705dbe6c4ae
cx20 <y-shimano@hondarer-soft.com>
2019-09-16 20:51:00 +0900

    [WebGL] GLBoost を試してみるテスト（standalone編）（その２）

--- cx20/Uvah/index.js
+++ cx20/Uvah/index.js
@@ -3,11 +3,15 @@
 // forked from cx20's "[WebGL] GLBoost を試してみるテスト" http://jsdo.it/cx20/SWec
 // forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
 
-var canvas = document.getElementById("world");
-var glBoostContext = new GLBoost.GLBoostMiddleContext(canvas);
-var renderer = glBoostContext.createRenderer({ canvas: canvas, clearColor: {red:1, green:1, blue:1, alpha:1}});
+let width = window.innerWidth;
+let height = window.innerHeight;
 
-var scene = glBoostContext.createScene();
+let canvas = document.getElementById("world");
+let glBoostContext = new GLBoost.GLBoostMiddleContext(canvas);
+let renderer = glBoostContext.createRenderer({ canvas: canvas, clearColor: {red:1, green:1, blue:1, alpha:1}});
+renderer.resize(width, height);
+
+let scene = glBoostContext.createScene();
 
 // 正方形の座標データを用意
 //             1.0 y 
@@ -25,53 +29,37 @@ var scene = glBoostContext.createScene();
 //         |        |
 //        [2]------[3]
 //
-/*
-// 頂点データを Vector で指定する場合
-var positions = [
-    new GLBoost.Vector3(-0.5,  0.5, 0.0), // v0
-    new GLBoost.Vector3( 0.5,  0.5, 0.0), // v1
-    new GLBoost.Vector3(-0.5, -0.5, 0.0), // v2
-    new GLBoost.Vector3( 0.5, -0.5, 0.0)  // v3
-];
-
-var colors = [
-    new GLBoost.Vector3(1.0, 0.0, 0.0),  // v0
-    new GLBoost.Vector3(0.0, 1.0, 0.0),  // v1
-    new GLBoost.Vector3(0.0, 0.0, 1.0),  // v2
-    new GLBoost.Vector3(1.0, 1.0, 0.0)   // v3
-];
-*/
 
 // 頂点データを配列で指定する場合
-var positions = [
+let positions = [
     [-0.5,  0.5, 0.0], // v0
     [ 0.5,  0.5, 0.0], // v1
     [-0.5, -0.5, 0.0], // v2
     [ 0.5, -0.5, 0.0]  // v3
 ];
 
-var colors = [
+let colors = [
     [1.0, 0.0, 0.0],  // v0
     [0.0, 1.0, 0.0],  // v1
     [0.0, 0.0, 1.0],  // v2
     [1.0, 1.0, 0.0]   // v3
 ];
 
-var indices = [
+let indices = [
     0, 2, 1, 3
 ];
 
-var geometry = glBoostContext.createGeometry();
+let geometry = glBoostContext.createGeometry();
 geometry.setVerticesData({
     position: positions,
     color: colors
 }, [indices], GLBoost.TRIANGLE_STRIP);
 
-var mesh = glBoostContext.createMesh(geometry);
+let mesh = glBoostContext.createMesh(geometry);
 
 scene.addChild(mesh);
 
-var expression = glBoostContext.createExpressionAndRenderPasses(1);
+let expression = glBoostContext.createExpressionAndRenderPasses(1);
 expression.renderPasses[0].scene = scene;
 expression.prepareToRender();
 
@@ -82,6 +70,6 @@ expression.prepareToRender();
 })();
 
 function draw(canvas) {
-    var domElement = canvas;
+    let domElement = canvas;
     canvas.context.drawImage(domElement, 0, 0, domElement.width, domElement.height);
 }
