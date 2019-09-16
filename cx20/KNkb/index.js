50a2260a92e4f26e0d5a220e130adc816df25f9d
cx20 <y-shimano@hondarer-soft.com>
2019-09-16 21:20:12 +0900

    [WebGL] GLBoost で消しゴムを表示させてみるテスト（standalone編）

--- cx20/KNkb/index.js
+++ cx20/KNkb/index.js
@@ -6,11 +6,16 @@
 // forked from cx20's "[WebGL] GLBoost を試してみるテスト" http://jsdo.it/cx20/SWec
 // forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
 
-var canvas = document.getElementById("world");
+let width = window.innerWidth;
+let height = window.innerHeight;
 
-var renderer = new GLBoost.Renderer({ canvas: canvas, clearColor: {red:1, green:1, blue:1, alpha:1}});
 
-var scene = new GLBoost.Scene();
+let canvas = document.getElementById("world");
+let glBoostContext = new GLBoost.GLBoostMiddleContext(canvas);
+let renderer = glBoostContext.createRenderer({ canvas: canvas, clearColor: {red:1, green:1, blue:1, alpha:1}});
+renderer.resize(width, height);
+
+let scene = glBoostContext.createScene();
 
 // 立方体の座標データを用意
 //             1.0 y 
@@ -30,69 +35,69 @@ var scene = new GLBoost.Scene();
 //       |/       |/
 //      [0]------[1]
 //
-var positions = [
+let positions = [
     // Front face
-    new GLBoost.Vector3(-0.5, -0.5,  0.5), // v0
-    new GLBoost.Vector3( 0.5, -0.5,  0.5), // v1
-    new GLBoost.Vector3( 0.5,  0.5,  0.5), // v2
-    new GLBoost.Vector3(-0.5,  0.5,  0.5), // v3
+    [-0.5, -0.5,  0.5], // v0
+    [ 0.5, -0.5,  0.5], // v1
+    [ 0.5,  0.5,  0.5], // v2
+    [-0.5,  0.5,  0.5], // v3
     // Back face
-    new GLBoost.Vector3(-0.5, -0.5, -0.5), // v4
-    new GLBoost.Vector3( 0.5, -0.5, -0.5), // v5
-    new GLBoost.Vector3( 0.5,  0.5, -0.5), // v6
-    new GLBoost.Vector3(-0.5,  0.5, -0.5), // v7
+    [-0.5, -0.5, -0.5], // v4
+    [ 0.5, -0.5, -0.5], // v5
+    [ 0.5,  0.5, -0.5], // v6
+    [-0.5,  0.5, -0.5], // v7
     // Top face
-    new GLBoost.Vector3( 0.5,  0.5,  0.5), // v2
-    new GLBoost.Vector3(-0.5,  0.5,  0.5), // v3
-    new GLBoost.Vector3(-0.5,  0.5, -0.5), // v7
-    new GLBoost.Vector3( 0.5,  0.5, -0.5), // v6
+    [ 0.5,  0.5,  0.5], // v2
+    [-0.5,  0.5,  0.5], // v3
+    [-0.5,  0.5, -0.5], // v7
+    [ 0.5,  0.5, -0.5], // v6
     // Bottom face
-    new GLBoost.Vector3(-0.5, -0.5,  0.5), // v0
-    new GLBoost.Vector3( 0.5, -0.5,  0.5), // v1
-    new GLBoost.Vector3( 0.5, -0.5, -0.5), // v5
-    new GLBoost.Vector3(-0.5, -0.5, -0.5), // v4
+    [-0.5, -0.5,  0.5], // v0
+    [ 0.5, -0.5,  0.5], // v1
+    [ 0.5, -0.5, -0.5], // v5
+    [-0.5, -0.5, -0.5], // v4
     // Right face
-    new GLBoost.Vector3( 0.5, -0.5,  0.5), // v1
-    new GLBoost.Vector3( 0.5,  0.5,  0.5), // v2
-    new GLBoost.Vector3( 0.5,  0.5, -0.5), // v6
-    new GLBoost.Vector3( 0.5, -0.5, -0.5), // v5
+    [ 0.5, -0.5,  0.5], // v1
+    [ 0.5,  0.5,  0.5], // v2
+    [ 0.5,  0.5, -0.5], // v6
+    [ 0.5, -0.5, -0.5], // v5
     // Left face
-    new GLBoost.Vector3(-0.5, -0.5,  0.5), // v0
-    new GLBoost.Vector3(-0.5,  0.5,  0.5), // v3
-    new GLBoost.Vector3(-0.5,  0.5, -0.5), // v7
-    new GLBoost.Vector3(-0.5, -0.5, -0.5)  // v4
+    [-0.5, -0.5,  0.5], // v0
+    [-0.5,  0.5,  0.5], // v3
+    [-0.5,  0.5, -0.5], // v7
+    [-0.5, -0.5, -0.5]  // v4
 ];
 
-var colors = [
-    new GLBoost.Vector3(1.0, 0.0, 0.0, 1.0), // Front face
-    new GLBoost.Vector3(1.0, 0.0, 0.0, 1.0), // Front face
-    new GLBoost.Vector3(1.0, 0.0, 0.0, 1.0), // Front face
-    new GLBoost.Vector3(1.0, 0.0, 0.0, 1.0), // Front face
+let colors = [
+    [1.0, 0.0, 0.0, 1.0], // Front face
+    [1.0, 0.0, 0.0, 1.0], // Front face
+    [1.0, 0.0, 0.0, 1.0], // Front face
+    [1.0, 0.0, 0.0, 1.0], // Front face
     
-    new GLBoost.Vector3(1.0, 1.0, 0.0, 1.0), // Back face
-    new GLBoost.Vector3(1.0, 1.0, 0.0, 1.0), // Back face
-    new GLBoost.Vector3(1.0, 1.0, 0.0, 1.0), // Back face
-    new GLBoost.Vector3(1.0, 1.0, 0.0, 1.0), // Back face
+    [1.0, 1.0, 0.0, 1.0], // Back face
+    [1.0, 1.0, 0.0, 1.0], // Back face
+    [1.0, 1.0, 0.0, 1.0], // Back face
+    [1.0, 1.0, 0.0, 1.0], // Back face
     
-    new GLBoost.Vector3(0.0, 1.0, 0.0, 1.0), // Top face
-    new GLBoost.Vector3(0.0, 1.0, 0.0, 1.0), // Top face
-    new GLBoost.Vector3(0.0, 1.0, 0.0, 1.0), // Top face
-    new GLBoost.Vector3(0.0, 1.0, 0.0, 1.0), // Top face
+    [0.0, 1.0, 0.0, 1.0], // Top face
+    [0.0, 1.0, 0.0, 1.0], // Top face
+    [0.0, 1.0, 0.0, 1.0], // Top face
+    [0.0, 1.0, 0.0, 1.0], // Top face
     
-    new GLBoost.Vector3(1.0, 0.5, 0.5, 1.0), // Bottom face
-    new GLBoost.Vector3(1.0, 0.5, 0.5, 1.0), // Bottom face
-    new GLBoost.Vector3(1.0, 0.5, 0.5, 1.0), // Bottom face
-    new GLBoost.Vector3(1.0, 0.5, 0.5, 1.0), // Bottom face
+    [1.0, 0.5, 0.5, 1.0], // Bottom face
+    [1.0, 0.5, 0.5, 1.0], // Bottom face
+    [1.0, 0.5, 0.5, 1.0], // Bottom face
+    [1.0, 0.5, 0.5, 1.0], // Bottom face
     
-    new GLBoost.Vector3(1.0, 0.0, 1.0, 1.0), // Right face
-    new GLBoost.Vector3(1.0, 0.0, 1.0, 1.0), // Right face
-    new GLBoost.Vector3(1.0, 0.0, 1.0, 1.0), // Right face
-    new GLBoost.Vector3(1.0, 0.0, 1.0, 1.0), // Right face
+    [1.0, 0.0, 1.0, 1.0], // Right face
+    [1.0, 0.0, 1.0, 1.0], // Right face
+    [1.0, 0.0, 1.0, 1.0], // Right face
+    [1.0, 0.0, 1.0, 1.0], // Right face
     
-    new GLBoost.Vector3(0.0, 0.0, 1.0, 1.0),  // Left face
-    new GLBoost.Vector3(0.0, 0.0, 1.0, 1.0),  // Left face
-    new GLBoost.Vector3(0.0, 0.0, 1.0, 1.0),  // Left face
-    new GLBoost.Vector3(0.0, 0.0, 1.0, 1.0)   // Left face
+    [0.0, 0.0, 1.0, 1.0],  // Left face
+    [0.0, 0.0, 1.0, 1.0],  // Left face
+    [0.0, 0.0, 1.0, 1.0],  // Left face
+    [0.0, 0.0, 1.0, 1.0]   // Left face
 ];
 
 // テクスチャ座標データを用意
@@ -104,45 +109,45 @@ var colors = [
 //     +----+----+----+----+ -> s
 //  (0, 0)               (1, 0)
 //
-var texcoords = [
+let texcoords = [
     // Front face
-    new GLBoost.Vector2(0.5,  0.5), // v0
-    new GLBoost.Vector2(0.75, 0.5), // v1
-    new GLBoost.Vector2(0.75, 1.0), // v2
-    new GLBoost.Vector2(0.5,  1.0), // v3
+    [0.5,  0.5], // v0
+    [0.75, 0.5], // v1
+    [0.75, 1.0], // v2
+    [0.5,  1.0], // v3
 
     // Back face
-    new GLBoost.Vector2(0.25, 0.5), // v4
-    new GLBoost.Vector2(0.5,  0.5), // v5
-    new GLBoost.Vector2(0.5,  1.0), // v6
-    new GLBoost.Vector2(0.25, 1.0), // v7
+    [0.25, 0.5], // v4
+    [0.5,  0.5], // v5
+    [0.5,  1.0], // v6
+    [0.25, 1.0], // v7
 
     // Top face
-    new GLBoost.Vector2(0.75, 0.5), // v2
-    new GLBoost.Vector2(0.5,  0.5), // v3
-    new GLBoost.Vector2(0.5,  0.0), // v7
-    new GLBoost.Vector2(0.75, 0.0), // v6
+    [0.75, 0.5], // v2
+    [0.5,  0.5], // v3
+    [0.5,  0.0], // v7
+    [0.75, 0.0], // v6
 
     // Bottom face
-    new GLBoost.Vector2(0.0,  0.5), // v0
-    new GLBoost.Vector2(0.25, 0.5), // v1
-    new GLBoost.Vector2(0.25, 1.0), // v5
-    new GLBoost.Vector2(0.0,  1.0), // v4
+    [0.0,  0.5], // v0
+    [0.25, 0.5], // v1
+    [0.25, 1.0], // v5
+    [0.0,  1.0], // v4
 
     // Right face
-    new GLBoost.Vector2(0.0,  0.5), // v1
-    new GLBoost.Vector2(0.0,  0.0), // v2
-    new GLBoost.Vector2(0.25, 0.0), // v6
-    new GLBoost.Vector2(0.25, 0.5), // v5
+    [0.0,  0.5], // v1
+    [0.0,  0.0], // v2
+    [0.25, 0.0], // v6
+    [0.25, 0.5], // v5
 
     // Left face
-    new GLBoost.Vector2(0.5,  0.5), // v0
-    new GLBoost.Vector2(0.5,  0.0), // v3
-    new GLBoost.Vector2(0.25, 0.0), // v7
-    new GLBoost.Vector2(0.25, 0.5)  // v4
+    [0.5,  0.5], // v0
+    [0.5,  0.0], // v3
+    [0.25, 0.0], // v7
+    [0.25, 0.5]  // v4
 ];
 
-var indices = [
+let indices = [
      0,  1,  2,    0,  2 , 3,  // Front face
      4,  5,  6,    4,  6 , 7,  // Back face
      8,  9, 10,    8, 10, 11,  // Top face
@@ -151,48 +156,52 @@ var indices = [
     20, 21, 22,   20, 22, 23   // Left face
 ];
 
-var geometry = new GLBoost.Geometry(canvas);
-//var texture = new GLBoost.Texture('/assets/A/k/w/j/AkwjW.jpg'); // frog.jpg
-var texture = new GLBoost.Texture('../../assets/7/5/o/P/75oPt.png'); // 512x256.png
-var material = new GLBoost.ClassicMaterial();
-material.diffuseTexture = texture;
+let geometry = glBoostContext.createGeometry();
+let texture = glBoostContext.createTexture('../../assets/7/5/o/P/75oPt.png'); // 512x256.png
+let material = glBoostContext.createClassicMaterial();
+material.setTexture(texture);
 geometry.setVerticesData({
     position: positions,
     texcoord: texcoords
 }, [indices], GLBoost.TRIANGLE);
 
-var mesh = new GLBoost.Mesh(geometry, material);
+let mesh = glBoostContext.createMesh(geometry, material);
 mesh.scale = new GLBoost.Vector3(1.0, 0.2, 0.5);
-scene.add( mesh );
+scene.addChild(mesh);
 
-var camera = new GLBoost.Camera({
+let camera = glBoostContext.createPerspectiveCamera({
     eye: new GLBoost.Vector3(0.0, 0.0, 3.0),
     center: new GLBoost.Vector3(0.0, 0.0, 0.0),
     up: new GLBoost.Vector3(0.0, 1.0, 0.0)
 }, {
     fovy: 45.0,
-    aspect: 1.0,
+    aspect: width/height,
     zNear: 0.1,
     zFar: 10.0
 });
 
-scene.add( camera );
+scene.addChild(camera);
 
-scene.prepareForRender();
+var expression = glBoostContext.createExpressionAndRenderPasses(1);
+expression.renderPasses[0].scene = scene;
+expression.prepareToRender();
 
-(function(){
+var render = function() {
     renderer.clearCanvas();
-    renderer.draw(scene);
-    var rotateMatrixX = GLBoost.Matrix33.rotateX(1);
-    var rotateMatrixY = GLBoost.Matrix33.rotateY(1);
-    var rotatedVector = rotateMatrixX.multiplyVector(camera.eye);
+    renderer.draw(expression);
+
+    let rotateMatrixX = GLBoost.Matrix33.rotateX(1);
+    let rotateMatrixY = GLBoost.Matrix33.rotateY(1);
+    let rotatedVector = rotateMatrixX.multiplyVector(camera.eye);
     rotatedVector = rotateMatrixY.multiplyVector(rotatedVector);
     camera.eye = rotatedVector;
 
-    requestAnimationFrame(arguments.callee);
-})();
+    requestAnimationFrame(render);
+};
 
 function draw(canvas) {
-    var domElement = canvas;
+    let domElement = canvas;
     canvas.context.drawImage(domElement, 0, 0, domElement.width, domElement.height);
 }
+
+render();
