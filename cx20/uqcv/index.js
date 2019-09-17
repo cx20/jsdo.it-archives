2e2c923d84a87f25ba24c473d5db07c5771933fa
cx20 <y-shimano@hondarer-soft.com>
2019-09-18 07:34:00 +0900

    [WebGL] Babylon.js を試してみるテスト（組み込み関数編）（その３）

---
+++ cx20/uqcv/index.js
@@ -0,0 +1,57 @@
+// forked from cx20's "[WebGL] Babylon.js を試してみるテスト（組み込み関数編）（その２）" http://jsdo.it/cx20/anpf
+// forked from cx20's "[WebGL] Babylon.js を試してみるテスト（組み込み関数編）" http://jsdo.it/cx20/oRtWo
+// forked from cx20's "[WebGL] Babylon.js を試してみるテスト" http://jsdo.it/cx20/84AP
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+
+var cube;
+var createScene = function(engine) {
+    var scene = new BABYLON.Scene(engine);
+    var camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(0, 0, -3), scene);
+    scene.clearColor = new BABYLON.Color3(1, 1, 1);
+    cube = new BABYLON.Mesh.CreateBox('cube', 1.0, scene);
+    var colors = [
+            1.0, 0.0, 0.0, 1.0, // Front face
+            1.0, 0.0, 0.0, 1.0, // Front face
+            1.0, 0.0, 0.0, 1.0, // Front face
+            1.0, 0.0, 0.0, 1.0, // Front face
+            1.0, 1.0, 0.0, 1.0, // Back face
+            1.0, 1.0, 0.0, 1.0, // Back face
+            1.0, 1.0, 0.0, 1.0, // Back face
+            1.0, 1.0, 0.0, 1.0, // Back face
+            0.0, 1.0, 0.0, 1.0, // Top face
+            0.0, 1.0, 0.0, 1.0, // Top face
+            0.0, 1.0, 0.0, 1.0, // Top face
+            0.0, 1.0, 0.0, 1.0, // Top face
+            1.0, 0.5, 0.5, 1.0, // Bottom face
+            1.0, 0.5, 0.5, 1.0, // Bottom face
+            1.0, 0.5, 0.5, 1.0, // Bottom face
+            1.0, 0.5, 0.5, 1.0, // Bottom face
+            1.0, 0.0, 1.0, 1.0, // Right face
+            1.0, 0.0, 1.0, 1.0, // Right face
+            1.0, 0.0, 1.0, 1.0, // Right face
+            1.0, 0.0, 1.0, 1.0, // Right face
+            0.0, 0.0, 1.0, 1.0, // Left face
+            0.0, 0.0, 1.0, 1.0, // Left face
+            0.0, 0.0, 1.0, 1.0, // Left face
+            0.0, 0.0, 1.0, 1.0  // Left face
+    ];
+    cube.setVerticesData(BABYLON.VertexBuffer.ColorKind, colors, true);
+    cube.material = new BABYLON.StandardMaterial("material", scene);
+    cube.material.emissiveColor = new BABYLON.Color3(1, 1, 1);
+
+    return scene;
+}
+
+var canvas = document.querySelector("#c");
+var engine = new BABYLON.Engine(canvas, true);
+var scene = createScene(engine);
+
+
+var rad = 0.0;
+engine.runRenderLoop(function () {
+    rad += Math.PI * 1.0 / 180.0;
+    cube.rotation.x = rad;
+    cube.rotation.y = rad;
+    cube.rotation.z = rad;
+    scene.render();
+});
---
+++ cx20/uqcv/readme.txt
@@ -0,0 +1,17 @@
+[WebGL] Babylon.js を試してみるテスト（組み込み関数編）（その３）
+
+＜対応した点＞
+・BABYLON.Mesh.CreateBox() にて立方体を作成するよう対応。
+
+＜参考＞
+■ BabylonJS/Babylon.js
+https://github.com/BabylonJS/Babylon.js
+
+■ Babylon.js
+http://www.babylonjs.com/
+
+■ [WebGL] Babylon.js を試してみるテスト（VBO 編）（その３）
+http://jsdo.it/cx20/o79P
+
+■ 各種 WebGL ライブラリで基本図形を表示してみる - Qiita
+http://qiita.com/cx20/items/0fa19c96aa6470d98807
