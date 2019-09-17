a1db0f28042e271bf0b214104b10ff749b0658f5
cx20 <y-shimano@hondarer-soft.com>
2019-09-17 23:59:42 +0900

    [WebGL] Babylon.js を試してみるテスト（組み込み関数編）

---
+++ cx20/oRtWo/index.js
@@ -0,0 +1,26 @@
+// forked from cx20's "[WebGL] Babylon.js を試してみるテスト" http://jsdo.it/cx20/84AP
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+
+var createScene = function(engine) {
+    var scene = new BABYLON.Scene(engine);
+    var camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(0, 0, -2.4), scene);
+    scene.clearColor = new BABYLON.Color3(1, 1, 1);
+    var points = [];
+    points.push( new BABYLON.Vector3( 0.0,  0.5, 0.0 ) );
+    points.push( new BABYLON.Vector3(-0.5, -0.5, 0.0 ) );
+    points.push( new BABYLON.Vector3( 0.5, -0.5, 0.0 ) );
+    points.push( new BABYLON.Vector3( 0.0,  0.5, 0.0 ) );
+
+    var triangle = new BABYLON.Mesh.CreateLines('triangle', points, scene);
+    triangle.color = new BABYLON.Color3(0, 0, 1);
+
+    return scene;
+}
+
+var canvas = document.querySelector("#c");
+var engine = new BABYLON.Engine(canvas, true);
+var scene = createScene(engine);
+
+engine.runRenderLoop(function () {
+    scene.render();
+});
---
+++ cx20/oRtWo/readme.txt
@@ -0,0 +1,20 @@
+[WebGL] Babylon.js を試してみるテスト（組み込み関数編）
+
+＜対応した点＞
+・BABYLON.Mesh.CreateLines() を用いて、三角形を描くよう対応。
+
+＜対応できていない点＞
+・CreateLines() の場合、gl.LINES で描画される為、塗りつぶしが行われないようです。
+
+＜参考＞
+■ BabylonJS/Babylon.js
+https://github.com/BabylonJS/Babylon.js
+
+■ Babylon.js
+http://www.babylonjs.com/
+
+■ [WebGL] Babylon.js を試してみるテスト（VBO 編）
+http://jsdo.it/cx20/84AP
+
+■ 各種 WebGL ライブラリで基本図形を表示してみる - Qiita
+http://qiita.com/cx20/items/0fa19c96aa6470d98807
