dde876fb34092ba6c28e7ccd1b2b75009a746913
cx20 <y-shimano@hondarer-soft.com>
2019-09-18 07:57:29 +0900

    [WebGL] Babylon.js で消しゴムを表示させてみるテスト（組み込み関数編）

---
+++ cx20/kIEf/index.js
@@ -0,0 +1,51 @@
+// forked from cx20's "[WebGL] Babylon.js を試してみるテスト（組み込み関数編）（その４）" http://jsdo.it/cx20/jwt0
+// forked from cx20's "[WebGL] Babylon.js を試してみるテスト（組み込み関数編）（その３）" http://jsdo.it/cx20/uqcv
+// forked from cx20's "[WebGL] Babylon.js を試してみるテスト（組み込み関数編）（その２）" http://jsdo.it/cx20/anpf
+// forked from cx20's "[WebGL] Babylon.js を試してみるテスト（組み込み関数編）" http://jsdo.it/cx20/oRtWo
+// forked from cx20's "[WebGL] Babylon.js を試してみるテスト" http://jsdo.it/cx20/84AP
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+
+var cube;
+var createScene = function(engine) {
+    var scene = new BABYLON.Scene(engine);
+    var camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(0, 0, -3), scene);
+    scene.clearColor = new BABYLON.Color3(1, 1, 1);
+    cube = new BABYLON.Mesh.CreateBox('cube', 1.0, scene);
+    var material = new BABYLON.StandardMaterial("material", scene);
+    material.reflectionTexture = new BABYLON.CubeTexture(
+        "../../assets",
+        scene,
+        [
+        "/k/J/E/L/kJELT.png", // "/eraser_px.png",
+        "/A/n/c/O/AncOu.png", // "/eraser_py.png",
+        "/4/5/e/g/45egw.png", // "/eraser_pz.png",
+        "/i/I/K/q/iIKql.png", // "/eraser_nx.png",
+        "/8/y/P/B/8yPB2.png", // "/eraser_ny.png",
+        "/2/m/0/f/2m0fo.png", // "/eraser_nz.png"
+        ]
+    );
+    material.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+    
+    cube.material = material;
+    cube.material.backFaceCulling = false;
+
+    // 消しゴムのサイズとなるよう調整
+    cube.scaling.x = 1.0;
+    cube.scaling.y = 0.2;
+    cube.scaling.z = 0.5;
+
+    return scene;
+}
+
+var canvas = document.querySelector("#c");
+var engine = new BABYLON.Engine(canvas, true);
+var scene = createScene(engine);
+
+var rad = 0.0;
+engine.runRenderLoop(function () {
+    rad += Math.PI * 1.0 / 180.0;
+    cube.rotation.x = rad;
+    //cube.rotation.y = rad;
+    cube.rotation.z = rad;
+    scene.render();
+});
---
+++ cx20/kIEf/readme.txt
@@ -0,0 +1,23 @@
+[WebGL] Babylon.js で消しゴムを表示させてみるテスト（組み込み関数編）
+
+＜対応した点＞
+・CubeTexture を用いて消しゴムを作成するよう対応。
+
+＜参考＞
+■ [WebGL] Babylon.js で消しゴムを表示させてみるテスト
+http://jsdo.it/cx20/0yXZ
+
+■ [簡易版] WebGL で消しゴムを表示させてみるテスト
+http://jsdo.it/cx20/48E8
+
+■ Three.js + Oimo.js で消しゴムを落下させてみるテスト（その３）
+http://jsdo.it/cx20/aFHd
+
+■ 13 Environment - BabylonJS_Babylon.js Wiki
+https://github.com/BabylonJS/Babylon.js/wiki/13-Environment
+
+■ [WebGL] Babylon.js の Water material を試してみるテスト
+http://jsdo.it/cx20/QJZU
+
+■ 各種 WebGL ライブラリで基本図形を表示してみる
+http://qiita.com/cx20/items/0fa19c96aa6470d98807
