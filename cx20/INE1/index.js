3569415bad8d4d08c7b88984a937f60e477a8c15
cx20 <y-shimano@hondarer-soft.com>
2019-09-19 00:54:25 +0900

    [WebGL] Babylon.js + SolidParticleSystem を試してみるテスト（その２改）

---
+++ cx20/INE1/index.js
@@ -0,0 +1,115 @@
+// forked from cx20's "[WebGL] Babylon.js + SolidParticleSystem を試してみるテスト（その２）" http://jsdo.it/cx20/Clm4
+// forked from cx20's "[WebGL] Babylon.js + SolidParticleSystem を試してみるテスト" http://jsdo.it/cx20/UVZC
+// forked from cx20's "[WebGL] Babylon.js + ParticleSystem を試してみるテスト（その４）" http://jsdo.it/cx20/0uS0
+// forked from cx20's "[WebGL] Babylon.js + ParticleSystem を試してみるテスト（その３）" http://jsdo.it/cx20/ATr7
+// forked from cx20's "[WebGL] Babylon.js + ParticleSystem を試してみるテスト（その２）" http://jsdo.it/cx20/WVW3
+// forked from cx20's "[WebGL] Babylon.js + ParticleSystem を試してみるテスト" http://jsdo.it/cx20/yxN2
+// forked from cx20's "[WebGL] Babylon.js を試してみるテスト（組み込み関数編）（その３）" http://jsdo.it/cx20/uqcv
+// forked from cx20's "[WebGL] Babylon.js を試してみるテスト（組み込み関数編）（その２）" http://jsdo.it/cx20/anpf
+// forked from cx20's "[WebGL] Babylon.js を試してみるテスト（組み込み関数編）" http://jsdo.it/cx20/oRtWo
+// forked from cx20's "[WebGL] Babylon.js を試してみるテスト" http://jsdo.it/cx20/84AP
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+
+// http://www.babylonjs-playground.com/#2KSQ1R#110
+var createScene = function() {
+    var scene = new BABYLON.Scene(engine);
+    scene.clearColor = new BABYLON.Color3(.1, .2, .4);
+    var camera = new BABYLON.ArcRotateCamera("camera1", 0, 0, 0, new BABYLON.Vector3(0, 0, -0), scene);
+    //camera.setPosition(new BABYLON.Vector3(0, 150, -500));
+    camera.setPosition(new BABYLON.Vector3(0, 150, -300));
+    camera.attachControl(canvas, true);
+    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene);
+    light.intensity = 0.9;
+    var pl = new BABYLON.PointLight("pl", new BABYLON.Vector3(0, 0, 0), scene);
+    pl.diffuse = new BABYLON.Color3(1, 1, 1);
+    pl.specular = new BABYLON.Color3(0.2, 0.2, 0.8);
+    //pl.intensity = 0.95;
+    pl.intensity = 0.3;
+
+    var ground = BABYLON.Mesh.CreateGround('gd', 300, 300, 2, scene);
+    ground.position.y = -20;
+    ground.freezeWorldMatrix();
+
+    var SPS = new BABYLON.SolidParticleSystem('SPS', scene);
+    //SPS.disableParticleColor();
+    //SPS.disableParticleTexture();
+
+    // The first parameter can be used to specify which mesh to import. Here we import all meshes
+    //BABYLON.SceneLoader.ImportMesh("", "scenes/", "rabbit.babylon", scene, function (meshes) {
+    //BABYLON.SceneLoader.ImportMesh("", "/assets/E/c/r/y/", "Ecryr", scene, function(meshes) { // rabbit.babylon
+    BABYLON.SceneLoader.ImportMesh("", "../../assets/4/D/o/o/", "4DooI.babylon", scene, function(meshes) { // monkey.babylon
+
+        var k = 0.0;
+        var gravity = -0.01;
+        //var v = 2;
+        var v = 2 * 0.15;
+        var skullNb = 60;
+
+        SPS.initParticles = function() {
+            var pi2 = Math.PI * 2;
+            for (var p = 0; p < this.nbParticles; p++) {
+                this.particles[p].position = BABYLON.Vector3.Zero();
+                this.particles[p].rotation = new BABYLON.Vector3(Math.random() * pi2, Math.random() * pi2, Math.random() * pi2);
+                this.particles[p].velocity = new BABYLON.Vector3((Math.random() - 0.5) * v, Math.random() * v, (Math.random() - 0.5) * v);
+                this.particles[p].color = new BABYLON.Color4(Math.random() / 2 + 0.5, Math.random() / 2 + 0.5, Math.random() / 2 + 0.5, 1);
+            }
+        };
+
+        SPS.updateParticle = function(particle) {
+            particle.rotation.y += 0.01;
+            particle.rotation.x += 0.1;
+            particle.position.addInPlace(particle.velocity);
+            particle.position.y += v / 2;
+            particle.velocity.y += gravity;
+            if (particle.position.y < 0) {
+                particle.position.x = 0.0;
+                particle.position.y = 0.0;
+                particle.position.z = 0.0;
+                var scl = 0.5 + Math.random() / 2;
+                particle.scale.x = scl;
+                particle.scale.y = scl;
+                particle.scale.z = scl;
+                particle.velocity.x = (Math.random() - 0.5) * v;
+                particle.velocity.y = Math.random() * v;
+                particle.velocity.z = (Math.random() - 0.5) * v;
+            }
+
+        };
+
+
+        //SPS.addShape(meshes[1], skullNb);
+        SPS.addShape(meshes[0], skullNb);
+        var mesh = SPS.buildMesh();
+        mesh.scaling = new BABYLON.Vector3(20, 20, 20);
+        mesh.freezeNormals();
+        mesh.freezeWorldMatrix();
+
+        //meshes[1].dispose();
+        meshes[0].dispose();
+
+        SPS.initParticles();
+        SPS.setParticles();
+
+        scene.registerBeforeRender(function() {
+            SPS.setParticles();
+            k += 0.01;
+        });
+
+    });
+
+    // Camera Animations
+    scene.registerBeforeRender(function() {
+        scene.activeCamera.alpha += 0.01;
+    });
+
+    return scene;
+};
+
+var canvas = document.querySelector("#c");
+var engine = new BABYLON.Engine(canvas, true);
+var scene = createScene(engine);
+
+engine.runRenderLoop(function() {
+    scene.render();
+
+});
---
+++ cx20/INE1/readme.txt
@@ -0,0 +1,16 @@
+[WebGL] Babylon.js + SolidParticleSystem を試してみるテスト（その２改）
+
+＜対応した点＞
+・モデルデータをウサギからサルに変更。
+　rabbit.babylon → monkey.babylon
+・カメラの位置を調整
+・ライトの明るさを調整
+・メッシュのサイズを調整
+・速度を調整
+
+＜参考＞
+■ Tutorial part 6: learning how to write a 3D software engine in C#, TS or JS – Texture mapping, back-face culling & WebGL
+https://blogs.msdn.microsoft.com/davrous/2013/07/18/tutorial-part-6-learning-how-to-write-a-3d-software-engine-in-c-ts-or-js-texture-mapping-back-face-culling-webgl/
+
+■ WebGL_ Suzanne rendered with Babylon.JS
+http://david.blob.core.windows.net/softengine3d/part6webgl/index.html
