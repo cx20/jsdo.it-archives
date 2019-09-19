3b706ab69f4dec0b3fd3119eca3a87757026b3be
cx20 <y-shimano@hondarer-soft.com>
2019-09-19 22:47:54 +0900

    [WebGL] PlayCanvas Engine を試してみるテスト（VBO編）（その３）

---
+++ cx20/MPE2/index.js
@@ -0,0 +1,136 @@
+// forked from cx20's "[WebGL] PlayCanvas Engine を試してみるテスト（VBO編）（その２）" http://jsdo.it/cx20/C70T
+// forked from cx20's "[WebGL] PlayCanvas Engine を試してみるテスト（VBO編）" http://jsdo.it/cx20/s9Lw
+// forked from cx20's "[WebGL] PlayCanvas Engine を試してみるテスト" http://jsdo.it/cx20/enuS
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+
+let canvas = document.getElementById('c');
+let app = new pc.Application(canvas, {
+    mouse: new pc.Mouse(document.body),
+    touch: new pc.TouchDevice(document.body)
+});
+app.setCanvasFillMode(pc.FILLMODE_FILL_WINDOW);
+app.setCanvasResolution(pc.RESOLUTION_AUTO);
+app.graphicsDevice.maxPixelRatio = window.devicePixelRatio;
+app.start();
+
+app.scene.ambientLight = new pc.Color(1, 1, 1);
+
+let camera = new pc.Entity();
+camera.addComponent('camera', {
+    clearColor: new pc.Color(1, 1, 1),
+    farClip: 1000,
+    fov: 45
+});
+
+camera.translate(0,0,2.5);
+camera.lookAt(0,0,0);
+app.root.addChild(camera);
+
+let Cube = pc.createScript('cube');
+Cube.prototype.initialize = function () {
+    let node = new pc.scene.GraphNode();
+    // Cube data
+    //             1.0 y 
+    //              ^  -1.0 
+    //              | / z
+    //              |/       x
+    // -1.0 -----------------> +1.0
+    //            / |
+    //      +1.0 /  |
+    //           -1.0
+    // 
+    //         [7]------[6]
+    //        / |      / |
+    //      [3]------[2] |
+    //       |  |     |  |
+    //       | [4]----|-[5]
+    //       |/       |/
+    //      [0]------[1]
+    //
+    let positions = [ 
+        // Front face
+        -0.5, -0.5,  0.5, // v0
+         0.5, -0.5,  0.5, // v1
+         0.5,  0.5,  0.5, // v2
+        -0.5,  0.5,  0.5, // v3
+        // Back face
+        -0.5, -0.5, -0.5, // v4
+         0.5, -0.5, -0.5, // v5
+         0.5,  0.5, -0.5, // v6
+        -0.5,  0.5, -0.5, // v7
+        // Top face
+         0.5,  0.5,  0.5, // v2
+        -0.5,  0.5,  0.5, // v3
+        -0.5,  0.5, -0.5, // v7
+         0.5,  0.5, -0.5, // v6
+        // Bottom face
+        -0.5, -0.5,  0.5, // v0
+         0.5, -0.5,  0.5, // v1
+         0.5, -0.5, -0.5, // v5
+        -0.5, -0.5, -0.5, // v4
+         // Right face
+         0.5, -0.5,  0.5, // v1
+         0.5,  0.5,  0.5, // v2
+         0.5,  0.5, -0.5, // v6
+         0.5, -0.5, -0.5, // v5
+         // Left face
+        -0.5, -0.5,  0.5, // v0
+        -0.5,  0.5,  0.5, // v3
+        -0.5,  0.5, -0.5, // v7
+        -0.5, -0.5, -0.5  // v4
+    ];
+    let colors = [
+        [1.0, 0.0, 0.0, 1.0], // Front face
+        [1.0, 1.0, 0.0, 1.0], // Back face
+        [0.0, 1.0, 0.0, 1.0], // Top face
+        [1.0, 0.5, 0.5, 1.0], // Bottom face
+        [1.0, 0.0, 1.0, 1.0], // Right face
+        [0.0, 0.0, 1.0, 1.0]  // Left face
+    ];
+    let unpackedColors = [];
+    for (let i in colors) {
+        let color = colors[i];
+        for (let j=0; j < 4; j++) {
+            unpackedColors = unpackedColors.concat(color);
+        }
+    }
+    let indices = [
+         0,  1,  2,    0,  2 , 3,  // Front face
+         4,  5,  6,    4,  6 , 7,  // Back face
+         8,  9, 10,    8, 10, 11,  // Top face
+        12, 13, 14,   12, 14, 15,  // Bottom face
+        16, 17, 18,   16, 18, 19,  // Right face
+        20, 21, 22,   20, 22, 23   // Left face
+    ];
+    let options = {
+        indices: indices,
+        colors: unpackedColors.map(function(value){ return value * 255; })
+    };
+    let mesh = pc.createMesh(app.graphicsDevice, positions, options);
+
+    let material = new pc.StandardMaterial();
+    material.diffuseVertexColor = true;
+    material.cull = pc.CULLFACE_NONE;
+
+    let instance = new pc.scene.MeshInstance(node, mesh, material);
+
+    let model = new pc.scene.Model();
+    model.graph = node;
+    model.meshInstances = [ instance ];
+
+    this.entity.addChild(node);
+    app.scene.addModel(model);
+};
+
+Cube.prototype.update = function (deltaTime) {
+    this.entity.rotate(deltaTime * 50, deltaTime * 50, deltaTime * 50);
+};
+
+let cube = new pc.Entity();
+app.root.addChild(cube);
+cube.addComponent('script');
+cube.script.create('cube');
+
+window.addEventListener("resize", function () {
+    app.resizeCanvas(canvas.width, canvas.height);
+});
---
+++ cx20/MPE2/readme.txt
@@ -0,0 +1,8 @@
+[WebGL] PlayCanvas Engine を試してみるテスト（VBO編）（その３）
+
+＜対応した点＞
+・立方体に色を付けるよう対応。
+
+＜参考＞
+■ playcanvas/engine
+https://github.com/playcanvas/engine
