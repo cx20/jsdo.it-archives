7e76b737f2c25c5c05abb4764d5ba6168679d4fd
cx20 <y-shimano@hondarer-soft.com>
2019-09-21 18:33:24 +0900

    [WebGL] Cesium.js で glTF 2.0形式のデータを表示してみるテスト（その３）（調整中）

---
+++ cx20/2s3w/index.js
@@ -0,0 +1,84 @@
+// forked from cx20's "[WebGL] Cesium.js で glTF 2.0形式のデータを表示してみるテスト（その２）" http://jsdo.it/cx20/eFsr
+// forked from cx20's "[WebGL] Cesium.js で glTF 2.0形式のデータを表示してみるテスト" http://jsdo.it/cx20/uw48
+// forked from cx20's "Cesium.js で3Dモデル表示してみるテスト（その１）" http://jsdo.it/cx20/UKCJB
+// forked from cx20's "3D地図ライブラリ「Cesium」を用いて地球を表示してみるテスト" http://jsdo.it/cx20/pGyG
+
+var viewer = new Cesium.Viewer('cesiumContainer', {
+    infoBox : false,
+    selectionIndicator : false,
+    shouldAnimate: true
+});                               
+
+/*
+function createModel(url, height) {
+    viewer.entities.removeAll();
+
+    var position = Cesium.Cartesian3.fromDegrees(139.691706, 35.689487, height);
+    var heading = Cesium.Math.toRadians(135);
+    var pitch = 0;
+    var roll = 0;
+    //var orientation = Cesium.Transforms.headingPitchRollQuaternion(position, heading, pitch, roll);
+    var hpr = new Cesium.HeadingPitchRoll(heading, pitch, roll);
+    var orientation = Cesium.Transforms.headingPitchRollQuaternion(position, hpr);
+
+    var entity = viewer.entities.add({
+        name : url,
+        position : position,
+        orientation : orientation,
+        model : {
+            uri : url,
+            minimumPixelSize : 128,
+            maximumScale : 20000
+        }
+    });
+    viewer.trackedEntity = entity;
+}
+
+function flyToHeadingPitchRoll() {
+    viewer.camera.flyTo({
+        destination : Cesium.Cartesian3.fromDegrees(139.691706 + 1.0, 35.689487 - 1.0, 100000.0),
+        orientation : {
+            heading : Cesium.Math.toRadians(-20.0),
+            pitch : Cesium.Math.toRadians(-35.0),
+            roll : 0.0
+        }
+    });
+}
+*/
+
+function createModel(url, height) {
+    viewer.entities.removeAll();
+
+    var position = Cesium.Cartesian3.fromDegrees(139.691706, 35.689487, height);
+/*
+    var heading = Cesium.Math.toRadians(135);
+    var pitch = 0;
+    var roll = 0;
+*/
+    var heading = Cesium.Math.toRadians(10.0);
+    var pitch = Cesium.Math.toRadians(0.0);
+    var roll = Cesium.Math.toRadians(0.0);
+
+    var hpr = new Cesium.HeadingPitchRoll(heading, pitch, roll);
+    var orientation = Cesium.Transforms.headingPitchRollQuaternion(position, hpr);
+
+    var entity = viewer.entities.add({
+        name : url,
+        position : position,
+        orientation : orientation,
+        model : {
+            uri : url,
+            scale : 1000.0
+        }
+    });
+    viewer.flyTo(entity, { duration: 4 }).then(function() {
+        viewer.trackedEntity = entity;
+    });
+}
+
+//createModel('https://cdn.rawgit.com/KhronosGroup/glTF-WebGL-PBR/817404a4/models/Triangle/glTF/Triangle.gltf', 10000); // Triangle.gltf
+//createModel('https://cdn.rawgit.com/KhronosGroup/glTF-Sample-Models/7268f989/2.0/TextureSettingsTest/glTF/TextureSettingsTest.gltf', 10000); // TextureSettingsTest.gltf
+createModel('https://cdn.rawgit.com/cx20/jsdo-static-contents/c51a03cbff72037e33aa2cc0b7fe7cac4e4bdea8/models/gltf/2.0/EmaSimpleSkin/glTF/EmaSimpleSkin.gltf', 10000); // COLLADA2GLTF 変換結果
+//createModel('https://cdn.rawgit.com/cx20/jsdo-static-contents/76dfc928/models/gltf/2.0/EmaSimpleSkin_blender/glTF/EmaSimpleSkin_blender.gltf', 10000); // Blender出力結果
+
+flyToHeadingPitchRoll();
---
+++ cx20/2s3w/readme.txt
@@ -0,0 +1,27 @@
+[WebGL] Cesium.js で glTF 2.0形式のデータを表示してみるテスト（その３）（調整中）
+
+＜対応した点＞
+・COLLADA2GLTF を使用して変換した glTF 2.0 ファイルを表示するよう対応。
+
+＜参考＞
+■ The COLLADA file created in Maya can not be correctly converted to glTF 2.0
+https://github.com/KhronosGroup/glTF-Blender-Exporter/issues/22
+
+＜他ライブラリ比較＞
+■ [WebGL] three.js で glTF 2.0形式のデータを表示してみるテスト（その３改）
+http://jsdo.it/cx20/YRG5
+■ [WebGL] Babylon.js + glTF2FileLoader を試してみるテスト（その３）（調整中）
+http://jsdo.it/cx20/U1kH
+■ [WebGL] Cesium.js で glTF 2.0形式のデータを表示してみるテスト（その３）（調整中）
+http://jsdo.it/cx20/2s3w
+■ [WebGL] Grimoire.js で glTF 2.0形式のデータを表示してみるテスト（その３改）
+http://jsdo.it/cx20/kPiTg
+■ [WebGL] ClayGL で glTF 2.0 モデルを表示してみるテスト（その３）
+http://jsdo.it/cx20/mPEP
+■ [WebGL] Hilo3d で glTF 2.0 モデルを表示してみるテスト（その３）（調整中）
+http://jsdo.it/cx20/Elp4
+■ [WebGL] PlayCanvas で glTF 2.0 モデルを表示してみるテスト（その３）（調整中）
+http://jsdo.it/cx20/Opvu
+■ [WebGL] GLBoost で glTF 2.0 モデルを表示してみるテスト（その３）（調整中）
+http://jsdo.it/cx20/89Fn
+
