7f6df29d73b093ecbbc96bb6c62ccb8d3b57a294
cx20 <y-shimano@hondarer-soft.com>
2019-09-21 20:16:33 +0900

    Cesium.js で地理院データを使ってみるテスト

---
+++ cx20/lNbX/index.js
@@ -0,0 +1,38 @@
+// forked from cx20's "3D地図ライブラリ「Cesium」を用いて地球を表示してみるテスト" http://jsdo.it/cx20/pGyG
+
+//var widget = new Cesium.CesiumWidget('container');
+var widget = new Cesium.CesiumWidget('container', {
+/*
+    imageryProvider : new Cesium.OpenStreetMapImageryProvider({
+        url : 'http://cyberjapandata.gsi.go.jp/xyz/std/',
+        credit: new Cesium.Credit('国土地理院', '', 'http://portal.cyberjapan.jp/help/termsofuse.html')
+    })
+*/
+    imageryProvider : Cesium.createOpenStreetMapImageryProvider({
+        url : 'http://cyberjapandata.gsi.go.jp/xyz/std/',
+        credit: new Cesium.Credit('国土地理院', '', 'http://portal.cyberjapan.jp/help/termsofuse.html')
+    })
+});
+var scene = widget.scene;
+
+function flyToTokyo() {
+    var longitude = 139.054784;
+    var latitude = 35.685692;
+    var height = 300000.0;
+    scene.camera.flyTo({
+        destination : Cesium.Cartesian3.fromDegrees(longitude, latitude, height)
+    });
+}
+
+function flyToTokyoRectangle() {
+    var west = 139.054784 - 2.0;
+    var south = 35.685692 - 2.0;
+    var east = 139.054784 + 2.0;
+    var north = 35.685692 + 2.0;
+    scene.camera.flyToRectangle({
+        destination : Cesium.Rectangle.fromDegrees(west, south, east, north)
+    });
+}
+
+flyToTokyo();
+//flyToTokyoRectangle();
---
+++ cx20/lNbX/readme.txt
@@ -0,0 +1,13 @@
+Cesium.js で地理院データを使ってみるテスト
+
+＜対応した点＞
+・3D地図ライブラリ Cesium.js を用いて国土地理院データを表示するよう対応。
+
+＜更新履歴＞
+2017/07/03 API仕様変更に対応。OpenStreetMapImageryProvider → createOpenStreetMapImageryProvider
+2015/05/15 ライブラリが 404 Not Found だった為、参照元を変更。
+2014/09/06 初版作成。
+
+＜参考＞
+■ 地理院タイルを用いたサイト構築サンプル集 - 地理院地図｜技術情報
+http://maps.gsi.go.jp/development/sample.html#sample-cs
