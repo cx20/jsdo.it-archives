14eee2985f08a7a724c476ea4e632643d1deeefa
cx20 <y-shimano@hondarer-soft.com>
2019-09-23 08:18:44 +0900

    [WebGL] ClayGL を試してみるテスト（組み込み関数編）（その２）

---
+++ cx20/6Cd9/index.js
@@ -0,0 +1,29 @@
+// forked from cx20's "[WebGL] ClayGL を試してみるテスト（組み込み関数編）" http://jsdo.it/cx20/WltV
+// forked from cx20's "[WebGL] ClayGL を試してみるテスト（VBO編）" http://jsdo.it/cx20/KVOj
+// forked from cx20's "[WebGL] QTEK を試してみるテスト（VBO編）" http://jsdo.it/cx20/ICwE
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+
+var app = clay.application.create('#main', {
+    init: function (app) {
+        // Create a orthographic camera
+        var camera = app.createCamera(null, null, 'perspective');
+        camera.position.set(0, 0, 4);
+        // Create geometry
+        var geometry = new clay.geometry.Plane();
+        geometry.generateTangents();
+        geometry.attributes.color.fromArray([
+            [1.0, 0.0, 0.0, 1.0], // v0
+            [0.0, 1.0, 0.0, 1.0], // v1
+            [0.0, 0.0, 1.0, 1.0], // v2
+            [1.0, 1.0, 0.0, 1.0]  // v3
+        ]);
+        var mesh = app.createMesh(geometry, {
+            // Use basic shader that only show color
+            shader: 'clay.basic'
+        });
+        // Enable vertex color in both vertex and fragment shader.
+        mesh.material.define('both', 'VERTEX_COLOR');
+    },
+
+    loop: function () {}
+});
---
+++ cx20/6Cd9/readme.txt
@@ -0,0 +1,16 @@
+[WebGL] ClayGL を試してみるテスト（組み込み関数編）（その２）
+
+ClayGL は pissang 氏による WebGL 対応の 3D 描画ライブラリです。
+
+＜対応した点＞
+・claygl.js で Plane() を使用して正方形を描くよう対応。
+・各頂点に色情報を設定するよう対応。
+
+＜参考＞
+■ pissang/claygl
+https://github.com/pissang/claygl
+■ ClayGL
+http://claygl.xyz/
+
+■ 各種 WebGL ライブラリで基本図形を表示してみる
+http://qiita.com/cx20/items/0fa19c96aa6470d98807
