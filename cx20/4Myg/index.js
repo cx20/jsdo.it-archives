ff9859c6251399084da140e0cbadb1a077f7d85f
cx20 <y-shimano@hondarer-soft.com>
2019-09-22 11:46:37 +0900

    [WebGL] Filament を試してみるテスト（その２）（調整中）

---
+++ cx20/4Myg/index.js
@@ -0,0 +1,103 @@
+// forked from cx20's "[WebGL] Filament を試してみるテスト（調整中）" http://jsdo.it/cx20/Kj5Y
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+
+class App {
+  constructor() {
+    this.canvas = document.getElementsByTagName('canvas')[0];
+    const engine = this.engine = Filament.Engine.create(this.canvas);
+    this.scene = engine.createScene();
+    this.triangle = Filament.EntityManager.get()
+      .create();
+    this.scene.addEntity(this.triangle);
+    // 正方形の座標データを用意
+    //             1.0 y 
+    //              ^  -1.0 
+    //              | / z
+    //              |/       x
+    // -1.0 -----------------> +1.0
+    //            / |
+    //      +1.0 /  |
+    //           -1.0
+    // 
+    //        [0]------[1]
+    //         |      / |
+    //         |    /   |
+    //         |  /     |
+    //        [2]------[3]
+    //
+    const TRIANGLE_POSITIONS = new Float32Array([
+      -0.5, 0.5, // v0
+       0.5, 0.5, // v1 
+      -0.5,-0.5, // v2
+       0.5,-0.5  // v3
+    ]);
+    const TRIANGLE_COLORS = new Uint32Array([
+      0x0000ff,
+      0x00ff00,
+      0xff0000,
+      0x00ffff
+    ]);
+    const VertexAttribute = Filament.VertexAttribute;
+    const AttributeType = Filament.VertexBuffer$AttributeType;
+    this.vb = Filament.VertexBuffer.Builder()
+      .vertexCount(4)
+      .bufferCount(2)
+      .attribute(VertexAttribute.POSITION, 0, AttributeType.FLOAT2, 0, 8)
+      .attribute(VertexAttribute.COLOR, 1, AttributeType.UBYTE4, 0, 4)
+      .normalized(VertexAttribute.COLOR)
+      .build(engine);
+    this.vb.setBufferAt(engine, 0, TRIANGLE_POSITIONS);
+    this.vb.setBufferAt(engine, 1, TRIANGLE_COLORS);
+    this.ib = Filament.IndexBuffer.Builder()
+      .indexCount(6)
+      .bufferType(Filament.IndexBuffer$IndexType.USHORT)
+      .build(engine);
+    this.ib.setBuffer(engine, new Uint16Array([
+      2, 0, 1, // v2-v0-v1
+      2, 1, 3  // v2-v1-v3
+    ]));
+    //const mat = engine.createMaterial('triangle.filamat');
+    //const mat = engine.createMaterial('/assets/k/S/h/N/kShN2'); // triangle.filamat
+    const mat = engine.createMaterial('https://rawcdn.githack.com/google/filament/724e9abf960700201c05f50df80a60ddd3a1ce06/docs/webgl/triangle.filamat');
+    const matinst = mat.getDefaultInstance();
+    Filament.RenderableManager.Builder(1)
+    .boundingBox({
+      center: [-1, -1, -1],
+      halfExtent: [1, 1, 1]
+    })
+    .material(0, matinst)
+    .geometry(0, Filament.RenderableManager$PrimitiveType.TRIANGLES, this.vb, this.ib)
+    .build(engine, this.triangle);
+    this.swapChain = engine.createSwapChain();
+    this.renderer = engine.createRenderer();
+    this.camera = engine.createCamera();
+    this.view = engine.createView();
+    this.view.setCamera(this.camera);
+    this.view.setScene(this.scene);
+    this.view.setClearColor([1.0, 1.0, 1.0, 1.0]);
+    this.resize(); // adjust the initial viewport
+    this.render = this.render.bind(this);
+    this.resize = this.resize.bind(this);
+    window.addEventListener('resize', this.resize);
+    window.requestAnimationFrame(this.render);
+  }
+  render() {
+    // Render the frame.
+    this.renderer.render(this.swapChain, this.view);
+    window.requestAnimationFrame(this.render);
+  }
+  resize() {
+    const dpr = window.devicePixelRatio;
+    const width = this.canvas.width = window.innerWidth * dpr;
+    const height = this.canvas.height = window.innerHeight * dpr;
+    this.view.setViewport([0, 0, width, height]);
+    const aspect = width / height;
+    const Projection = Filament.Camera$Projection;
+    this.camera.setProjection(Projection.ORTHO, -aspect, aspect, -1, 1, 0, 1);
+  }
+}
+//Filament.init(['triangle.filamat'], () => {
+//Filament.init(['/assets/k/S/h/N/kShN2'], () => { // triangle.filamat
+Filament.init(['https://rawcdn.githack.com/google/filament/724e9abf960700201c05f50df80a60ddd3a1ce06/docs/webgl/triangle.filamat'], () => {
+  window.app = new App()
+});
---
+++ cx20/4Myg/readme.txt
@@ -0,0 +1,16 @@
+[WebGL] Filament を試してみるテスト（その２）（調整中）
+
+Filament は Google が開発中のオープンソースのリアルタイムレンダリングエンジンです。
+
+＜対応した点＞
+・頂点バッファで色を指定して四角形を描画するよう対応。
+
+＜対応できていない点＞
+・全体的に灰色っぽくなってしまっている。
+
+＜参考＞
+■ Filament Tutorial
+https://google.github.io/filament/webgl/tutorial_triangle.html
+https://github.com/google/filament/tree/master/docs/webgl
+https://github.com/google/filament/blob/master/web/docs/triangle.mat
+
