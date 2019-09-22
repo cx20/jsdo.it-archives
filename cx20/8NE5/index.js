2969bf90bf48853f3f9318c7ab5bf399d3fa5073
cx20 <y-shimano@hondarer-soft.com>
2019-09-23 08:35:18 +0900

    [WebGL] ClayGL で glTF 2.0 モデルを表示してみるテスト

---
+++ cx20/8NE5/index.js
@@ -0,0 +1,42 @@
+// forked from cx20's "[WebGL] ClayGL を試してみるテスト（組み込み関数編）（その４）" http://jsdo.it/cx20/g28w
+// forked from cx20's "[WebGL] ClayGL を試してみるテスト（組み込み関数編）（その３）" http://jsdo.it/cx20/czeo
+// forked from cx20's "[WebGL] ClayGL を試してみるテスト（組み込み関数編）（その２）" http://jsdo.it/cx20/6Cd9
+// forked from cx20's "[WebGL] ClayGL を試してみるテスト（組み込み関数編）" http://jsdo.it/cx20/WltV
+// forked from cx20's "[WebGL] ClayGL を試してみるテスト（VBO編）" http://jsdo.it/cx20/KVOj
+// forked from cx20's "[WebGL] QTEK を試してみるテスト（VBO編）" http://jsdo.it/cx20/ICwE
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+
+var url = "https://cdn.rawgit.com/KhronosGroup/glTF-WebGL-PBR/817404a4/models/Triangle/glTF/Triangle.gltf";
+
+var app = clay.application.create('#main', {
+    init: function (app) {
+        // Create camera
+        this._camera = app.createCamera([0, 2, 3], [0, 0, 0]);
+
+        // Create light
+        app.createDirectionalLight([0.0, -1.0, 0.5]);
+        app.createDirectionalLight([-1, -1, -1]);
+
+        // Use orbit control
+        this._control = new clay.plugin.OrbitControl({
+            // The target or orbit control. Usually is a camera.
+            target: this._camera,
+            // The HTMLDomElement where we need to addEventListener.
+            domElement: app.container,
+        });
+        this._control.autoRotateSpeed = 20;
+
+        // Load specified model. return a load promise to make sure the look will be start after model loaded.
+        return app.loadModel(url).then((function (result) {
+            this._gltfModel = result.rootNode;
+        }).bind(this));
+    },
+    loop: function (app) {
+        this._control.autoRotate = true;
+        this._control.update(app.frameTime);
+    }
+});
+
+window.onresize = function () {
+    app.resize()
+};
---
+++ cx20/8NE5/readme.txt
@@ -0,0 +1,7 @@
+[WebGL] ClayGL で glTF 2.0 モデルを表示してみるテスト
+
+＜参考＞
+■ pissang/claygl
+https://github.com/pissang/claygl
+■ ClayGL
+http://claygl.xyz/
