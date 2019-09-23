dd6f539131b6b408322e594031c8a05f06d37101
cx20 <y-shimano@hondarer-soft.com>
2019-09-23 12:18:44 +0900

    [WebGL] ClayGL で glTF 2.0 モデルを表示してみるテスト（その８改）（調整中）

---
+++ cx20/QFV2/index.js
@@ -0,0 +1,84 @@
+// forked from cx20's "[WebGL] ClayGL で glTF 2.0 モデルを表示してみるテスト（その８）（調整中）" http://jsdo.it/cx20/Eblx
+// forked from cx20's "[WebGL] ClayGL で glTF 2.0 モデルを表示してみるテスト（その７）（調整中）" http://jsdo.it/cx20/ogoH
+// forked from cx20's "[WebGL] ClayGL で glTF 2.0 モデルを表示してみるテスト（その６）（調整中）" http://jsdo.it/cx20/4GIJ
+// forked from cx20's "[WebGL] ClayGL で glTF 2.0 モデルを表示してみるテスト（その５）（調整中）" http://jsdo.it/cx20/2xCG
+// forked from cx20's "[WebGL] ClayGL で glTF 2.0 モデルを表示してみるテスト（その４）" http://jsdo.it/cx20/mz7B
+// forked from cx20's "[WebGL] ClayGL で glTF 2.0 モデルを表示してみるテスト（その３）" http://jsdo.it/cx20/mPEP
+// forked from cx20's "[WebGL] ClayGL で glTF 2.0 モデルを表示してみるテスト（その２）" http://jsdo.it/cx20/iuML
+// forked from cx20's "[WebGL] ClayGL で glTF 2.0 モデルを表示してみるテスト" http://jsdo.it/cx20/8NE5
+// forked from cx20's "[WebGL] ClayGL を試してみるテスト（組み込み関数編）（その４）" http://jsdo.it/cx20/g28w
+// forked from cx20's "[WebGL] ClayGL を試してみるテスト（組み込み関数編）（その３）" http://jsdo.it/cx20/czeo
+// forked from cx20's "[WebGL] ClayGL を試してみるテスト（組み込み関数編）（その２）" http://jsdo.it/cx20/6Cd9
+// forked from cx20's "[WebGL] ClayGL を試してみるテスト（組み込み関数編）" http://jsdo.it/cx20/WltV
+// forked from cx20's "[WebGL] ClayGL を試してみるテスト（VBO編）" http://jsdo.it/cx20/KVOj
+// forked from cx20's "[WebGL] QTEK を試してみるテスト（VBO編）" http://jsdo.it/cx20/ICwE
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+
+//var url = "https://cdn.rawgit.com/KhronosGroup/glTF-WebGL-PBR/817404a4/models/Triangle/glTF/Triangle.gltf";
+//var url = "https://cdn.rawgit.com/KhronosGroup/glTF-Sample-Models/7268f989/2.0/TextureSettingsTest/glTF/TextureSettingsTest.gltf";
+//var url = "https://cdn.rawgit.com/cx20/jsdo-static-contents/76dfc928/models/gltf/2.0/EmaSimpleSkin_blender/glTF/EmaSimpleSkin_blender.gltf";
+//var url = "https://cdn.rawgit.com/cx20/gltf-test/9fb5f39992bdd548e17fb18b256c41b14fb8840e/sampleModels/CesiumMilkTruck/glTF/CesiumMilkTruck.gltf";
+//var url = "http://jsrun.it/assets/U/j/F/1/UjF1i";
+//var url = "https://cdn.rawgit.com/KhronosGroup/glTF-Sample-Models/c89c1709fbfd67a11aa7e540ab4ecb795763b627/2.0/MetalRoughSpheres/glTF/MetalRoughSpheres.gltf";
+//var url = "https://raw.githubusercontent.com/shrekshao/minimal-gltf-loader/store-drone-model/glTFs/glTF_version_2/buster_drone/scene.gltf";
+var url = "https://cdn.rawgit.com/KhronosGroup/glTF-Blender-Exporter/0e23c773bf27dad67d2c25f060370d6fa012d87d/polly/project_polly.gltf";
+
+var app = clay.application.create('#main', {
+    graphic: {
+        // Enable shadow
+        shadow: true,
+        //tonemapping: true,
+        linear: true
+    },
+    init: function (app) {
+        // Create camera
+/*
+        this._camera = app.createCamera([0, 0.5, 2], [0, 0, 0]);
+*/
+        // Create light
+        //app.createDirectionalLight([1, 1, 1]);
+        app.createDirectionalLight([-1, -1, -1]);
+        app.createAmbientLight("#fff", 1.0);
+
+/*
+        // Skybox need a cubemap texture.
+        app.loadTextureCube({
+            px: 'http://jsrun.it/assets/K/A/Q/S/KAQS9.jpg',
+            nx: 'http://jsrun.it/assets/W/i/p/E/WipE5.jpg',
+            py: 'http://jsrun.it/assets/G/b/O/t/GbOtw.jpg',
+            ny: 'http://jsrun.it/assets/Y/H/v/c/YHvca.jpg',
+            pz: 'http://jsrun.it/assets/i/X/C/d/iXCdr.jpg',
+            nz: 'http://jsrun.it/assets/y/s/c/P/yscPf.jpg'
+        }).then(function (cubemap) {
+            var skybox = new clay.plugin.Skybox({
+                // Attach skybox to the scene.
+                scene: app.scene,
+                // Use the cubemap as environment
+                environmentMap: cubemap
+            });
+        });
+*/
+        // pisa.hdr
+        //app.createAmbientCubemapLight('http://jsrun.it/assets/w/O/a/o/wOaoO', 1, 0.3, 1).then(function (result) {
+        // uffizi-large.hdr
+        app.createAmbientCubemapLight('http://examples.claygl.xyz/assets/textures/hdr/uffizi-large.hdr', 1, 0.3, 1).then(function (result) {
+            var skybox = new clay.plugin.Skybox({
+                scene: app.scene,
+                environmentMap: result.specular.cubemap
+            });
+            // Use high lod to show the `rough` skybox
+            skybox.material.set('lod', 3);
+        });
+        // Load specified model. return a load promise to make sure the look will be start after model loaded.
+        return app.loadModel(url).then((function (result) {
+            this._gltfModel = result.rootNode;
+            app.scene.setMainCamera(result.cameras[1]);
+        }).bind(this));
+    },
+    loop: function (app) {
+    }
+});
+
+window.onresize = function () {
+    app.resize()
+};
---
+++ cx20/QFV2/readme.txt
@@ -0,0 +1,31 @@
+[WebGL] ClayGL で glTF 2.0 モデルを表示してみるテスト（その８改）（調整中）
+
+＜対応した点＞
+・ライブラリを最新化
+・環境マップを使用するよう対応。
+
+＜使用モデル＞
+■ glTF Project Polly
+https://www.youtube.com/watch?v=l7TB1O51X_M
+■ glTF-Blender-Exporter/polly/
+https://github.com/KhronosGroup/glTF-Blender-Exporter/tree/master/polly
+
+＜参考＞
+■ pissang/claygl
+https://github.com/pissang/claygl
+■ ClayGL
+http://claygl.xyz/
+■ Camera Animation in glTF Model - ClayGL Example
+http://examples.claygl.xyz/examples/basicGLTFCameraAnimation.html
+
+＜他ライブラリ比較＞
+■ [WebGL] three.js で glTF 2.0形式のデータを表示してみるテスト（その８改）
+http://jsdo.it/cx20/Cnr2
+■ [WebGL] Babylon.js + glTF2FileLoader を試してみるテスト（その８改）
+http://jsdo.it/cx20/m7JB
+■ [WebGL] ClayGL で glTF 2.0 モデルを表示してみるテスト（その８改）（調整中）
+http://jsdo.it/cx20/QFV2
+■ [WebGL] Hilo3d で glTF 2.0 モデルを表示してみるテスト（その８改）（調整中）
+http://jsdo.it/cx20/icRh
+■ [WebGL] CZPG.js で glTF 2.0 モデルを表示してみるテスト（その８改）（調整中）
+http://jsdo.it/cx20/6dru
