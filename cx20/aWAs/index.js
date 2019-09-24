bfa18f8c99e2bd2f039e7bbedad6da14e0238f55
cx20 <y-shimano@hondarer-soft.com>
2019-09-24 22:03:51 +0900

    [WebGL] Hilo3d を試してみるテスト（組み込み関数編）（その４）

---
+++ cx20/aWAs/index.js
@@ -0,0 +1,42 @@
+// forked from cx20's "[WebGL] Hilo3d を試してみるテスト（組み込み関数編）（その３）" http://jsdo.it/cx20/6Ujc
+// forked from cx20's "[WebGL] Hilo3d を試してみるテスト（組み込み関数編）（その２）" http://jsdo.it/cx20/O7Hi
+// forked from cx20's "[WebGL] Hilo3d を試してみるテスト（組み込み関数編）" http://jsdo.it/cx20/gKXr
+// forked from cx20's "[WebGL] Hilo3d を試してみるテスト" http://jsdo.it/cx20/gwMA
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+
+//var camera = new Hilo3d.PerspectiveCamera();
+var camera = new Hilo3d.PerspectiveCamera({
+    aspect: innerWidth / innerHeight,
+    far: 100,
+    near: 0.1,
+    z: 3
+});
+var stage = new Hilo3d.Stage({
+    container: document.getElementById('container'),
+    camera: camera,
+    clearColor: new Hilo3d.Color(1.0, 1.0, 1.0),
+    width: innerWidth,
+    height: innerHeight
+});
+
+var geometry = new Hilo3d.BoxGeometry();
+geometry.setAllRectUV([[0, 1], [1, 1], [1, 0], [0, 0]]);
+
+var mesh = new Hilo3d.Mesh({
+    geometry: geometry,
+    material: new Hilo3d.BasicMaterial({
+        diffuse:new Hilo3d.LazyTexture({
+            src:'../../assets/A/k/w/j/AkwjW.jpg'
+        })
+    }),
+    onUpdate: function() {
+       this.rotationX += 0.5;
+       this.rotationY += 0.5;
+       this.rotationZ += 0.5;
+    }
+});
+stage.addChild(mesh);
+
+var ticker = new Hilo3d.Ticker(60);
+ticker.addTick(stage);
+ticker.start(true);
---
+++ cx20/aWAs/readme.txt
@@ -0,0 +1,16 @@
+[WebGL] Hilo3d を試してみるテスト（組み込み関数編）（その４）
+
+Hilo3d は Alibaba Groupによって開発されたWebGLレンダリングエンジンです。
+
+＜対応した点＞
+・テクスチャ画像を立方体に貼り付けるよう対応
+
+＜参考＞
+■ hiloteam/Hilo3d
+https://github.com/hiloteam/Hilo3d
+
+■ Hilo3d API Reference
+https://hiloteam.github.io/Hilo3d/docs/index.html
+
+■ 各種 WebGL ライブラリによる基本サンプル一覧
+http://qiita.com/cx20/items/0fa19c96aa6470d98807
