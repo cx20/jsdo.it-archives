e036e6eb67fce25e715ab142711d539c2b9d718b
cx20 <y-shimano@hondarer-soft.com>
2019-09-24 21:51:58 +0900

    [WebGL] Hilo3d を試してみるテスト（VBO編）

---
+++ cx20/gwMA/index.js
@@ -0,0 +1,33 @@
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+
+var camera = new Hilo3d.PerspectiveCamera();
+var stage = new Hilo3d.Stage({
+    container: document.getElementById('container'),
+    camera: camera,
+    clearColor: new Hilo3d.Color(1.0, 1.0, 1.0),
+    width: innerWidth,
+    height: innerHeight
+});
+var positions = new Float32Array([
+    -0.5, -0.5, 0.0,
+     0.5, -0.5, 0.0,
+     0.0,  0.5, 0.0
+]);
+var geometry = new Hilo3d.Geometry({
+    vertices:new Hilo3d.GeometryData(positions, 3)
+});
+var mesh = new Hilo3d.Mesh({
+    geometry: geometry,
+    material: new Hilo3d.ShaderMaterial({
+        attributes:{
+            a_position: 'POSITION',
+        },
+        fs: document.getElementById('fs').textContent,
+        vs: document.getElementById('vs').textContent
+    })
+});
+stage.addChild(mesh);
+
+var ticker = new Hilo3d.Ticker(60);
+ticker.addTick(stage);
+ticker.start(true);
---
+++ cx20/gwMA/readme.txt
@@ -0,0 +1,17 @@
+[WebGL] Hilo3d を試してみるテスト（VBO編）
+
+Hilo3d は Alibaba Groupによって開発されたWebGLレンダリングエンジンです。
+
+＜対応した点＞
+・Hilo3d.ShaderMaterial() により自前のシェーダを使うよう対応
+
+＜参考＞
+■ hiloteam/Hilo3d
+https://github.com/hiloteam/Hilo3d
+
+■ Hilo3d API Reference
+https://hiloteam.github.io/Hilo3d/docs/index.html
+
+■ 各種 WebGL ライブラリによる基本サンプル一覧
+http://qiita.com/cx20/items/0fa19c96aa6470d98807
+
