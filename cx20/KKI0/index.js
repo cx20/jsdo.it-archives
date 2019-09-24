d5e057ce3f73971a3c45428a1d3060dd5c9cd8b3
cx20 <y-shimano@hondarer-soft.com>
2019-09-25 01:42:47 +0900

    [WebGL] X3DOM を試してみるテスト（その３）（VBO編）

---
+++ cx20/KKI0/index.js
@@ -0,0 +1,4 @@
+// forked from cx20's "[WebGL] X3DOM を試してみるテスト（その２）" http://jsdo.it/cx20/oKuB
+// forked from cx20's "[WebGL] X3DOM を試してみるテスト" http://jsdo.it/cx20/S1FS
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+//nothing
