aae01605c944e8908702a89c11752ffb50703bb1
cx20 <y-shimano@hondarer-soft.com>
2019-09-26 23:54:40 +0900

    [WebGL] xeogl を試してみるテスト（その２）

---
+++ cx20/syca/index.js
@@ -0,0 +1,61 @@
+// forked from cx20's "[WebGL] xeogl を試してみるテスト" http://jsdo.it/cx20/A7bW
+// forked from cx20's "[WebGL] xeoEngine を試してみるテスト" http://jsdo.it/cx20/MKkV
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+
+let geometry = new xeogl.Geometry({
+    primitive: "triangle-strip",
+    // Square data
+    //             1.0 y 
+    //              ^  -1.0 
+    //              | / z
+    //              |/       x
+    // -1.0 -----------------> +1.0
+    //            / |
+    //      +1.0 /  |
+    //           -1.0
+    // 
+    //        [0]------[1]
+    //         |     /  |
+    //         |   /    |
+    //         | /      |
+    //        [2]------[3]
+    //
+    positions: [
+       -0.5, 0.5, 0.0, // v0
+        0.5, 0.5, 0.0, // v1 
+       -0.5,-0.5, 0.0, // v2
+        0.5,-0.5, 0.0  // v3
+    ],
+    normals: [
+        0.0, 0.0, 1.0, // v0
+        0.0, 0.0, 1.0, // v1
+        0.0, 0.0, 1.0, // v2
+        0.0, 0.0, 1.0  // v3
+    ],
+    colors: [
+        1.0, 0.0, 0.0, 1.0, // v0
+        0.0, 1.0, 0.0, 1.0, // v1
+        0.0, 0.0, 1.0, 1.0, // v2
+        1.0, 1.0, 0.0, 1.0  // v3
+    ],
+    indices: [
+        2, 0, 1, // v2-v0-v1
+        2, 1, 3, // v2-v1-v3
+    ]
+});
+
+let material = new xeogl.PhongMaterial({
+    ambient: [1.0, 1.0, 1.0],
+    diffuse: [1, 1, 1]
+});
+
+let ambientLight = new xeogl.AmbientLight({
+    color: [1.0, 1.0, 1.0]
+});
+
+let mesh = new xeogl.Mesh({
+    geometry: geometry,
+    material: material
+});
+
+mesh.scene.camera.view.eye = [0.0, 0.0, 2.0];
---
+++ cx20/syca/readme.txt
@@ -0,0 +1,18 @@
+[WebGL] xeogl を試してみるテスト（その２）
+
+xeoEngine はバージョンアップに伴い xeogl に名称変更されたようです。
+
+＜対応した点＞
+・四角形の頂点バッファに個別に色を設定するよう対応。
+・カスタムシェーダを用いるよう対応。
+　組み込みシェーダだと白色背景色を設定できなかった為、カスタムシェーダを用いるよう対応。
+
+＜参考＞
+■ xeogl - WebGL-based 3D visualization engine
+http://xeogl.org/
+
+■ xeolabs/xeogl
+https://github.com/xeolabs/xeogl
+
+■ 各種 WebGL ライブラリで基本図形を表示してみる - Qiita
+http://qiita.com/cx20/items/0fa19c96aa6470d98807
