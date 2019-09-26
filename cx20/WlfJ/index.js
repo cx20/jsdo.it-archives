5c475619b638bebdac45ee7d73ce05c2eb905ffa
cx20 <y-shimano@hondarer-soft.com>
2019-09-27 00:00:47 +0900

    [WebGL] xeogl を試してみるテスト（その４）

---
+++ cx20/WlfJ/index.js
@@ -0,0 +1,157 @@
+// forked from cx20's "[WebGL] xeogl を試してみるテスト（その３）" http://jsdo.it/cx20/W5Mu
+// forked from cx20's "[WebGL] xeogl を試してみるテスト（その２）" http://jsdo.it/cx20/syca
+// forked from cx20's "[WebGL] xeogl を試してみるテスト" http://jsdo.it/cx20/A7bW
+// forked from cx20's "[WebGL] xeoEngine を試してみるテスト" http://jsdo.it/cx20/MKkV
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+
+let geometry = new xeogl.Geometry({
+    primitive: "triangles",
+    // Cube data
+    //             1.0 y 
+    //              ^  -1.0 
+    //              | / z
+    //              |/       x
+    // -1.0 -----------------> +1.0
+    //            / |
+    //      +1.0 /  |
+    //           -1.0
+    // 
+    //         [7]------[6]
+    //        / |      / |
+    //      [3]------[2] |
+    //       |  |     |  |
+    //       | [4]----|-[5]
+    //       |/       |/
+    //      [0]------[1]
+    //
+    positions: [
+        // Front face
+        -0.5, -0.5,  0.5, // v0
+         0.5, -0.5,  0.5, // v1
+         0.5,  0.5,  0.5, // v2
+        -0.5,  0.5,  0.5, // v3
+        // Back face
+        -0.5, -0.5, -0.5, // v4
+        -0.5,  0.5, -0.5, // v7
+         0.5,  0.5, -0.5, // v6
+         0.5, -0.5, -0.5, // v5
+        // Top face
+        -0.5,  0.5, -0.5, // v7
+        -0.5,  0.5,  0.5, // v3
+         0.5,  0.5,  0.5, // v2
+         0.5,  0.5, -0.5, // v6
+        // Bottom face
+        -0.5, -0.5, -0.5, // v4
+         0.5, -0.5, -0.5, // v5
+         0.5, -0.5,  0.5, // v1
+        -0.5, -0.5,  0.5, // v0
+         // Right face
+         0.5, -0.5, -0.5, // v5
+         0.5,  0.5, -0.5, // v6
+         0.5,  0.5,  0.5, // v2
+         0.5, -0.5,  0.5, // v1
+         // Left face
+        -0.5, -0.5, -0.5, // v4
+        -0.5, -0.5,  0.5, // v0
+        -0.5,  0.5,  0.5, // v3
+        -0.5,  0.5, -0.5  // v7
+    ],
+
+    normals: [
+        // Front face
+        0, 0, 1,  // v0
+        0, 0, 1,  // v1
+        0, 0, 1,  // v2
+        0, 0, 1,  // v3
+        // Back face
+        0, 0, -1, // v4
+        0, 0, -1, // v5
+        0, 0, -1, // v6
+        0, 0, -1, // v7
+        // Top face
+        0, 1, 0,  // v2
+        0, 1, 0,  // v3
+        0, 1, 0,  // v7
+        0, 1, 0,  // v6
+        // Bottom face
+        0, -1, 0, // v0
+        0, -1, 0, // v1
+        0, -1, 0, // v5
+        0, -1, 0, // v4
+         // Right face
+        1, 0, 0,  // v1
+        1, 0, 0,  // v2
+        1, 0, 0,  // v6
+        1, 0, 0,  // v5
+         // Left face
+        -1, 0, 0, // v0
+        -1, 0, 0, // v3
+        -1, 0, 0, // v7
+        -1, 0, 0, // v4
+    ],
+    // UV coords
+    uv: [
+        // Front face
+        1, 0,
+        0, 0,
+        0, 1,
+        1, 1,
+        // Back face
+        1, 0,
+        0, 0,
+        0, 1,
+        1, 1,
+        // Top face
+        1, 0,
+        0, 0,
+        0, 1,
+        1, 1,
+        // Bottom face
+        1, 0,
+        0, 0,
+        0, 1,
+        1, 1,
+        // Right face
+        1, 0,
+        0, 0,
+        0, 1,
+        1, 1,
+        // Left face
+        1, 0,
+        0, 0,
+        0, 1,
+        1, 1
+    ],
+    indices: [
+         0,  1,  2,    0,  2 , 3,  // Front face
+         4,  5,  6,    4,  6 , 7,  // Back face
+         8,  9, 10,    8, 10, 11,  // Top face
+        12, 13, 14,   12, 14, 15,  // Bottom face
+        16, 17, 18,   16, 18, 19,  // Right face
+        20, 21, 22,   20, 22, 23   // Left face
+    ]
+});
+
+let material = new xeogl.PhongMaterial({
+    ambient: [1.0, 1.0, 1.0],
+    diffuse: [1, 1, 1],
+    diffuseMap: new xeogl.Texture({
+        src: "../../assets/A/k/w/j/AkwjW.jpg", // frog.jpg
+    })
+});
+
+let ambientLight = new xeogl.AmbientLight({
+    color: [1.0, 1.0, 1.0]
+});
+
+let mesh = new xeogl.Mesh({
+    geometry: geometry,
+    material: material
+});
+
+mesh.scene.camera.view.eye = [0.0, 0.0, 2.0];
+
+mesh.scene.on("tick", function () {
+    mesh.scene.camera.orbitYaw(1.0);
+    mesh.scene.camera.orbitPitch(1.0);
+});
---
+++ cx20/WlfJ/readme.txt
@@ -0,0 +1,16 @@
+[WebGL] xeogl を試してみるテスト（その４）
+
+xeoEngine はバージョンアップに伴い xeogl に名称変更されたようです。
+
+＜対応した点＞
+・テクスチャ画像を立方体に貼り付けるよう対応
+
+＜参考＞
+■ xeogl - WebGL-based 3D visualization engine
+http://xeogl.org/
+
+■ xeolabs/xeogl
+https://github.com/xeolabs/xeogl
+
+■ 各種 WebGL ライブラリで基本図形を表示してみる - Qiita
+http://qiita.com/cx20/items/0fa19c96aa6470d98807
