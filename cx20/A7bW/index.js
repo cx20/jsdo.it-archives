9f8371075d7f4aef3b491d3664f30ef3fc095e61
cx20 <y-shimano@hondarer-soft.com>
2019-09-26 23:41:50 +0900

    [WebGL] xeogl を試してみるテスト

---
+++ cx20/A7bW/index.js
@@ -0,0 +1,40 @@
+// forked from cx20's "[WebGL] xeoEngine を試してみるテスト" http://jsdo.it/cx20/MKkV
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+
+let geometry = new xeogl.Geometry({
+    primitive: "triangles",
+    positions: [
+        0.0,  0.5, 0.0, // v0
+       -0.5, -0.5, 0.0, // v1
+        0.5, -0.5, 0.0  // v2
+    ],
+    normals: [
+        0.0,  0.0, 1.0, // v0
+        0.0,  0.0, 1.0, // v1
+        0.0,  0.0, 1.0, // v2
+    ],
+    colors: [
+        0.0, 0.0, 1.0, 1.0, // v0
+        0.0, 0.0, 1.0, 1.0, // v1
+        0.0, 0.0, 1.0, 1.0, // v2
+    ],
+    indices: [
+        0, 1, 2, // v0-v1-v2
+    ]
+});
+
+let material = new xeogl.PhongMaterial({
+    ambient: [1.0, 1.0, 1.0],
+    diffuse: [1, 1, 1]
+});
+
+let ambientLight = new xeogl.AmbientLight({
+    color: [1.0, 1.0, 1.0]
+});
+
+let mesh = new xeogl.Mesh({
+    geometry: geometry,
+    material: material
+});
+
+mesh.scene.camera.view.eye = [0.0, 0.0, 2.0];
---
+++ cx20/A7bW/readme.txt
@@ -0,0 +1,17 @@
+[WebGL] xeogl を試してみるテスト
+
+xeoEngine はバージョンアップに伴い xeogl に名称変更されたようです。
+
+＜対応した点＞
+・xeoEngine → xeogl に変更
+　XEO.～ → xeogl.～
+
+＜参考＞
+■ xeogl - WebGL-based 3D visualization engine
+http://xeogl.org/
+
+■ xeolabs/xeogl
+https://github.com/xeolabs/xeogl
+
+■ 各種 WebGL ライブラリで基本図形を表示してみる - Qiita
+http://qiita.com/cx20/items/0fa19c96aa6470d98807
