d7b2a9fdcfe3cbd04a21ae7d1945c34adf63ecf6
cx20 <y-shimano@hondarer-soft.com>
2019-09-30 02:09:43 +0900

    Three.js で火星をテラフォーミングしてみるテスト

---
+++ cx20/OKaP/index.js
@@ -0,0 +1,76 @@
+// forked from cx20's "Three.js で火星を表示させてみるテスト" http://jsdo.it/cx20/AFvB
+// forked from cx20's "Three.js で地球を表示させてみるテスト" http://jsdo.it/cx20/78Dn
+// forked from cx20's "Three.js で月を表示させてみるテスト" http://jsdo.it/cx20/vcVy
+// forked from cx20's "Three.js で冥王星を表示させてみるテスト" http://jsdo.it/cx20/tenj
+// forked from cx20's "Three.js で地球を回してみるテスト" http://jsdo.it/cx20/tv0T
+var container;
+var camera;
+var scene;
+var geometry;
+var material;
+var mars;
+var cloud;
+var renderer;
+var light;
+var light2;
+
+init();
+animate();
+
+function init() {
+    container = document.getElementById('container');
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+    camera.position.z = 400;
+
+    scene = new THREE.Scene();
+    light = new THREE.PointLight( new THREE.Color(0x808080), 3);
+    light2 = new THREE.PointLight( new THREE.Color(0x101010), 2);
+    light.position.set(0, -100, 1000);
+    light2.position.set(50, 50, 1000);
+    scene.add(light);
+    scene.add(light2);
+    geometry = new THREE.SphereGeometry(140, 30, 30);
+    //var planetTexture = THREE.ImageUtils.loadTexture('../../assets/K/C/E/D/KCEDg.jpg'); // mars_atmos_1024.jpg
+    //var planetTexture = THREE.ImageUtils.loadTexture('../../assets/M/y/f/I/MyfIJ.jpg'); // mars_ocean_1024_001.jpg
+    //var planetTexture = THREE.ImageUtils.loadTexture('../../assets/Q/Z/O/f/QZOf3.jpg'); // mars_ocean_1024_002.jpg
+    //var planetTexture = THREE.ImageUtils.loadTexture('../../assets/I/f/x/I/IfxIP.jpg'); // mars_ocean_1024_003.jpg
+    //var planetTexture = THREE.ImageUtils.loadTexture('../../assets/A/D/5/D/AD5Dj.jpg'); // mars_ocean_1024_004.jpg
+    var planetTexture = THREE.ImageUtils.loadTexture('../../assets/e/0/L/P/e0LPo.jpg'); // mars_ocean_1024_005.jpg
+    var normalTexture = THREE.ImageUtils.loadTexture('../../assets/k/u/S/Y/kuSYW.jpg'); // mars_normal_1024.jpg
+    //var specularTexture = THREE.ImageUtils.loadTexture('../../assets/w/N/j/h/wNjh6.jpg'); // mars_specular_1024.jpg
+    material = new THREE.MeshPhongMaterial({
+        map: planetTexture,
+        normalMap: normalTexture,
+        //specularMap: specularTexture,
+        color: 0xffffff,
+        specular: 0x111111,
+        shininess: 20
+    });
+    mars = new THREE.Mesh(geometry, material);
+    scene.add(mars);
+
+    var cloudsTexture = THREE.ImageUtils.loadTexture('../../assets/n/l/9/m/nl9m8.png'); // earth_clouds_1024.png
+    var materialClouds = new THREE.MeshLambertMaterial({
+        color: 0xffffff,
+        map: cloudsTexture,
+        transparent: true
+    });
+    cloud = new THREE.Mesh(geometry, materialClouds);
+    cloud.scale.set(1.010, 1.010, 1.010);
+    scene.add(cloud);
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    container.appendChild(renderer.domElement);
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    render();
+}
+
+function render() {
+    mars.rotation.y += 0.005;
+    cloud.rotation.y += 0.006;
+    renderer.render(scene, camera);
+}
---
+++ cx20/OKaP/readme.txt
@@ -0,0 +1,27 @@
+﻿Three.js で火星をテラフォーミングしてみるテスト
+
+＜対応した点＞
+・火星に海と緑地と雲を追加
+・海と緑地は、Height Map の標高データを参考にテクスチャ画像を作成（動的ではなく別途作成）
+
+＜対応できなかった点＞
+・シェーダを用いて動的に海を表示したかったがスキルが足らず挫折。orz
+
+＜参考＞
+■ 火星表面の地図
+http://www.geocities.jp/planetnekonta/map.html
+
+■ テラフォーミングされた火星 ～｢宇宙戦艦ヤマト2119｣の場合 MASA Planetary Log
+http://masamich.at.webry.info/201205/article_1.html
+
+■ Mars | 3D Resources (Beta) - NASA
+http://nasa3d.arc.nasa.gov/detail/mar0kuu2
+
+■ Is it possible to generate a high-poly spherical model from a mercator-projection heightmap_ - Unity Answers
+http://answers.unity3d.com/questions/313232/is-it-possible-to-generate-a-high-poly-spherical-m.html
+
+■ なるほど！火星に水が流れているのがよく分かる写真が話題に｜面白ニュース 秒刊SUNDAY
+http://www.yukawanet.com/archives/4945121.html
+
+■ [GLSL] テクスチャを合成してみるテスト（その２）
+http://jsdo.it/cx20/U2QP
