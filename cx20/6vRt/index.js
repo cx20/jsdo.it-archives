4ab4e793eb215bcfed46f37459ac8efb8f1dd00f
cx20 <y-shimano@hondarer-soft.com>
2019-09-30 01:00:55 +0900

    [WebGL] three.js で glTF 2.0形式のデータを表示してみるテスト（その３改）

---
+++ cx20/6vRt/index.js
@@ -0,0 +1,93 @@
+// forked from cx20's "[WebGL] three.js で glTF 2.0形式のデータを表示してみるテスト（その３改）（修正前）" http://jsdo.it/cx20/yfvq
+// forked from cx20's "[WebGL] three.js で glTF 2.0形式のデータを表示してみるテスト（その３）（調整中）" http://jsdo.it/cx20/q0cx
+// forked from cx20's "[WebGL] three.js で glTF 2.0形式のデータを表示してみるテスト（その２）" http://jsdo.it/cx20/K0k6
+// forked from cx20's "[WebGL] three.js で glTF 2.0形式のデータを表示してみるテスト" http://jsdo.it/cx20/gMdU
+// forked from cx20's "three.js で glTF 形式のデータを表示してみるテスト（その１）" http://jsdo.it/cx20/2qm8N
+// forked from cx20's "three.js で OBJ 形式のデータを表示してみるテスト（その１）" http://jsdo.it/cx20/wGMY
+// forked from cx20's "three.js で Blender のデータを表示してみるテスト" http://jsdo.it/cx20/2CXI
+// forked from 【WebGL特集】第4回：Blenderでモデル出力 http://mox-motion.com/blog/webgl04-2/
+
+var gltf = null;
+var mixer = null;
+var clock = new THREE.Clock();
+
+init();
+animate();
+  
+function init() {
+    width = window.innerWidth;
+    height = window.innerHeight;
+    
+    scene = new THREE.Scene();
+    
+    var ambient = new THREE.AmbientLight( 0x101030 );
+    scene.add( ambient );
+
+    var directionalLight = new THREE.DirectionalLight( 0xffeedd );
+    directionalLight.position.set( 0, 0, 1 );
+    scene.add( directionalLight );
+
+    var directionalLight2 = new THREE.DirectionalLight( 0xffeedd );
+    directionalLight2.position.set( 0, 5, -5 );
+    scene.add( directionalLight2 );
+    
+    camera = new THREE.PerspectiveCamera( 75, width / height, 1, 2000 );
+    camera.position.set(2, 2, 3);
+
+    var manager = new THREE.LoadingManager();
+    manager.onProgress = function ( item, loaded, total ) {
+        console.log( item, loaded, total );
+    };
+
+    var loader = new THREE.GLTF2Loader();
+    loader.setCrossOrigin( 'anonymous' ); // r84 以降は明示的に setCrossOrigin() を指定する必要がある
+
+    var scale = 1.0;
+    //var url = "https://cdn.rawgit.com/cx20/jsdo-static-contents/f21b9b7a/models/gltf/2.0/EmaSimpleSkin_blender/glTF/EmaSimpleSkin_blender.gltf";
+    var url = "https://cdn.rawgit.com/cx20/jsdo-static-contents/76dfc928/models/gltf/2.0/EmaSimpleSkin_blender/glTF/EmaSimpleSkin_blender.gltf";
+    
+    loader.load(url, function (data) {
+        gltf = data;
+        var object = gltf.scene;
+        object.scale.set(scale, scale, scale);
+        //object.position.y -= 10;
+
+        var animations = gltf.animations;
+        if ( animations && animations.length ) {
+            mixer = new THREE.AnimationMixer( object );
+            for ( var i = 0; i < animations.length; i ++ ) {
+                var animation = animations[ i ];
+                mixer.clipAction( animation ).play();
+            }
+        }
+        scene.add(object);
+    });
+
+    var axis = new THREE.AxisHelper(1000);   
+    scene.add(axis);
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setClearColor( 0xaaaaaa );
+
+    controls = new THREE.OrbitControls( camera, renderer.domElement );
+    controls.userPan = false;
+    controls.userPanSpeed = 0.0;
+    controls.maxDistance = 5000.0;
+    controls.maxPolarAngle = Math.PI * 0.495;
+    //controls.autoRotate = true;
+    controls.autoRotateSpeed = -10.0;
+
+    renderer.setSize( width, height );
+    document.body.appendChild( renderer.domElement );
+}
+
+function animate() {
+    requestAnimationFrame( animate );
+    if (mixer) mixer.update(clock.getDelta());
+    controls.update();
+    render();
+}
+
+function render() {
+    renderer.render( scene, camera );
+}
---
+++ cx20/6vRt/readme.txt
@@ -0,0 +1,27 @@
+﻿[WebGL] three.js で glTF 2.0形式のデータを表示してみるテスト（その３改）（修正後）
+
+＜対応した点＞
+・最新版のBlender glTF 2.0 Exporter を使用して変換した glTF 2.0 ファイルを表示するよう対応。
+　正しい方向に曲がることを確認。
+
+＜参考＞
+■ The COLLADA file created in Maya can not be correctly converted to glTF 2.0
+https://github.com/KhronosGroup/glTF-Blender-Exporter/issues/22
+
+＜他ライブラリ比較＞
+■ [WebGL] three.js で glTF 2.0形式のデータを表示してみるテスト（その３改）（修正後）
+http://jsdo.it/cx20/6vRt
+■ [WebGL] Babylon.js + glTF2FileLoader を試してみるテスト（その３改）（修正後）
+http://jsdo.it/cx20/EGIL
+■ [WebGL] Cesium.js で glTF 2.0形式のデータを表示してみるテスト（その３改）（調整中）
+http://jsdo.it/cx20/oq3E
+■ [WebGL] Grimoire.js で glTF 2.0形式のデータを表示してみるテスト（その３改）（調整中）
+http://jsdo.it/cx20/2hwP
+■ [WebGL] ClayGL で glTF 2.0 モデルを表示してみるテスト（その３改）
+http://jsdo.it/cx20/IbwT
+■ [WebGL] Hilo3d で glTF 2.0 モデルを表示してみるテスト（その３改）（調整中）
+http://jsdo.it/cx20/M7Ei
+■ [WebGL] PlayCanvas で glTF 2.0 モデルを表示してみるテスト（その３改）（調整中）
+http://jsdo.it/cx20/M14q
+■ [WebGL] GLBoost で glTF 2.0 モデルを表示してみるテスト（その３改）（調整中）
+http://jsdo.it/cx20/2dkm
