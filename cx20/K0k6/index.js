0a720d117c58ff394cbd3f411fb61a55ef5715c0
cx20 <y-shimano@hondarer-soft.com>
2019-09-30 00:59:44 +0900

    [WebGL] three.js で glTF 2.0形式のデータを表示してみるテスト

---
+++ cx20/K0k6/index.js
@@ -0,0 +1,91 @@
+// forked from cx20's "[WebGL] three.js で glTF 2.0形式のデータを表示してみるテスト" http://jsdo.it/cx20/gMdU
+// forked from cx20's "three.js で glTF 形式のデータを表示してみるテスト（その１）" http://jsdo.it/cx20/2qm8N
+// forked from cx20's "three.js で OBJ 形式のデータを表示してみるテスト（その１）" http://jsdo.it/cx20/wGMY
+// forked from cx20's "three.js で Blender のデータを表示してみるテスト" http://jsdo.it/cx20/2CXI
+// forked from 【WebGL特集】第4回：Blenderでモデル出力 http://mox-motion.com/blog/webgl04-2/
+
+var gltf = null;
+var mixer = null;
+
+init();
+animate();
+  
+function init() {
+    width = window.innerWidth;
+    height = window.innerHeight;
+    
+    scene = new THREE.Scene();
+    
+    var ambient = new THREE.AmbientLight( 0x101030 );
+    scene.add( ambient );
+
+    var directionalLight = new THREE.DirectionalLight( 0xffeedd );
+    directionalLight.position.set( 0, 0, 1 );
+    scene.add( directionalLight );
+
+    var directionalLight2 = new THREE.DirectionalLight( 0xffeedd );
+    directionalLight2.position.set( 0, 5, -5 );
+    scene.add( directionalLight2 );
+    
+    camera = new THREE.PerspectiveCamera( 75, width / height, 1, 2000 );
+    camera.position.set(0, 1, 10);
+
+    var manager = new THREE.LoadingManager();
+    manager.onProgress = function ( item, loaded, total ) {
+        console.log( item, loaded, total );
+    };
+
+    var loader = new THREE.GLTF2Loader();
+    loader.setCrossOrigin( 'anonymous' ); // r84 以降は明示的に setCrossOrigin() を指定する必要がある
+
+    var scale = 1.0;
+    //var url = "https://cdn.rawgit.com/KhronosGroup/glTF-WebGL-PBR/817404a4/models/Triangle/glTF/Triangle.gltf";
+    //var url = "https://cdn.rawgit.com/KhronosGroup/glTF-WebGL-PBR/817404a4/models/Avocado/glTF/Avocado.gltf";
+    //var url = "https://cdn.rawgit.com/KhronosGroup/glTF-WebGL-PBR/817404a4/models/AppleTree/glTF/AppleTree.gltf";
+    var url = "https://cdn.rawgit.com/KhronosGroup/glTF-Sample-Models/7268f989/2.0/TextureSettingsTest/glTF/TextureSettingsTest.gltf";
+    
+    loader.load(url, function (data) {
+        gltf = data;
+        var object = gltf.scene;
+        object.scale.set(scale, scale, scale);
+        //object.position.y -= 10;
+
+        var animations = gltf.animations;
+        if ( animations && animations.length ) {
+            mixer = new THREE.AnimationMixer( object );
+            for ( var i = 0; i < animations.length; i ++ ) {
+                var animation = animations[ i ];
+                mixer.clipAction( animation ).play();
+            }
+        }
+        scene.add(object);
+    });
+
+    var axis = new THREE.AxisHelper(1000);   
+    scene.add(axis);
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setClearColor( 0x000000 );
+
+    controls = new THREE.OrbitControls( camera, renderer.domElement );
+    controls.userPan = false;
+    controls.userPanSpeed = 0.0;
+    controls.maxDistance = 5000.0;
+    controls.maxPolarAngle = Math.PI * 0.495;
+    controls.autoRotate = true;
+    controls.autoRotateSpeed = -10.0;
+
+    renderer.setSize( width, height );
+    document.body.appendChild( renderer.domElement );
+}
+
+function animate() {
+    requestAnimationFrame( animate );
+    if (mixer) mixer.update(clock.getDelta());
+    controls.update();
+    render();
+}
+
+function render() {
+    renderer.render( scene, camera );
+}
---
+++ cx20/K0k6/readme.txt
@@ -0,0 +1,5 @@
+﻿[WebGL] three.js で glTF 2.0形式のデータを表示してみるテスト（その２）
+
+＜参考＞
+■ Texture Settings Test
+https://github.com/KhronosGroup/glTF-Sample-Models/tree/master/2.0/TextureSettingsTest
