feb92c5105a2e94518e2491f150040bc1da88232
cx20 <y-shimano@hondarer-soft.com>
2019-10-12 00:52:55 +0900

    [簡易版] WebGL で型付き配列を操作してみるテスト（その２）

--- cx20/wF83/index.js
+++ cx20/wF83/index.js
@@ -65,9 +65,12 @@ var data = [];
 var color = [];
 var colorBuffers;
 var colorBuffersTemp;
+var SCALE = window.innerHeight / 465;
 
 function initWebGL() {
     c = document.getElementById("c");
+    c.width = window.innerHeight;
+    c.height = window.innerHeight;
     gl = c.getContext("experimental-webgl");
 }
 
@@ -87,6 +90,7 @@ function initShaders() {
     gl.useProgram(p);
     aLoc[0] = gl.getAttribLocation(p, "position");
     aLoc[1] = gl.getAttribLocation(p, "color");
+    uLoc[0] = gl.getUniformLocation(p, "scale");
     gl.enableVertexAttribArray(aLoc[0]);
     gl.enableVertexAttribArray(aLoc[1]);
 }
@@ -142,6 +146,8 @@ function render() {
     
     gl.bufferData(gl.ARRAY_BUFFER, colorBuffers, gl.STATIC_DRAW);
     gl.vertexAttribPointer(aLoc[1], 4, gl.FLOAT, false, 0, 0);
+
+    gl.uniform1f(uLoc[0], SCALE);
 
     gl.drawArrays(gl.POINTS, 0, data.length / 3);
     gl.flush();
