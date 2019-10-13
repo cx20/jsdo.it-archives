ea76105def8320a3ba62e30cb207d18ac5c4e221
cx20 <y-shimano@hondarer-soft.com>
2019-10-13 19:20:07 +0900

    [WebGL] Perlin noise を用いて頂点データを加工してみるテスト（その２）

---
+++ cx20/8SAg/index.js
@@ -0,0 +1,141 @@
+// forked from cx20's "[WebGL] Perlin noise を用いて頂点データを加工してみるテスト" http://jsdo.it/cx20/adVd
+// forked from cx20's "[簡易版] WebGL で球体を描いてみるテスト（その２）" http://jsdo.it/cx20/iFeA
+// forked from cx20's "[簡易版] WebGL で球体を描いてみるテスト" http://jsdo.it/cx20/gIb0
+// forked from cx20's "[簡易版] WebGL でリサージュ図形を描いてみるテスト" http://jsdo.it/cx20/9Dru
+// forked from cx20's "[簡易版] WebGL で円を角度をつけて回転させてみるテスト" http://jsdo.it/cx20/5vCG
+// forked from cx20's "[簡易版] WebGL で円を描いてみるテスト" http://jsdo.it/cx20/dz02
+// forked from cx20's "[簡易版] WebGL でサインカーブを描いてみるテスト" http://jsdo.it/cx20/fPok
+// forked from cx20's "[簡易版] WebGL で点をプロットしてみるテスト" http://jsdo.it/cx20/puXG
+// forked from cx20's "[簡易版] WebGL で四角形を描いてみるテスト" http://jsdo.it/cx20/vwnxi
+// forked from cx20's "[簡易版 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+
+var c, gl;
+var aLoc = [];
+var uLoc = [];
+
+function initWebGL() {
+    c = document.getElementById("c");
+    gl = c.getContext("experimental-webgl");
+}
+
+function initShaders() {
+    var p = gl.createProgram();
+    var vs = gl.createShader(gl.VERTEX_SHADER);
+    var fs = gl.createShader(gl.FRAGMENT_SHADER);
+    var v = document.getElementById("vs").textContent;
+    var f = document.getElementById("fs").textContent;
+    gl.shaderSource(vs, v);
+    gl.shaderSource(fs, f);
+    gl.compileShader(vs);
+    gl.compileShader(fs);
+    console.log(gl.getShaderInfoLog(vs));
+    console.log(gl.getShaderInfoLog(fs));
+    gl.attachShader(p, vs);
+    gl.attachShader(p, fs);
+    gl.linkProgram(p);
+    gl.useProgram(p);
+    aLoc[0] = gl.getAttribLocation(p, "position");
+    aLoc[1] = gl.getAttribLocation(p, "color");
+    uLoc[0] = gl.getUniformLocation(p, 'matAxisX');
+    uLoc[1] = gl.getUniformLocation(p, 'matAxisY');
+    uLoc[2] = gl.getUniformLocation(p, 'time');
+    gl.enableVertexAttribArray(aLoc[0]);
+    gl.enableVertexAttribArray(aLoc[1]);
+}
+
+function draw() {
+    // 正弦波×余弦波の座標データを用意
+    //             1.0 y 
+    //              ^  -1.0 
+    //              | / z
+    //              |/       x
+    // -1.0 -----------------> +1.0
+    //            / |
+    //      +1.0 /  |
+    //           -1.0
+    // 
+    //             [3]
+    //         [4]     [2]
+    //      [5]            [1]
+    //      *                *
+    //     [6]               [0]
+    //      *                *
+    //      [7]            [11]
+    //         [8]     [10]
+    //             [9]
+    //
+    var data = [];
+    var colors = [];
+    var latitudeBands = 36;
+    var longitudeBands = 36;
+    
+    // ■ WebGL Lesson 11 – spheres, rotation matrices, and mouse events
+    // http://learningwebgl.com/blog/?p=1253
+    
+    // 緯度
+    for (var latNumber = 0; latNumber <= latitudeBands; latNumber++) {
+        var theta = latNumber * Math.PI / latitudeBands;
+        var sinTheta = Math.sin(theta);
+        var cosTheta = Math.cos(theta);
+        // 経度
+        for (var longNumber = 0; longNumber <= longitudeBands; longNumber++) {
+            var phi = longNumber * 2 * Math.PI / longitudeBands;
+            var sinPhi = Math.sin(phi);
+            var cosPhi = Math.cos(phi);
+
+            var x = 0.5 * cosPhi * sinTheta;
+            var y = 0.5 * cosTheta;
+            var z = 0.5 * sinPhi * sinTheta;
+            data = data.concat([x, y, z]);
+            //colors = colors.concat([x, y, z, 1.0]);
+            colors = colors.concat([x+0.5, y+0.5, z+0.5, 1.0]);
+        }
+    }
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
+    gl.vertexAttribPointer(aLoc[0], 3, gl.FLOAT, false, 0, 0);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+    gl.vertexAttribPointer(aLoc[1], 4, gl.FLOAT, false, 0, 0);
+
+    var rad = 0;
+    var baseTime = +new Date;
+    var time = 0;
+    (function(){
+        rad += Math.PI * 1.0 / 180.0;
+        time = (+new Date - baseTime) / 1000;
+        gl.uniform1f(uLoc[2], time);
+
+        var c = Math.cos(rad);
+        var s = Math.sin(rad);
+
+        // 変換行列を用意
+        // x軸で回転
+        var matAxisX = [
+            1.0, 0.0, 0.0, 0.0,
+            0.0,   c,  -s, 0.0,
+            0.0,   s,   c, 0.0,
+            0.0, 0.0, 0.0, 1.0
+        ];
+        // y軸で回転
+        var matAxisY = [
+              c, 0.0,   s, 0.0,
+            0.0, 1.0, 0.0, 0.0,
+             -s, 0.0,   c, 0.0,
+            0.0, 0.0, 0.0, 1.0
+        ];
+        gl.uniformMatrix4fv(uLoc[0], false, new Float32Array(matAxisX));
+        gl.uniformMatrix4fv(uLoc[1], false, new Float32Array(matAxisY));
+
+        gl.drawArrays(gl.LINE_STRIP, 0, data.length / 3);
+        gl.drawArrays(gl.POINTS, 0, data.length / 3);
+        gl.flush();
+        requestAnimationFrame(arguments.callee);
+    })();
+}
+
+initWebGL();
+initShaders();
+draw();
---
+++ cx20/8SAg/readme.txt
@@ -0,0 +1,19 @@
+﻿[WebGL] Perlin noise を用いて頂点データを加工してみるテスト（その２）
+※ 初期化処理やエラー処理を省いた簡易版のコードになります。ご注意ください。
+
+＜対応した点＞
+・球体の頂点データにノイズを加えるよう変更
+　＜変更前＞
+　vec3 newPosition = position * snoise(position.xyz + time / 2.0) * 2.0;
+　＜変更後＞
+　vec3 newPosition = position + snoise(position.xyz + time / 2.0) * 0.1;
+
+＜参考＞
+■ WebGLでSimplex NoiseのGLSLを使ってグラフィックパターンを生成する - Qiita
+http://qiita.com/yuichiroharai/items/1eaf4ce7e542b11da9ac
+
+■ ashima/webgl-noise
+https://github.com/ashima/webgl-noise
+
+■ Vertex displacement with GLSL - Blog - Clicktorelease
+http://www.clicktorelease.com/blog/vertex-displacement-noise-3d-webgl-glsl-three-js
