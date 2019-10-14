e8d7da52a423415023d9a3a427b6277351dc1daa
cx20 <y-shimano@hondarer-soft.com>
2019-10-14 21:46:09 +0900

    [WebGL] Grimoire.js で球体にエフェクトをかけてみるテスト

---
+++ cx20/eRe9/index.js
@@ -0,0 +1,38 @@
+// forked from cx20's "[WebGL] Grimoire.js で立方体にエフェクトをかけてみるテスト" http://jsdo.it/cx20/4glF
+// forked from cx20's "[WebGL] Grimoire.js で render-quad を使ってシェーダを実行してみるテスト（その３）" http://jsdo.it/cx20/Ehzy
+// forked from cx20's "forked: [WebGL] Grimoire.js で render-quad を使ってシェーダを実行してみるテスト" http://jsdo.it/cx20/wVDH
+// forked from cx20's "[WebGL] Grimoire.js で render-quad を使ってシェーダを実行してみるテスト" http://jsdo.it/cx20/AtCh
+// forked from cx20's "[WebGL] Grimoire.js を試してみるテスト（その２）" http://jsdo.it/cx20/mjC6
+// forked from cx20's "[WebGL] Grimoire.js を試してみるテスト" http://jsdo.it/cx20/4ZEB
+// forked from cx20's "[WebGL] jThree を試してみるテスト" http://jsdo.it/cx20/Go5s
+// nothing
+
+var Vector3 = gr.lib.math.Vector3;
+var Quaternion = gr.lib.math.Quaternion;
+
+gr.registerComponent('Rotate', {
+  attributes: {
+    speed: {
+      default: '0.3',
+      converter: 'Number',
+    },
+  },
+  $mount: function () {
+    this.phi = 0;
+  },
+  $update: function () {
+    this.phi += this.getAttribute('speed');
+    // オイラー角による回転
+    //this.node.setAttribute('rotation', this.phi + ',' + this.phi + ',' + 0);
+    
+    // クォータニオンによる回転
+    var axis = new Vector3(0, 1, 0);
+    var angle = this.phi * Math.PI / 180;
+    var q = Quaternion.angleAxis(angle, axis);
+    this.node.setAttribute('rotation', q.normalize());
+  },
+});
+
+gr(function() {
+  var $$ = gr('#canvas');
+});
---
+++ cx20/eRe9/readme.txt
@@ -0,0 +1,18 @@
+﻿[WebGL] Grimoire.js で球体にエフェクトをかけてみるテスト
+
+＜対応した点＞
+・立方体を球体に変更
+・球体とエフェクトを合成するよう対応。
+
+＜参考＞
+■ Grimoire.jsでシェーダープログラミング入門1
+http://qiita.com/kyasbal_1994/items/cff1466719934f461ca8
+
+■ GLSL Sandbox #41190.0
+http://glslsandbox.com/e#41190.0
+
+■ GLSL Sandbox #37038.1
+http://glslsandbox.com/e#37038.1
+
+■ 20行以下のコードで楽しむ簡単GLSL
+http://qiita.com/naoyashiga/items/b8e1233367083fde6c63
