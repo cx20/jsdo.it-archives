300676cfcbd4f7d89d66e23ce486b48f5d4a96c7
cx20 <y-shimano@hondarer-soft.com>
2019-10-15 07:50:33 +0900

    [簡易版] WebGL で立方体に光を当ててみるテスト（フラットシェーディング編）

---
+++ cx20/gv10/index.js
@@ -0,0 +1,171 @@
+// forked from cx20's "[簡易版] WebGL で立方体に光を当ててみるテスト（調整中）" http://jsdo.it/cx20/q4Uk
+// forked from cx20's "[簡易版] WebGL で小惑星に光を当ててみるテスト" http://jsdo.it/cx20/yIKo
+// forked from cx20's "[簡易版] WebGL で小惑星をプロットしてみるテスト" http://jsdo.it/cx20/qSiE
+// forked from cx20's "[簡易版] WebGL で３次元関数をプロットしてみるテスト" http://jsdo.it/cx20/8H2X
+// forked from cx20's "[簡易版] WebGL で３次元リサージュ図形を描いてみるテスト" http://jsdo.it/cx20/yoSK
+// forked from cx20's "[簡易版] WebGL でリサージュ図形を描いてみるテスト" http://jsdo.it/cx20/9Dru
+// forked from cx20's "[簡易版] WebGL で円を角度をつけて回転させてみるテスト" http://jsdo.it/cx20/5vCG
+// forked from cx20's "[簡易版] WebGL で円を描いてみるテスト" http://jsdo.it/cx20/dz02
+// forked from cx20's "[簡易版] WebGL でサインカーブを描いてみるテスト" http://jsdo.it/cx20/fPok
+// forked from cx20's "[簡易版] WebGL で点をプロットしてみるテスト" http://jsdo.it/cx20/puXG
+// forked from cx20's "[簡易版] WebGL で四角形を描いてみるテスト" http://jsdo.it/cx20/vwnxi
+// forked from cx20's "[簡易版 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+
+var c, gl, ext;
+var stats;
+
+var aLoc = [];
+var uLoc = [];
+
+function initWebGL() {
+    c = document.getElementById("c");
+    gl = c.getContext("experimental-webgl");
+    ext = gl.getExtension('OES_standard_derivatives');
+
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+    //gl.enable(gl.CULL_FACE);
+    
+    // Stats
+    stats = new Stats();
+    // 左上に設定
+    stats.domElement.style.position = "fixed";
+    stats.domElement.style.left     = "5px";
+    stats.domElement.style.top      = "5px";
+    document.body.appendChild(stats.domElement);
+}
+
+function initShaders() {
+    var p = gl.createProgram();
+    var vs = gl.createShader(gl.VERTEX_SHADER);
+    var fs = gl.createShader(gl.FRAGMENT_SHADER);
+    var v = document.getElementById("vs").textContent;
+    var f = document.getElementById("fs").textContent;
+    gl.shaderSource(vs, v);
+    gl.shaderSource(fs, f);
+    gl.compileShader(vs);
+    gl.compileShader(fs);
+    gl.attachShader(p, vs);
+    gl.attachShader(p, fs);
+    gl.linkProgram(p);
+    gl.useProgram(p);
+    aLoc[0] = gl.getAttribLocation(p, "position");
+    uLoc[0] = gl.getUniformLocation(p, 'matAxisX');
+    uLoc[1] = gl.getUniformLocation(p, 'matAxisY');
+    uLoc[2] = gl.getUniformLocation(p, 'time');
+    gl.enableVertexAttribArray(aLoc[0]);
+}
+
+function draw() {
+    // 立方体の座標データを用意
+    //             1.0 y 
+    //              ^  -1.0 
+    //              | / z
+    //              |/       x
+    // -1.0 -----------------> +1.0
+    //            / |
+    //      +1.0 /  |
+    //           -1.0
+    // 
+    //         [7]------[6]
+    //        / |      / |
+    //      [3]------[2] |
+    //       |  |     |  |
+    //       | [4]----|-[5]
+    //       |/       |/
+    //      [0]------[1]
+    //
+    var positions = [
+        // Front face
+        -0.5, -0.5,  0.5, // v0
+         0.5, -0.5,  0.5, // v1
+         0.5,  0.5,  0.5, // v2
+        -0.5,  0.5,  0.5, // v3
+        // Back face
+        -0.5, -0.5, -0.5, // v4
+         0.5, -0.5, -0.5, // v5
+         0.5,  0.5, -0.5, // v6
+        -0.5,  0.5, -0.5, // v7
+        // Top face
+         0.5,  0.5,  0.5, // v2
+        -0.5,  0.5,  0.5, // v3
+        -0.5,  0.5, -0.5, // v7
+         0.5,  0.5, -0.5, // v6
+        // Bottom face
+        -0.5, -0.5,  0.5, // v0
+         0.5, -0.5,  0.5, // v1
+         0.5, -0.5, -0.5, // v5
+        -0.5, -0.5, -0.5, // v4
+         // Right face
+         0.5, -0.5,  0.5, // v1
+         0.5,  0.5,  0.5, // v2
+         0.5,  0.5, -0.5, // v6
+         0.5, -0.5, -0.5, // v5
+         // Left face
+        -0.5, -0.5,  0.5, // v0
+        -0.5,  0.5,  0.5, // v3
+        -0.5,  0.5, -0.5, // v7
+        -0.5, -0.5, -0.5  // v4
+    ];
+
+    var indices = [
+         0,  1,  2,    0,  2 , 3,  // Front face
+         4,  5,  6,    4,  6 , 7,  // Back face
+         8,  9, 10,    8, 10, 11,  // Top face
+        12, 13, 14,   12, 14, 15,  // Bottom face
+        16, 17, 18,   16, 18, 19,  // Right face
+        20, 21, 22,   20, 22, 23   // Left face
+    ];
+    
+    gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+    gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);
+
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, gl.createBuffer());
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+
+    var baseTime = +new Date;
+    var time = 0;
+
+    var rad = 0;
+    (function(){
+        rad += Math.PI * 1.0 / 180.0;
+
+        var c = Math.cos(rad);
+        var s = Math.sin(rad);
+
+        // 変換行列を用意
+        // x軸で回転
+        var matAxisX = [
+            1.0, 0.0, 0.0, 0.0,
+            0.0,   c,  -s, 0.0,
+            0.0,   s,   c, 0.0,
+            0.0, 0.0, 0.0, 1.0
+        ];
+        // y軸で回転
+        var matAxisY = [
+              c, 0.0,   s, 0.0,
+            0.0, 1.0, 0.0, 0.0,
+             -s, 0.0,   c, 0.0,
+            0.0, 0.0, 0.0, 1.0
+        ];
+        gl.uniformMatrix4fv(uLoc[0], false, new Float32Array(matAxisX));
+        gl.uniformMatrix4fv(uLoc[1], false, new Float32Array(matAxisY));
+
+        // uniform float time 
+        time = (+new Date - baseTime) / 1000;
+        gl.uniform1f(uLoc[2], time);
+
+        gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
+        //gl.drawArrays(gl.POINTS, 0, data.length / 3);
+        gl.flush();
+        requestAnimationFrame(arguments.callee);
+
+        // stats 更新
+        stats.update();
+    })();
+}
+
+initWebGL();
+initShaders();
+draw();
---
+++ cx20/gv10/readme.txt
@@ -0,0 +1,11 @@
+﻿[簡易版] WebGL で立方体に光を当ててみるテスト（フラットシェーディング編）
+
+＜対応した点＞
+・フラットシェーディングを使用するよう対応。
+
+＜参考＞
+■ フラットシェーディング - wgld.org
+https://wgld.org/d/webgl/w087.html
+
+■ 法線ベクトルとスムージング角
+http://sky.geocities.jp/freakish_osprey/opengl/opengl_normalvecotr.htm
