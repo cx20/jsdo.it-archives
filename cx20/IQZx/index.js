13289907bc3706d2bb6ef9e4918d95b98f82e9c0
cx20 <y-shimano@hondarer-soft.com>
2019-10-14 22:40:43 +0900

    voxel.css を試してみるテスト（その２）

---
+++ cx20/IQZx/index.js
@@ -0,0 +1,54 @@
+// forked from cx20's "voxel.css を試してみるテスト" http://jsdo.it/cx20/UK3K
+// forked from https://github.com/HunterLarco/voxel.css/blob/master/tests/demo.html
+
+var scene;
+var world;
+var editor;
+var lightSource;
+
+function init() {
+    scene = new voxelcss.Scene();
+    scene.rotate(-Math.PI / 5, Math.PI / 4, 0);
+    scene.attach(document.body);
+
+    world = new voxelcss.World(scene, 'Demo World');
+    editor = new voxelcss.Editor(world);
+    editor.enableAutoSave();
+
+    lightSource = new voxelcss.LightSource(300, 300, 300, 750, 0.2, 1);
+    scene.addLightSource(lightSource);
+
+    buildCube();
+    animate();
+}
+
+function buildCube() {
+    for (var x = -3; x <= 3; x++) {
+        for (var y = -3; y <= 3; y++) {
+            for (var z = -3; z <= 3; z++) {
+                if ([x, y, z].filter(function(value) {
+                        return Math.abs(value) === 3
+                    }).length >= 2) {
+                    editor.add(new voxelcss.Voxel(x * 40, y * 40, z * 40, 40, {
+                        mesh: new voxelcss.Mesh(new voxelcss.ColorFace('#ca8940'))
+                    }));
+                }
+            }
+        }
+    }
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    render();
+}
+
+function render() {
+    var x = -Math.PI * 2 / 180 ;
+    var y = Math.PI * 2 / 180;
+    var z = 0;
+    scene.rotate(x, y, z);
+}
+
+init();
---
+++ cx20/IQZx/readme.txt
@@ -0,0 +1,11 @@
+﻿voxel.css を試してみるテスト（その２）
+
+＜対応した点＞
+・Voxel を回転するよう対応。
+
+＜参考＞
+■ HunterLarco/voxel.css
+https://github.com/HunterLarco/voxel.css
+
+■ voxel.css
+http://voxelcss.com/
