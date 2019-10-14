f1ed5d209736d79fe831d3fd218e9a28ff156437
cx20 <y-shimano@hondarer-soft.com>
2019-10-14 21:21:08 +0900

    [WebGL] Grimoire.js で render-quad を使ってシェーダを実行してみるテスト（その３）

---
+++ cx20/Ehzy/index.js
@@ -0,0 +1,32 @@
+// forked from cx20's "forked: [WebGL] Grimoire.js で render-quad を使ってシェーダを実行してみるテスト" http://jsdo.it/cx20/wVDH
+// forked from cx20's "[WebGL] Grimoire.js で render-quad を使ってシェーダを実行してみるテスト" http://jsdo.it/cx20/AtCh
+// forked from cx20's "[WebGL] Grimoire.js を試してみるテスト（その２）" http://jsdo.it/cx20/mjC6
+// forked from cx20's "[WebGL] Grimoire.js を試してみるテスト" http://jsdo.it/cx20/4ZEB
+// forked from cx20's "[WebGL] jThree を試してみるテスト" http://jsdo.it/cx20/Go5s
+// nothing
+
+gr.registerComponent('Rotate', {
+  attributes: {
+    speed: {
+      default: '1',
+      converter: 'Number',
+    },
+  },
+  $mount: function () {
+    this.phi = 0;
+  },
+  $update: function () {
+    this.phi += this.getAttribute('speed');
+    this.node.setAttribute('rotation', this.phi + ',' + this.phi + ',' + 0);
+  },
+});
+
+var Base64 = {
+  encode: function(str) {
+    return btoa(unescape(encodeURIComponent(str)));
+  }
+};
+
+gr(function() {
+  var $$ = gr('#canvas');
+});
