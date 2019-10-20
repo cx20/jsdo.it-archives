134726d3c93af61401ed8ef6c3449e8e0c64cc10
cx20 <y-shimano@hondarer-soft.com>
2019-10-20 21:16:13 +0900

    [WebVR] Babylon.js で WebVR を試してみるテスト（その２）（調整中）

---
+++ cx20/Ut6C/index.js
@@ -0,0 +1,52 @@
+// forked from cx20's "[WebGL] Babylon.js で WebVR を試してみるテスト（調整中）" http://jsdo.it/cx20/WPxV
+// forked from cx20's "[WebGL] Babylon.js を試してみるテスト（組み込み関数編）（その４）" http://jsdo.it/cx20/jwt0
+// forked from cx20's "[WebGL] Babylon.js を試してみるテスト（組み込み関数編）（その３）" http://jsdo.it/cx20/uqcv
+// forked from cx20's "[WebGL] Babylon.js を試してみるテスト（組み込み関数編）（その２）" http://jsdo.it/cx20/anpf
+// forked from cx20's "[WebGL] Babylon.js を試してみるテスト（組み込み関数編）" http://jsdo.it/cx20/oRtWo
+// forked from cx20's "[WebGL] Babylon.js を試してみるテスト" http://jsdo.it/cx20/84AP
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+
+var createScene = function(engine) {
+    var scene = new BABYLON.Scene(engine);
+    scene.enablePhysics(new BABYLON.Vector3(0,-9.8,0), new BABYLON.OimoJSPlugin());
+    scene.getPhysicsEngine().setTimeStep(1 / 30);
+
+    var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 10, new BABYLON.Vector3(0, 0, 0), scene);
+    camera.setPosition(new BABYLON.Vector3(0, 20, -200));
+    camera.attachControl(canvas, true);
+
+    // Enable VR
+    var vrHelper = scene.createDefaultVRExperience({createDeviceOrientationCamera:false});
+    
+    scene.clearColor = new BABYLON.Color3(0, 0, 0);
+    
+
+    var material = new BABYLON.StandardMaterial("material", scene);
+    material.diffuseTexture = new BABYLON.Texture("../../assets/A/k/w/j/AkwjW.jpg", scene); // frog.jpg
+    material.emissiveColor = new BABYLON.Color3(1, 1, 1);
+    var ground = new BABYLON.Mesh.CreateBox('ground', 200.0, scene);
+    ground.position.y = -20;
+    ground.scaling.y = 0.01;
+    ground.material = material;
+    ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, {mass: 0, friction: 0.1, restitution: 0.1}, scene);
+
+    var cube = new BABYLON.Mesh.CreateBox('cube', 50.0, scene);
+    cube.material = material;
+    cube.position.y = 100;
+    cube.rotation.x = Math.PI * 10/180;
+    cube.rotation.z = Math.PI * 10/180;
+    cube.physicsImpostor = new BABYLON.PhysicsImpostor(cube, BABYLON.PhysicsImpostor.BoxImpostor, {mass: 1, friction: 0.2, restitution: 0.5}, scene);
+
+    engine.runRenderLoop(function () {
+        scene.render();
+        scene.activeCamera.alpha += 0.005;
+    });
+    return scene;
+}
+
+var canvas = document.querySelector("#renderCanvas");
+var engine = new BABYLON.Engine(canvas, true);
+window.addEventListener('resize', function() {
+    engine.resize();
+});
+var scene = createScene(engine);
---
+++ cx20/Ut6C/readme.txt
@@ -0,0 +1,15 @@
+﻿[WebVR] Babylon.js で WebVR を試してみるテスト（その２）（調整中）
+
+＜参考＞
+■ [WebGL] Babylon.js + Oimo.js を試してみるテスト（調整中）
+http://jsdo.it/cx20/ijR9
+
+＜他ライブラリ比較＞
+■ [WebVR] three.js で WebVR を試してみるテスト（その２）（調整中）
+http://jsdo.it/cx20/21Bb
+■ [WebVR] Babylon.js で WebVR を試してみるテスト（その２）（調整中）
+http://jsdo.it/cx20/Ut6C
+■ [WebVR] GLBoost で WebVR を試してみるテスト（その２）（調整中）
+http://jsdo.it/cx20/akl7
+■ [WebVR] A-Frame で WebVR を試してみるテスト（その２）（調整中）
+http://jsdo.it/cx20/yzbu
