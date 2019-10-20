0099966f7b5b9745992d3aef72a09069cf54fbcd
cx20 <y-shimano@hondarer-soft.com>
2019-10-20 22:06:45 +0900

    [WebVR] A-Frame で WebVR を試してみるテスト（その２）（調整中）

---
+++ cx20/yzbu/index.js
@@ -0,0 +1,7 @@
+// forked from cx20's "[WebVR] A-Frame で WebVR を試してみるテスト（調整中）" http://jsdo.it/cx20/6X3S
+// forked from cx20's "[WebGL] A-Frame を試してみるテスト（その４）" http://jsdo.it/cx20/k7B2
+// forked from cx20's "[WebGL] A-Frame を試してみるテスト（その３）" http://jsdo.it/cx20/qgOl
+// forked from cx20's "[WebGL] A-Frame を試してみるテスト（その２）" http://jsdo.it/cx20/OclK
+// forked from cx20's "[WebGL] A-Frame を試してみるテスト" http://jsdo.it/cx20/AOwl
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+// nothing
---
+++ cx20/yzbu/readme.txt
@@ -0,0 +1,18 @@
+﻿[WebVR] A-Frame で WebVR を試してみるテスト（その２）（調整中）
+
+＜対応した点＞
+・A-Frame で物理演算ライブラリを使うよう対応。
+
+＜参考＞
+■ Physics for A-Frame VR
+https://github.com/donmccurdy/aframe-physics-system
+
+＜他ライブラリ比較＞
+■ [WebVR] three.js で WebVR を試してみるテスト（その２）（調整中）
+http://jsdo.it/cx20/21Bb
+■ [WebVR] Babylon.js で WebVR を試してみるテスト（その２）（調整中）
+http://jsdo.it/cx20/Ut6C
+■ [WebVR] GLBoost で WebVR を試してみるテスト（その２）（調整中）
+http://jsdo.it/cx20/akl7
+■ [WebVR] A-Frame で WebVR を試してみるテスト（その２）（調整中）
+http://jsdo.it/cx20/yzbu
