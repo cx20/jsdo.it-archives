1e366433d8848d263d911d6b2eedc47733f76d8c
cx20 <y-shimano@hondarer-soft.com>
2019-10-20 22:15:21 +0900

    [WebVR] A-Frame で WebVR を試してみるテスト（その６）（調整中）

---
+++ cx20/8tid/index.js
@@ -0,0 +1,10 @@
+// forked from cx20's "[WebVR] A-Frame で WebVR を試してみるテスト（その５）（調整中）" http://jsdo.it/cx20/syCY
+// forked from cx20's "[WebVR] A-Frame で WebVR を試してみるテスト（その４）（調整中）" http://jsdo.it/cx20/qh52
+// forked from cx20's "[WebVR] A-Frame で WebVR を試してみるテスト（その３）（調整中）" http://jsdo.it/cx20/0ZUL
+// forked from cx20's "[WebVR] A-Frame で WebVR を試してみるテスト（その２）（調整中）" http://jsdo.it/cx20/yzbu
+// forked from cx20's "[WebVR] A-Frame で WebVR を試してみるテスト（調整中）" http://jsdo.it/cx20/6X3S
+// forked from cx20's "[WebGL] A-Frame を試してみるテスト（その４）" http://jsdo.it/cx20/k7B2
+// forked from cx20's "[WebGL] A-Frame を試してみるテスト（その３）" http://jsdo.it/cx20/qgOl
+// forked from cx20's "[WebGL] A-Frame を試してみるテスト（その２）" http://jsdo.it/cx20/OclK
+// forked from cx20's "[WebGL] A-Frame を試してみるテスト" http://jsdo.it/cx20/AOwl
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
---
+++ cx20/8tid/readme.txt
@@ -0,0 +1,15 @@
+﻿[WebVR] A-Frame で WebVR を試してみるテスト（その６）（調整中）
+
+
+＜対応した点＞
+・頂点法線を頂点色として設定するよう対応
+
+＜他ライブラリ比較＞
+■ [WebVR] three.js で WebVR を試してみるテスト（その６）（調整中）
+http://jsdo.it/cx20/AJ0Q
+■ [WebVR] Babylon.js で WebVR を試してみるテスト（その６）（調整中）
+http://jsdo.it/cx20/AJDq
+■ [WebVR] GLBoost で WebVR を試してみるテスト（その６）（調整中）
+http://jsdo.it/cx20/ygHt
+■ [WebVR] A-Frame で WebVR を試してみるテスト（その６）（調整中）
+http://jsdo.it/cx20/8tid
