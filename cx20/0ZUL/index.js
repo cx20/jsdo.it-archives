cea9b88298d0054cd9a9d89cc6a6f336baa7ddf7
cx20 <y-shimano@hondarer-soft.com>
2019-10-20 22:08:00 +0900

    [WebVR] A-Frame で WebVR を試してみるテスト（その３）（調整中）

---
+++ cx20/0ZUL/index.js
@@ -0,0 +1,84 @@
+// forked from cx20's "[WebVR] A-Frame で WebVR を試してみるテスト（その２）（調整中）" http://jsdo.it/cx20/yzbu
+// forked from cx20's "[WebVR] A-Frame で WebVR を試してみるテスト（調整中）" http://jsdo.it/cx20/6X3S
+// forked from cx20's "[WebGL] A-Frame を試してみるテスト（その４）" http://jsdo.it/cx20/k7B2
+// forked from cx20's "[WebGL] A-Frame を試してみるテスト（その３）" http://jsdo.it/cx20/qgOl
+// forked from cx20's "[WebGL] A-Frame を試してみるテスト（その２）" http://jsdo.it/cx20/OclK
+// forked from cx20's "[WebGL] A-Frame を試してみるテスト" http://jsdo.it/cx20/AOwl
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+
+// ‥‥‥‥‥‥‥‥‥‥‥‥‥□□□
+// ‥‥‥‥‥‥〓〓〓〓〓‥‥□□□
+// ‥‥‥‥‥〓〓〓〓〓〓〓〓〓□□
+// ‥‥‥‥‥■■■□□■□‥■■■
+// ‥‥‥‥■□■□□□■□□■■■
+// ‥‥‥‥■□■■□□□■□□□■
+// ‥‥‥‥■■□□□□■■■■■‥
+// ‥‥‥‥‥‥□□□□□□□■‥‥
+// ‥‥■■■■■〓■■■〓■‥‥‥
+// ‥■■■■■■■〓■■■〓‥‥■
+// □□■■■■■■〓〓〓〓〓‥‥■
+// □□□‥〓〓■〓〓□〓〓□〓■■
+// ‥□‥■〓〓〓〓〓〓〓〓〓〓■■
+// ‥‥■■■〓〓〓〓〓〓〓〓〓■■
+// ‥■■■〓〓〓〓〓〓〓‥‥‥‥‥
+// ‥■‥‥〓〓〓〓‥‥‥‥‥‥‥‥
+const dataSet = [
+    "BK","BK","BK","BK","BK","BK","BK","BK","BK","BK","BK","BK","BK","BG","BG","BG",
+    "BK","BK","BK","BK","BK","BK","RD","RD","RD","RD","RD","BK","BK","BG","BG","BG",
+    "BK","BK","BK","BK","BK","RD","RD","RD","RD","RD","RD","RD","RD","RD","BG","BG",
+    "BK","BK","BK","BK","BK","BR","BR","BR","BG","BG","BR","BG","BK","RD","RD","RD",
+    "BK","BK","BK","BK","BR","BG","BR","BG","BG","BG","BR","BG","BG","RD","RD","RD",
+    "BK","BK","BK","BK","BR","BG","BR","BR","BG","BG","BG","BR","BG","BG","BG","RD",
+    "BK","BK","BK","BK","BR","BR","BG","BG","BG","BG","BR","BR","BR","BR","RD","BK",
+    "BK","BK","BK","BK","BK","BK","BG","BG","BG","BG","BG","BG","BG","RD","BK","BK",
+    "BK","BK","RD","RD","RD","RD","RD","BL","RD","RD","RD","BL","RD","BK","BK","BK",
+    "BK","RD","RD","RD","RD","RD","RD","RD","BL","RD","RD","RD","BL","BK","BK","BR",
+    "BG","BG","RD","RD","RD","RD","RD","RD","BL","BL","BL","BL","BL","BK","BK","BR",
+    "BG","BG","BG","BK","BL","BL","RD","BL","BL","YL","BL","BL","YL","BL","BR","BR",
+    "BK","BG","BK","BR","BL","BL","BL","BL","BL","BL","BL","BL","BL","BL","BR","BR",
+    "BK","BK","BR","BR","BR","BL","BL","BL","BL","BL","BL","BL","BL","BL","BR","BR",
+    "BK","BR","BR","BR","BL","BL","BL","BL","BL","BL","BL","BK","BK","BK","BK","BK",
+    "BK","BR","BK","BK","BL","BL","BL","BL","BK","BK","BK","BK","BK","BK","BK","BK"
+];
+
+function getRgbColor(colorType)
+{
+    var colorHash = {
+        "BK":"#dcaa6b", // black
+        "WH":"#FFFFFF", // white
+        "BG":"#FFCCCC", // beige
+        "BR":"#800000", // brown
+        "RD":"#FF0000", // red
+        "YL":"#FFFF00", // yellow
+        "GN":"#00FF00", // green
+        "WT":"#00FFFF", // water
+        "BL":"#0000FF", // blue
+        "PR":"#800080"  // purple
+    };
+    return colorHash[colorType];
+}
+
+let scene = document.querySelector("a-scene");
+let grassTexture = document.querySelector("#grass-texture");
+
+for ( let x = 0; x < 16; x++ ) {
+    for ( let y = 0; y < 16; y ++ ) {
+        let x1 = (x-8) / 10 + Math.random() * 0.01;
+        let y1 = y / 10 + Math.random() * 0.01;
+        let z1 = -2 + Math.random() * 0.01;
+        let i = x + (15-y) * 16;
+        let color = getRgbColor( dataSet[i] );
+        
+        /*
+            <a-sphere position="0 2 -2" dynamic-body  radius="0.5" material="src: #football-texture"></a-sphere>
+        */
+        let sphere = document.createElement("a-sphere");
+        sphere.setAttribute("position", `${x1} ${y1} ${z1}`);
+        sphere.setAttribute("dynamic-body", "");
+        sphere.setAttribute("radius", "0.05");
+        sphere.setAttribute("material", `src: #football-texture; color: ${color}`);
+        
+        scene.appendChild(sphere);
+    }
+}
+
---
+++ cx20/0ZUL/readme.txt
@@ -0,0 +1,15 @@
+﻿[WebVR] A-Frame で WebVR を試してみるテスト（その３）（調整中）
+
+＜参考＞
+■ Physics for A-Frame VR
+https://github.com/donmccurdy/aframe-physics-system
+
+＜他ライブラリ比較＞
+■ [WebVR] three.js で WebVR を試してみるテスト（その３）（調整中）
+http://jsdo.it/cx20/qc23
+■ [WebVR] Babylon.js で WebVR を試してみるテスト（その３）（調整中）
+http://jsdo.it/cx20/Om12
+■ [WebVR] GLBoost で WebVR を試してみるテスト（その３）（調整中）
+http://jsdo.it/cx20/GeUE
+■ [WebVR] A-Frame で WebVR を試してみるテスト（その３）（調整中）
+http://jsdo.it/cx20/0ZUL
