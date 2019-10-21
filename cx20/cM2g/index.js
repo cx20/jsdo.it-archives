752105ff9c93f81ebc9c1d55b9f5b8901c7881b0
cx20 <y-shimano@hondarer-soft.com>
2019-10-22 06:39:30 +0900

    御嶽山の場所を表示してみる

--- cx20/cM2g/index.js
+++ cx20/cM2g/index.js
@@ -7,9 +7,16 @@ var earth;
 function initialize() {
     var options = {atmosphere: true, center: [0, 0], zoom: 0};
     earth = new WE.map('earth_div', options);
+/*
     WE.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.jpg', {
         subdomains: '1234',
         attribution: 'Tiles Courtesy of MapQuest'
+    }).addTo(earth);
+*/
+    WE.tileLayer('http://tileserver.maptiler.com/nasa/{z}/{x}/{y}.jpg', {
+      minZoom: 0,
+      maxZoom: 5,
+      attribution: 'NASA'
     }).addTo(earth);
 }
 
