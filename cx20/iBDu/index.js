e90eb5ad09ea68ee9c070837422390a7532f8d3d
cx20 <y-shimano@hondarer-soft.com>
2019-10-22 15:49:59 +0900

    箱根山周辺の3Dデータを表示してみる（その３）

---
+++ cx20/iBDu/index.js
@@ -0,0 +1,98 @@
+// forked from cx20's "箱根山周辺の3Dデータを表示してみる（その２）" http://jsdo.it/cx20/ineU
+// forked from cx20's "箱根山周辺の3Dデータを表示してみる" http://jsdo.it/cx20/ljKt
+// forked from cx20's "地理院地図3Dデータを使ってみるテスト" http://jsdo.it/cx20/l4shv
+
+var scene;
+var camera;
+var renderer;
+var controls;
+var width = window.innerWidth;
+var height = window.innerHeight;
+
+// heightMap より標高データを取得する
+// 参考：http://danni-three.blogspot.jp/2013/09/threejs-heightmaps.html
+function getHeightData(img) {
+    var canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+    var context = canvas.getContext("2d");
+
+    var size = img.width * img.height;
+    var data = new Float32Array(size);
+
+    context.drawImage(img, 0, 0);
+
+    var imgd = context.getImageData(0, 0, img.width, img.height);
+    var pix = imgd.data;
+
+    var j = 0;
+    for (var i = 0; i < pix.length; i += 4) {
+        var all = pix[i] + pix[i + 1] + pix[i + 2];
+        data[j++] = all / 25;
+    }
+
+    return data;
+}
+
+var img = new Image();
+img.onload = function() {
+    scene = new THREE.Scene();
+    scene.add(new THREE.AmbientLight(0xffffff));
+
+    camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 1000);
+    camera.position.set(0, -100, 100);
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(width, height);
+
+    // OrbitControls の準備
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.userPan = false;
+    controls.userPanSpeed = 0.0;
+    controls.maxDistance = 5000.0;
+    controls.maxPolarAngle = Math.PI * 0.495;
+    controls.rotateUp(Math.PI * 0.38);
+    controls.autoRotate = true; //true:自動回転する,false:自動回転しない
+    controls.autoRotateSpeed = -2.0; //自動回転する時の速度
+
+    // heightMap より標高データを取得
+    var data = getHeightData(img);
+
+    // 標高データを元に地形を生成
+    var x1 = 128;
+    var y1 = 128;
+    var x2 = 256;
+    var y2 = 256;
+    var geometry = new THREE.PlaneGeometry(x1, y1, x2 - 1, y2 - 1);
+    for (var i = 0; i < geometry.vertices.length; i++) {
+        geometry.vertices[i].z = data[i];
+    }
+
+    // テクスチャを貼り付け
+    var material = new THREE.MeshPhongMaterial({
+        //map: THREE.ImageUtils.loadTexture("/assets/1/j/j/H/1jjHY.jpg") // 箱根山通常地図
+        //map: THREE.ImageUtils.loadTexture("/assets/p/Q/e/W/pQeWF.jpg") // 箱根山の防災マップ
+        //map: THREE.ImageUtils.loadTexture("/assets/x/z/j/2/xzj2Z.jpg") // 箱根山の地殻変動マップ１
+        map: THREE.ImageUtils.loadTexture("../../assets/d/q/g/U/dqgUo.jpg") // 箱根山の地殻変動マップ２
+    });
+    var plane = new THREE.Mesh(geometry, material);
+    
+    // 座標回転
+    plane.rotation.x = Math.PI / -2; // 90度回転（地面を上向きに設定）
+    scene.add(plane);
+
+    document.getElementById("webgl").appendChild(renderer.domElement);
+    animate();
+};
+
+img.src = "../../assets/g/H/h/L/gHhLD.png"; // heightMap.png
+
+function animate() {
+    requestAnimationFrame(animate);
+    render();
+}
+
+function render() {
+    controls.update();
+    renderer.render(scene, camera);
+}
