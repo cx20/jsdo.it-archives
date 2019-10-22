c0f6225ae5adaa82ad950f04ee564de69da84c23
cx20 <y-shimano@hondarer-soft.com>
2019-10-22 18:11:41 +0900

    草津の本白根山付近の3Dデータを表示してみる

---
+++ cx20/c48E/index.js
@@ -0,0 +1,193 @@
+// forked from cx20's "地理院地図3Dデータを使ってみるテスト" http://jsdo.it/cx20/l4shv
+
+var gui;
+var scene;
+var camera;
+var renderer;
+var controls;
+var engine;
+var showSmoke = false;
+var clock = new THREE.Clock();
+var width = window.innerWidth - 2;
+var height = window.innerHeight - 2;
+var MAP = "../../assets/s/9/Z/2/s9Z2u.jpg";
+var SMOKE = false;
+var ROTATE = true;
+var WIREFRAME = false;
+var emitter, particleGroup;
+
+// heightMap より標高データを取得する
+// 参考：http://danni-three.blogspot.jp/2013/09/threejs-heightmaps.html
+function getHeightData(img) {
+    var canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+    var context = canvas.getContext("2d");
+
+    var size = img.width * img.height;
+    var data = new Float32Array(size);
+
+    context.drawImage(img, 0, 0);
+
+    var imgd = context.getImageData(0, 0, img.width, img.height);
+    var pix = imgd.data;
+
+    var j = 0;
+    for (var i = 0; i < pix.length; i += 4) {
+        var k = 3.0; // 起伏の強調度
+        var height = (pix[i] + pix[i + 1] + pix[i + 2])/3 * 1/16 * k;
+        data[j++] = height;
+    }
+
+    return data;
+}
+
+function initParticles() {
+    var loader = new THREE.TextureLoader();
+    var texture = loader.load('../../assets/h/z/U/p/hzUpb.png');  // smokeparticle.png
+
+    particleGroup = new SPE.Group({
+        texture: {
+            value: texture
+        },
+        maxParticleCount: 1000, 
+        blending: THREE.NormalBlending,
+        transparent: true
+    });
+
+    emitter = new SPE.Emitter({
+        maxAge: {
+            value: 2
+        },
+        position: {
+            value: new THREE.Vector3(12, 13, -9),
+            spread: new THREE.Vector3( 0, 0, 5 )
+        },
+        acceleration: {
+            value: new THREE.Vector3(10, 0, -5),
+            spread: new THREE.Vector3( 10, 10, 10 )
+        },
+        velocity: {
+            value: new THREE.Vector3(0, 10, 0),
+            spread: new THREE.Vector3(10, 10, 10)
+        },
+        color: {
+            value: [ new THREE.Color('#A0A0A0') ]
+        },
+        opacity: {
+            value: 0.5
+        },
+        size: {
+            value: 10
+        },
+        particleCount: 1000
+    });
+    
+}
+
+var img = new Image();
+img.onload = function() {
+    scene = new THREE.Scene();
+    scene.add(new THREE.AmbientLight(0xffffff));
+
+    camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 1000);
+    camera.position.set(0, 50, 100);
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(width, height);
+
+    // OrbitControls の準備
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.autoRotate = ROTATE; //true:自動回転する,false:自動回転しない
+    controls.autoRotateSpeed = -2.0; //自動回転する時の速度
+
+    // heightMap より標高データを取得
+    var data = getHeightData(img);
+
+    // 標高データを元に地形を生成
+    var x1 = 128;
+    var y1 = 128;
+    var x2 = 256;
+    var y2 = 256;
+    var geometry = new THREE.PlaneGeometry(x1, y1, x2 - 1, y2 - 1);
+    for (var i = 0; i < geometry.vertices.length; i++) {
+        geometry.vertices[i].z = data[i] - 30;
+    }
+
+    // テクスチャを貼り付け
+    var material = new THREE.MeshPhongMaterial({
+        map: THREE.ImageUtils.loadTexture(MAP)
+    });
+    var plane = new THREE.Mesh(geometry, material);
+    
+    // 座標回転
+    plane.rotation.x = Math.PI / -2; // 90度回転（地面を上向きに設定）
+    scene.add(plane);
+
+    initParticles();
+    
+    // GUI
+    gui = new dat.GUI();
+    var mapSelector = gui.add(window, 'MAP', {
+        "通常地図": "../../assets/u/C/D/P/uCDPS.jpg",
+        "空撮写真": "../../assets/g/K/n/a/gKnaC.jpg",
+        "噴火口位置": "../../assets/s/f/t/D/sftDZ.jpg",
+        "火山弾飛散域": "../../assets/s/9/Z/2/s9Z2u.jpg",
+        "過去噴火口": "../../assets/A/X/u/7/AXu7z.jpg",
+        "火山災害対策用図": "../../assets/o/c/P/g/ocPgv.jpg",
+        "火山土地条件図": "../../assets/q/q/3/g/Qq3gf.jpg",
+        //"雪山": "../../assets/g/B/6/9/gB69w.png",
+    });
+    var mapSmoke = gui.add(window, 'SMOKE').name('Smoke');
+    var mapRotate = gui.add(window, 'ROTATE').name('Rotate');
+    var mapWireframe = gui.add(window, 'WIREFRAME').name('Wireframe');
+
+    mapSelector.onChange(function (value) {
+        plane.material.map = THREE.ImageUtils.loadTexture(value);
+    });
+    
+    mapSmoke.onChange(function (value) {
+        if ( value ) {
+            particleGroup.addEmitter( emitter );
+            scene.add( particleGroup.mesh );
+        } else {
+            particleGroup.removeEmitter( emitter );
+        }
+        
+        showSmoke = value;
+    });
+
+    mapRotate.onChange(function (value) {
+        controls.autoRotate = value;
+    });
+
+    mapWireframe.onChange(function (value) {
+        plane.material.wireframe = value;
+    });
+    
+    document.getElementById("webgl").appendChild(renderer.domElement);
+    animate();
+
+    window.addEventListener('resize', function() {
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+      }, false );
+};
+
+//img.src = "/assets/E/R/Q/2/ERQ2H.png"; // heightMap.png
+img.src = "../../assets/g/B/6/9/gB69w.png";
+
+function animate() {
+    requestAnimationFrame(animate);
+    render();
+}
+
+function render() {
+    controls.update();
+    var dt = clock.getDelta();
+    if ( showSmoke ) {
+        particleGroup.tick( dt );
+    }
+    renderer.render(scene, camera);
+}
---
+++ cx20/c48E/readme.txt
@@ -0,0 +1,28 @@
+﻿草津の本白根山付近の3Dデータを表示してみる
+
+正確な情報については各情報提供元を参照願います。
+
+＜対応した点＞
+・各種地図を選択できるよう対応。
+・Three.js を r69 → r90 に変更
+・Particle ライブラリを ParticleEngine.js → SPE.js に変更
+
+＜情報源＞
+3Dデータ：国土地理院
+通常地図：国土地理院
+空撮写真：Google, ZENRIN
+噴火口位置：https://twitter.com/HayakawaYukio/status/955788296260472833
+火山弾飛散域：https://twitter.com/HayakawaYukio/status/956108408934944769
+過去噴火口：https://twitter.com/HayakawaYukio/status/956132299204014085
+火山災害対策用図：国土地理院
+火山土地条件図：国土地理院
+
+＜参考＞
+■ 地理院地図３D
+http://cyberjapandata.gsi.go.jp/3d/index.html
+
+■ 草津 本白根山（もとしらねさん）
+https://maps.gsi.go.jp/index_3d.html?lat=36.62277777777778&lon=138.53194444444446&z=15&pxsize=2048&ls=std
+
+■ 標高CSV→Height Map 変換ツールv2（仮）
+http://jsdo.it/cx20/i7Lt
