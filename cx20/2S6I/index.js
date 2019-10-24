8c85a45bfcc3cdae23681ec26a73d7d85bd22dc4
cx20 <y-shimano@hondarer-soft.com>
2019-10-25 03:51:51 +0900

    GLSL で正弦波を描いてみるテスト（その４）

---
+++ cx20/2S6I/index.js
@@ -0,0 +1,5 @@
+// forked from cx20's "GLSL で正弦波を描いてみるテスト（その３）" http://jsdo.it/cx20/Ajwm
+// forked from cx20's "GLSL で正弦波を描いてみるテスト（その２）" http://jsdo.it/cx20/iwrh
+// forked from cx20's "GLSL で正弦波を描いてみるテスト" http://jsdo.it/cx20/sOX6
+// forked from cx20's "GLSL で富士山を描いてみるテスト" http://jsdo.it/cx20/lMo6
+// forked from h_doxas's "コロスケダヨー" http://jsdo.it/h_doxas/99hR
---
+++ cx20/2S6I/readme.txt
@@ -0,0 +1,10 @@
+﻿GLSL で正弦波を描いてみるテスト（その４）
+
+＜対応した点＞
+・縦軸と横軸のグラフの交点を青でプロットするよう対応
+
+＜追記＞
+IE11 で表示できるよう、以下変更しました。
+
+変更前） var gl = c.getContext('webgl');
+変更後） var gl = c.getContext('webgl') || c.getContext('experimental-webgl');
