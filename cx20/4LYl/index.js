ce51d60160da11c45c6235a92488b8b0d33632d9
cx20 <y-shimano@hondarer-soft.com>
2019-10-26 22:19:52 +0900

    vox.js を試してみるテスト（その９）

---
+++ cx20/4LYl/index.js
@@ -0,0 +1,175 @@
+// forked from cx20's "vox.js を試してみるテスト（その８）" http://jsdo.it/cx20/iEi8
+// forked from cx20's "vox.js を試してみるテスト（その７）" http://jsdo.it/cx20/K6wm
+// forked from cx20's "vox.js を試してみるテスト（その６）" http://jsdo.it/cx20/oZg0
+// forked from cx20's "vox.js を試してみるテスト（その５）" http://jsdo.it/cx20/af9L
+// forked from cx20's "vox.js を試してみるテスト（その４）" http://jsdo.it/cx20/qL0R
+// forked from cx20's "vox.js を試してみるテスト（その３）" http://jsdo.it/cx20/ymYt
+// forked from cx20's "vox.js を試してみるテスト（その２）" http://jsdo.it/cx20/adwz
+// forked from cx20's "vox.js を試してみるテスト" http://jsdo.it/cx20/u80n
+// forked from ohisama1's "vox.js test 0" http://jsdo.it/ohisama1/e9dm
+
+var DOT_SIZE = 16;
+
+var renderer, scene, camera, particleSystem;
+var geometry;
+var uniforms, attributes;
+var controls;
+var ROTATE = true;
+
+var PARTICLE_SIZE   = DOT_SIZE;
+var PARTICLE_RANGE       = 80;
+var PARTICLE_RANGE_HALF  = PARTICLE_RANGE/2;
+
+init();
+
+function init() {
+
+    // dom
+    var container = document.getElementById('container');
+
+    // renderer
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    container.appendChild(renderer.domElement);
+
+    //camera
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
+    camera.position.z = 300;
+
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.userPan = false;
+    controls.userPanSpeed = 0.0;
+    controls.maxDistance = 5000.0;
+    controls.maxPolarAngle = Math.PI * 0.495;
+    controls.rotateUp(Math.PI * 0.1);
+    controls.autoRotate = ROTATE; //true:自動回転する,false:自動回転しない
+    controls.autoRotateSpeed = 4.0; //自動回転する時の速度
+
+    // scene
+    scene = new THREE.Scene();
+
+    // particle system geometry
+    geometry = new THREE.BufferGeometry();
+
+    // material
+    attributes = {
+        size: {    type: 'f', value: [] },
+        customColor: { type: 'c', value: [] }
+    };
+
+    uniforms = {
+        amplitude: { type: "f", value: 1.0 },
+        color:     { type: "c", value: new THREE.Color( 0xffffff ) },
+        //texture:   { type: "t", value: THREE.ImageUtils.loadTexture( "/assets/o/Q/R/Y/oQRYj.png" ) },  // spark1.png
+    };
+
+    // material
+    var material = new THREE.ShaderMaterial( {
+
+        uniforms:       uniforms,
+        attributes:     attributes,
+        vertexShader:   document.getElementById( 'vertexshader' ).textContent,
+        fragmentShader: document.getElementById( 'fragmentshader' ).textContent,
+
+        blending:       THREE.AdditiveBlending,
+        depthTest:      false,
+        transparent:    true
+
+    });
+
+    var parser = new vox.Parser();
+    // adobe.vox
+    parser.parse("../../assets/y/l/j/y/yljy3.vox").then(function(voxelData) {
+        var particles = voxelData.voxels.length;
+        var positions = new Float32Array( particles * 3 );
+        var velocities = new Float32Array( particles * 3 );
+        var colors = new Float32Array( particles * 3 );
+        var sizes = new Float32Array( particles );
+        var color = new THREE.Color();
+        for ( var i = 0; i  < voxelData.voxels.length; i++ ) {
+            var voxel = voxelData.voxels[i];
+            // positions
+            var x = voxel.x * DOT_SIZE - voxelData.size.x * DOT_SIZE / 2;
+            var y = voxel.z * DOT_SIZE - voxelData.size.z * DOT_SIZE / 2;
+            var z = voxel.y * DOT_SIZE - voxelData.size.y * DOT_SIZE / 2;
+
+            positions[ i * 3 + 0 ] = x;
+            positions[ i * 3 + 1 ] = y;
+            positions[ i * 3 + 2 ] = z;
+
+            // velocities
+            velocities[ i * 3 + 0 ] = Math.random();
+            velocities[ i * 3 + 1 ] = Math.random();
+            velocities[ i * 3 + 2 ] = Math.random();
+
+            // colors
+            var c = voxelData.palette[voxel.colorIndex];
+
+            colors[ i * 3 + 0 ] = c.r / 255;
+            colors[ i * 3 + 1 ] = c.g / 255;
+            colors[ i * 3 + 2 ] = c.b / 255;
+
+            sizes[ i ] = DOT_SIZE * 3.0;
+        }
+
+        geometry.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
+        geometry.addAttribute( 'velocity', new THREE.BufferAttribute( velocities, 3 ) );
+        geometry.addAttribute( 'customColor', new THREE.BufferAttribute( colors, 3 ) );
+        geometry.addAttribute( 'size', new THREE.BufferAttribute( sizes, 1 ) );
+
+        // particle system
+        particleSystem = new THREE.Points(geometry, material);
+        particleSystem.dynamic = true;
+        particleSystem.rotation.y -= Math.PI / 2;
+
+        scene.add(particleSystem);
+
+        animate();
+    });
+
+}
+
+function animate() {
+
+    requestAnimationFrame(animate);
+    render();
+}
+
+function render() {
+    for ( var i = 0; i < geometry.attributes.velocity.array.length; i += 3 ) {
+        var vx = geometry.attributes.velocity.array[i + 0];
+        var vy = geometry.attributes.velocity.array[i + 1];
+        var vz = geometry.attributes.velocity.array[i + 2];
+
+        var px = geometry.attributes.position.array[i + 0];
+        var py = geometry.attributes.position.array[i + 1];
+        var pz = geometry.attributes.position.array[i + 2];
+        
+        px += vx;
+        py += vy;
+        pz += vz;
+
+        if      (px <-PARTICLE_RANGE) { px =-PARTICLE_RANGE; vx *= -1; }
+        else if (px > PARTICLE_RANGE) { px = PARTICLE_RANGE; vx *= -1; }
+        if      (py <-PARTICLE_RANGE) { py =-PARTICLE_RANGE; vy *= -1; }
+        else if (py > PARTICLE_RANGE) { py = PARTICLE_RANGE; vy *= -1; }
+        if      (pz <-PARTICLE_RANGE) { pz =-PARTICLE_RANGE; vz *= -1; }
+        else if (pz > PARTICLE_RANGE) { pz = PARTICLE_RANGE; vz *= -1; }
+
+        geometry.attributes.position.array[i + 0] = px;
+        geometry.attributes.position.array[i + 1] = py;
+        geometry.attributes.position.array[i + 2] = pz;
+
+        geometry.attributes.velocity.array[i + 0] = vx;
+        geometry.attributes.velocity.array[i + 1] = vy;
+        geometry.attributes.velocity.array[i + 2] = vz;
+    }
+    geometry.attributes.position.needsUpdate = true;
+    geometry.attributes.velocity.needsUpdate = true;
+
+    controls.update();
+
+    // render
+    renderer.render(scene, camera);
+
+}
---
+++ cx20/4LYl/readme.txt
@@ -0,0 +1,20 @@
+﻿vox.js を試してみるテスト（その９）
+
+＜対応した点＞
+・ドット絵をバウンドするよう変更。
+
+＜参考＞
+■ Three.js + ParticleSystem でドット絵を描いてみるテスト（その３改）
+http://jsdo.it/cx20/xxkj
+
+■ Particle を個別に動かしてみた - tmlib.js x three.js
+http://jsdo.it/phi/cPm8
+
+■ MagicaVoxel
+https://ephtracy.github.io/
+
+■ JavaScript - MagicaVoxelで作ったデータをThree.jsで表示する - Qiita
+http://qiita.com/daishi_hmr/items/9125e367ab862fd9b41e
+
+■ vox.js
+http://daishihmr.github.io/vox.js/
