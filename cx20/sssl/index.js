bb12425d9fc236001e7140908d6663525880ca87
cx20 <y-shimano@hondarer-soft.com>
2019-10-27 21:09:07 +0900

    [WebGL] lightgl.js でトーラスを描いてみるテスト

---
+++ cx20/sssl/index.js
@@ -0,0 +1,53 @@
+// forked from cx20's "[WebGL] lightgl.js でリンゴ曲面を描いてみるテスト" http://jsdo.it/cx20/pejt
+// forked from cx20's "[WebGL] lightgl.js で貝殻曲面を描いてみるテスト" http://jsdo.it/cx20/lyR5
+// forked from cx20's "[WebGL] lightgl.js でローマ曲面を描いてみるテスト" http://jsdo.it/cx20/v0rS
+// forked from cx20's "[WebGL] lightgl.js で３次元リサージュ図形を描いてみるテスト" http://jsdo.it/cx20/5RXU
+// forked from cx20's "[WebGL] lightgl.js でリサージュ図形を描いてみるテスト" http://jsdo.it/cx20/4kX2
+// forked from cx20's "[WebGL] lightgl.js を試してみるテスト" http://jsdo.it/cx20/rm4H
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+
+var angle = 0;
+var gl = GL.create();
+
+gl.onupdate = function(seconds) {
+    angle += 45 * seconds;
+};
+
+gl.ondraw = function() {
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.loadIdentity();
+    gl.translate(0, 0, -2);
+    gl.rotate(60, 1, 0, 0);
+    gl.rotate(angle, angle, angle, 1);
+
+    // トーラス（Torus）の座標データを用意
+    //             1.0 y 
+    //              ^  -1.0 
+    //              | / z
+    //              |/       x
+    // -1.0 -----------------> +1.0
+    //            / |
+    //      +1.0 /  |
+    //           -1.0
+    // 
+    gl.begin(gl.POINTS);
+    gl.pointSize(2.0);
+    var ustep = Math.PI * 5 / 180;
+    var vstep = Math.PI * 5 / 180;
+    var R = 0.5;
+    var r = 0.2;
+    for (var v = 0; v <= 2 * Math.PI; v += vstep) {
+        for (var u = 0; u <= 2 * Math.PI; u += ustep) {
+            var x = (R + r * Math.cos(v)) * Math.cos(u);
+            var y = (R + r * Math.cos(v)) * Math.sin(u);
+            var z = r * Math.sin(v);
+            gl.color(x + 0.5, y + 0.5, z + 0.5, 1.0);
+            gl.vertex(x, y, z);
+        }
+    }
+    gl.end();
+};
+
+gl.fullscreen();
+gl.animate();
---
+++ cx20/sssl/readme.txt
@@ -0,0 +1,17 @@
+﻿[WebGL] lightgl.js でトーラスを描いてみるテスト
+
+＜対応した点＞
+・lightgl.js の OpenGL immediate mode を使用してトーラス（Torus）を描くよう対応
+　x = (R + r cos(v)) cos(u)
+　y = (R + r cos(v)) sin(u)
+　z = r sin(v)
+
+　u is an element of the set of numbers [0, 2 pi]
+　v is an element of the set of numbers [0, 2 pi]
+ 
+＜参考＞
+■ Torus
+http://www.3d-meier.de/tut3/Seite58.html
+
+■ Parametrische Flächen und Körper
+http://www.3d-meier.de/tut3/Seite0.html
