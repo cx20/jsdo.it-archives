9692baf9f8ea54862d6eac77d844cbd4cc7d6720
cx20 <y-shimano@hondarer-soft.com>
2019-10-27 21:10:24 +0900

    [WebGL] lightgl.js で球体を描いてみるテスト

---
+++ cx20/lQVx/index.js
@@ -0,0 +1,55 @@
+// forked from cx20's "[WebGL] lightgl.js で３次元リサージュ図形を描いてみるテスト" http://jsdo.it/cx20/5RXU
+// forked from cx20's "[WebGL] lightgl.js でリサージュ図形を描いてみるテスト" http://jsdo.it/cx20/4kX2
+// forked from cx20's "[WebGL] lightgl.js を試してみるテスト" http://jsdo.it/cx20/rm4H
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+
+var angle = 0;
+var gl = GL.create();
+
+gl.onupdate = function(seconds) {
+    angle += 45 * seconds;
+};
+
+gl.ondraw = function() {
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.loadIdentity();
+    gl.translate(0, 0, -2);
+    gl.rotate(30, 1, 0, 0);
+    gl.rotate(angle, 0, 1, 0);
+
+    // 正弦波×余弦波の座標データを用意
+    //             1.0 y 
+    //              ^  -1.0 
+    //              | / z
+    //              |/       x
+    // -1.0 -----------------> +1.0
+    //            / |
+    //      +1.0 /  |
+    //           -1.0
+    // 
+    //             [3]
+    //         [4]     [2]
+    //      [5]            [1]
+    //      *                *
+    //     [6]               [0]
+    //      *                *
+    //      [7]            [11]
+    //         [8]     [10]
+    //             [9]
+    //
+    var MAX = 360;
+    var A = 15.0;
+    var B = 16.0;
+    gl.begin(gl.LINE_STRIP);
+    for ( var i = 0; i <= MAX; i++ ) {
+        var x = 0.5 * Math.sin(2 * Math.PI * i / MAX * A) * Math.cos(2 * Math.PI * i / MAX * B);
+        var y = 0.5 * Math.sin(2 * Math.PI * i / MAX * A) * Math.sin(2 * Math.PI * i / MAX * B);
+        var z = 0.5 * Math.cos(2 * Math.PI * i / MAX * A);
+        gl.color(x+0.5, y+0.5, z+0.5, 1.0);
+        gl.vertex(x, y, z);
+    }
+    gl.end();
+};
+
+gl.fullscreen();
+gl.animate();
---
+++ cx20/lQVx/readme.txt
@@ -0,0 +1,16 @@
+﻿[WebGL] lightgl.js で球体を描いてみるテスト
+
+＜対応した点＞
+・lightgl.js の OpenGL immediate mode を使用して球体を描くよう対応。
+・座標計算は球座標の公式を使用。
+
+　■ 極座標系 - Wikipedia
+　http://ja.wikipedia.org/wiki/%E6%A5%B5%E5%BA%A7%E6%A8%99%E7%B3%BB
+　
+　　x = r sinθcosφ
+　　y = r sinθsinφ
+　　z = r cosθ
+
+＜参考＞
+■ [簡易版] WebGL で球体を描いてみるテスト
+http://jsdo.it/cx20/gIb0
