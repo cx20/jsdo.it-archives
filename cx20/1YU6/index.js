239b53be5c1b07bc81a7267d7fa728fa8113a765
cx20 <y-shimano@hondarer-soft.com>
2019-10-27 21:11:56 +0900

    [WebGL] lightgl.js で Wave Ball を描いてみるテスト

---
+++ cx20/1YU6/index.js
@@ -0,0 +1,55 @@
+// forked from cx20's "[WebGL] lightgl.js でトーラスを描いてみるテスト" http://jsdo.it/cx20/sssl
+// forked from cx20's "[WebGL] lightgl.js でリンゴ曲面を描いてみるテスト" http://jsdo.it/cx20/pejt
+// forked from cx20's "[WebGL] lightgl.js で貝殻曲面を描いてみるテスト" http://jsdo.it/cx20/lyR5
+// forked from cx20's "[WebGL] lightgl.js でローマ曲面を描いてみるテスト" http://jsdo.it/cx20/v0rS
+// forked from cx20's "[WebGL] lightgl.js で３次元リサージュ図形を描いてみるテスト" http://jsdo.it/cx20/5RXU
+// forked from cx20's "[WebGL] lightgl.js でリサージュ図形を描いてみるテスト" http://jsdo.it/cx20/4kX2
+// forked from cx20's "[WebGL] lightgl.js を試してみるテスト" http://jsdo.it/cx20/rm4H
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+
+var angle = 0;
+var gl = GL.create();
+
+gl.onupdate = function(seconds) {
+    angle += 45 * seconds;
+};
+
+gl.ondraw = function() {
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.loadIdentity();
+    gl.translate(0, 0, -2);
+    gl.rotate(60, 1, 0, 0);
+    gl.rotate(angle, angle, angle, 1);
+
+    // Wave Ball の座標データを用意
+    //             1.0 y 
+    //              ^  -1.0 
+    //              | / z
+    //              |/       x
+    // -1.0 -----------------> +1.0
+    //            / |
+    //      +1.0 /  |
+    //           -1.0
+    // 
+    gl.begin(gl.POINTS);
+    gl.pointSize(1.0);
+    var ustep = 0.1;
+    var vstep = Math.PI * 5 / 180;
+    for (var v = 0; v <= 2 * Math.PI; v += vstep) {
+        for (var u = 0; u <= 14.5; u += ustep) {
+            var x = u * Math.cos(Math.cos(u)) * Math.cos(v);
+            var y = u * Math.cos(Math.cos(u)) * Math.sin(v);
+            var z = u * Math.sin(Math.cos(u));
+            var x2 = x/25;
+            var y2 = y/25;
+            var z2 = z/25;
+            gl.color(x2 + 0.5, y2 + 0.5, z2 + 0.5, 1.0);
+            gl.vertex(x2, y2, z2);
+        }
+    }
+    gl.end();
+};
+
+gl.fullscreen();
+gl.animate();
---
+++ cx20/1YU6/readme.txt
@@ -0,0 +1,19 @@
+﻿[WebGL] lightgl.js で Wave Ball を描いてみるテスト
+
+元のタイトルは、ドイツ語で「Wellenkugel」、英語だと「Wave Ball」となるようです。
+
+＜対応した点＞
+・lightgl.js の OpenGL immediate mode を使用して Wave Ball を描くよう対応
+　x = u cos(cos(u)) cos(v)
+　y = u cos(cos(u)) sin(v)
+　z = u sin(cos(u))
+
+　u is an element of the set of numbers [0, 14.5]
+　v is an element of the set of numbers [0, 2 pi]
+
+＜参考＞
+■ Wellenkugel
+http://www.3d-meier.de/tut3/Seite63.html
+
+■ Parametrische Flächen und Körper
+http://www.3d-meier.de/tut3/Seite0.html
