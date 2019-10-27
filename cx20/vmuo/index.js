de884dab3dc0ac554bd3fab0ec0e3dc10e98ac9f
cx20 <y-shimano@hondarer-soft.com>
2019-10-27 22:34:51 +0900

    pixi.js v2 を試してみるテスト

---
+++ cx20/vmuo/index.js
@@ -0,0 +1,91 @@
+// create an new instance of a pixi stage
+var stage = new PIXI.Stage(0x0, true);
+
+// create a renderer instance
+var renderer = PIXI.autoDetectRenderer(465, 465);
+
+// add the renderer view element to the DOM
+renderer.view.style.position = "absolute";
+renderer.view.style.top = "0px";
+renderer.view.style.left = "0px";
+document.body.appendChild(renderer.view);
+
+/// smoke shader..
+var uniforms = {};
+
+uniforms.resolution = { type: '2f', value: { x: 465, y: 465 } };
+uniforms.alpha = { type: '1f', value: 1.0 };
+uniforms.shift = { type: '1f', value: 1.6 };
+uniforms.time = { type: '1f', value: 0 };
+uniforms.speed = { type: '2f', value: { x: 0.7, y: 0.4 } };
+
+var fragmentSrc = document.getElementById("fs1").text.split("\n");
+var coolFilter = new PIXI.AbstractFilter(fragmentSrc, uniforms);
+
+var uniforms = {};
+uniforms.uScan = {type: 'sampler2D', value:new PIXI.Texture.fromImage("scan.png")};
+uniforms.power = { type: '1f', value: 3/465 };
+uniforms.noise = { type: '1f', value: 0.2 };
+
+//// grain filter..
+
+var fragmentSrc = document.getElementById("fs2").text.split("\n");
+var scanFilter = new PIXI.AbstractFilter(fragmentSrc, uniforms);
+
+// console.log(fragmentSource);
+//var bg = PIXI.Sprite.fromImage("test_BG.jpg");
+var bg = PIXI.Sprite.fromImage("../../assets/9/7/E/z/97EzZ.jpg");
+bg.position.y = (465-432)/2; // 背景画像をセンタリング（(キャンバスサイズ-背景画像サイズ)/2）
+bg.shader = coolFilter;
+
+stage.addChild(bg);
+
+//var logo = PIXI.Sprite.fromImage("pixiJsV2.png");
+var logo = PIXI.Sprite.fromImage("../../assets/u/e/E/Z/ueEZK.png");
+logo.position.x = 465/2;
+logo.position.y = 465/2;
+
+logo.anchor.set(0.5);
+logo.blendMode = PIXI.blendModes.ADD
+stage.addChild(logo);
+
+stage.filters = [scanFilter];
+
+var count = 0;
+
+var count2 = 0;
+var ease = 0;
+
+// var targetValue
+function animate() {
+   
+    count += 0.01;
+
+    coolFilter.uniforms.time.value = count;
+    coolFilter.syncUniforms();
+
+    count2 ++;
+
+    if(count2 > 150)
+    {
+        count2 = 0;
+        ease = ( Math.random() * 30) /// 630;
+    }
+
+    ease *= 0.9;
+
+    scanFilter.uniforms.power.value += (ease -  scanFilter.uniforms.power.value ) * 0.3;
+
+    ease =  0.8 + Math.random() * 0.2;
+
+    logo.alpha += (ease - logo.alpha ) * 0.1
+    scanFilter.uniforms.noise.value += 0.1;
+    scanFilter.uniforms.noise.value %= 1;
+    scanFilter.syncUniforms();
+
+    renderer.render(stage);
+    
+    requestAnimFrame(animate);
+}
+
+requestAnimFrame(animate);
---
+++ cx20/vmuo/readme.txt
@@ -0,0 +1,19 @@
+﻿pixi.js v2 を試してみるテスト
+
+pixi.js は Goodboy Digital 社による 2D 描画用ライブラリです。
+最近、v2 が出たようなので試してみました（サンプルを移植しただけですが。。。）
+v2 より GLSL シェーダが指定できるようなったようです。
+
+＜参考＞
+■ Pixi.js v2 the fastest 2D webGL renderer around - Goodboy
+http://www.goodboydigital.com/pixi-js-v2-fastest-2d-webgl-renderer/
+
+■ pixi.js example 
+http://www.goodboydigital.com/pixijs/examples/25/
+
+■ GoodBoyDigital/pixi.js
+https://github.com/GoodBoyDigital/pixi.js/
+
+＜旧バージョンサンプル＞
+■ pixi.js でドット絵を変形させるテスト
+http://jsdo.it/cx20/3vKz
