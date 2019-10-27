d6c727b7ec944e4f105fa1fb2f6184ec9f9ff368
cx20 <y-shimano@hondarer-soft.com>
2019-10-27 20:49:57 +0900

    [WebGL] lightgl.js を試してみるテスト（その４）

---
+++ cx20/u16o/index.js
@@ -0,0 +1,34 @@
+// forked from cx20's "[WebGL] lightgl.js を試してみるテスト（その３）" http://jsdo.it/cx20/elUj
+// forked from cx20's "[WebGL] lightgl.js を試してみるテスト（その２）" http://jsdo.it/cx20/ejpI
+// forked from cx20's "[WebGL] lightgl.js を試してみるテスト" http://jsdo.it/cx20/rm4H
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+
+var gl = GL.create();
+var mesh = GL.Mesh.cube({coords: true});
+var texture = GL.Texture.fromURL('/assets/A/k/w/j/AkwjW.jpg');    // frog.jpg
+
+var vs = document.getElementById("vs").textContent;
+var fs = document.getElementById("fs").textContent;
+var shader = new GL.Shader(vs, fs);
+
+var angle = 0;
+gl.onupdate = function(seconds) {
+    angle += 1.0;
+};
+
+gl.ondraw = function() {
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.loadIdentity();
+    gl.translate(0, 0, -5);
+    gl.rotate(angle, angle, angle, 0);
+
+    texture.bind(0);
+    shader.uniforms({
+        texture: 0
+    }).draw(mesh);
+};
+
+gl.enable(gl.DEPTH_TEST);
+gl.fullscreen();
+gl.animate();
---
+++ cx20/u16o/readme.txt
@@ -0,0 +1,17 @@
+﻿[WebGL] lightgl.js を試してみるテスト（その４）
+
+＜対応した点＞
+・テクスチャ画像を立方体に貼り付けるよう対応
+
+＜参考＞
+■ evanw/lightgl.js
+https://github.com/evanw/lightgl.js/
+
+■ Learning WebGL — lesson 5
+http://learningwebgl.com/lessons/lesson05/index.html
+
+■ [簡易版] WebGL でテクスチャ付き立方体を回転させてみるテスト（glMatrix.js v2.2.2編）
+http://jsdo.it/cx20/jqD6
+
+■ WebGL 軽量ライブラリを比較してみる - Qiita
+http://qiita.com/cx20/items/0fa19c96aa6470d98807
