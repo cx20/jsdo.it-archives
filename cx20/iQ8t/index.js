c6ec9f5f94dbcb50b9c9e16b09546422dd02d903
cx20 <y-shimano@hondarer-soft.com>
2019-10-30 05:52:27 +0900

    [WebGL] Grimoire.js + forward-shading でマテリアルをいろいろ試してみるテスト（その２）（調整中）

--- cx20/iQ8t/index.js
+++ cx20/iQ8t/index.js
@@ -92,7 +92,7 @@ gr.registerComponent('Rotate', {
       isInitialized = false;
       isTextureLoaded = true;
     }
-    img.src = "http://jsrun.it/assets/6/c/3/X/6c3XK.png"; // heightmap_256x256.png
+    img.src = "../../assets/6/C/3/X/6C3XK.png"; // heightmap_256x256.png
   },
   $update: function () {
     var gr = this.companion.get("GeometryRegistory");
