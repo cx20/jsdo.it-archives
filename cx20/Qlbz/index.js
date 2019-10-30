0ebcda115d9a90a77d2bf35729552b808f2c68dd
cx20 <y-shimano@hondarer-soft.com>
2019-10-31 06:00:46 +0900

    [WebGL] A-Frame を試してみるテスト（BufferGeometry編）（その２）

---
+++ cx20/Qlbz/index.js
@@ -0,0 +1,5 @@
+// forked from cx20's "[WebGL] A-Frame を試してみるテスト（BufferGeometry編）" http://jsdo.it/cx20/W5J3
+// forked from cx20's "[WebGL] A-Frame を試してみるテスト（改）" http://jsdo.it/cx20/iFPs
+// forked from cx20's "[WebGL] A-Frame を試してみるテスト" http://jsdo.it/cx20/AOwl
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+// nothing
---
+++ cx20/Qlbz/readme.txt
@@ -0,0 +1,12 @@
+﻿[WebGL] A-Frame を試してみるテスト（BufferGeometry編）（その２）
+
+＜対応した点＞
+・組み込みシェーダでなく ShaderMaterial() を用いて自前のシェーダを使うよう対応。
+・色設定用の頂点バッファを追加。
+
+＜参考＞
+■ [WebGL] three.js を試してみるテスト（BufferGeometry編）（その２）
+http://jsdo.it/cx20/vryW
+
+■ 各種 WebGL ライブラリで基本図形を表示してみる - Qiita
+http://qiita.com/cx20/items/0fa19c96aa6470d98807
