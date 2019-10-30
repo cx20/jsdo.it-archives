975c9226d43f4779883588153526af5ae3c3944a
cx20 <y-shimano@hondarer-soft.com>
2019-10-31 05:31:19 +0900

    [WebGL] A-Frame で glTF 2.0形式のデータを表示してみるテスト（その５）

---
+++ cx20/ov9L/index.js
@@ -0,0 +1,11 @@
+// forked from cx20's "[WebGL] A-Frame で glTF 2.0形式のデータを表示してみるテスト（その４）（調整中）" http://jsdo.it/cx20/yCwk
+// forked from cx20's "[WebGL] A-Frame で glTF 2.0形式のデータを表示してみるテスト（その３）" http://jsdo.it/cx20/O3gd
+// forked from cx20's "[WebGL] A-Frame で glTF 2.0形式のデータを表示してみるテスト（その２）" http://jsdo.it/cx20/G1CS
+// forked from cx20's "[WebGL] A-Frame で glTF 2.0形式のデータを表示してみるテスト" http://jsdo.it/cx20/Aoc0
+// forked from cx20's "[WebGL] A-Frame + glTF ファイルを試してみるテスト" http://jsdo.it/cx20/wHVp
+// forked from cx20's "[WebGL] A-Frame を試してみるテスト（その４）" http://jsdo.it/cx20/k7B2
+// forked from cx20's "[WebGL] A-Frame を試してみるテスト（その３）" http://jsdo.it/cx20/qgOl
+// forked from cx20's "[WebGL] A-Frame を試してみるテスト（その２）" http://jsdo.it/cx20/OclK
+// forked from cx20's "[WebGL] A-Frame を試してみるテスト" http://jsdo.it/cx20/AOwl
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+// nothing
---
+++ cx20/ov9L/readme.txt
@@ -0,0 +1,10 @@
+﻿[WebGL] A-Frame で glTF 2.0形式のデータを表示してみるテスト（その５）
+
+A-Frame は WebVR に対応したタグベースの WebGL フレームワークです。レンダリングには three.js が使用されています。
+
+＜対応した点＞
+・Three.js glTF Exporter で出力した glTF ファイルを A-Frmae で表示するよう対応。
+
+＜参考＞
+■ aframevr/aframe
+https://github.com/aframevr/aframe
