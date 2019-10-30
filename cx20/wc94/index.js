90d895f14dcbc81b5f597d729bb48b8b976b351a
cx20 <y-shimano@hondarer-soft.com>
2019-10-31 04:52:05 +0900

    [WebGL] p5.js を試してみるテスト（その２）

---
+++ cx20/wc94/index.js
@@ -0,0 +1,41 @@
+// forked from cx20's "[WebGL] p5.js を試してみるテスト" http://jsdo.it/cx20/uwAD
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+
+function setup(){
+    createCanvas(465, 465, 'webgl');
+}
+
+function draw(){
+    background(255, 255, 255);
+    fill(0, 0, 255);
+
+    beginShape('TRIANGLE_STRIP');
+    vertex(-230, -230, 0);
+    vertex( 230, -230, 0);
+    vertex(-230,  230, 0);
+    vertex( 230,  230, 0);
+    endShape(CLOSE);
+}
+
+/*
+function setup(){
+    createGraphics(465, 465, 'webgl');
+    background(255, 255, 255);
+    noStroke();
+    windowResized(); // 何故かリサイズしないと再描画しなかった為、追加
+}
+
+function draw(){
+    beginShape(TRIANGLE_STRIP);
+    fill(0, 0, 255);
+    vertex(115, 115);
+    vertex(350, 115);
+    vertex(115, 350);
+    vertex(350, 350);
+    endShape(CLOSE);
+}
+
+function windowResized() {
+    resizeCanvas(465, 465);
+}
+*/
---
+++ cx20/wc94/readme.txt
@@ -0,0 +1,27 @@
+﻿[WebGL] p5.js を試してみるテスト（その２）
+
+＜対応した点＞
+・四角形を描くよう対応
+
+＜対応できていない点＞
+・頂点バッファ毎に色を設定する方法が分からなかった。
+
+＜ライブラリの仕様なのかバグなのか不明な点＞
+・createCanvas() と createGraphics() で 'webgl' 指定した場合の座標系が異なる模様。
+　createCanvas　：中心が (0,0)
+　createGraphics：左上が (0,0)
+・createCanvas() + 'webgl' を使用した場合、TRIANGLE_STRIP オプションを文字定数で指定しないと効かない模様。
+
+＜変更履歴＞
+・2015/09/25 createGraphics()→createCanvas(), vertex(x,y)→vertex(x,y,z)
+・2015/09/23 初版作成
+
+＜参考＞
+■ [WebGL] Processing.js を試してみるテスト（その２）
+http://jsdo.it/cx20/eA2K
+
+■ processing/p5.js
+https://github.com/processing/p5.js
+
+■ Draft of getting started with webgl in p5js doc.
+https://gist.github.com/indefinit/45a78da8d7c943b22dc1
