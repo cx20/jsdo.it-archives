0f38a745b2743163d8e1acaa32ee7dec21f680fb
cx20 <y-shimano@hondarer-soft.com>
2019-10-31 05:42:45 +0900

    [WebGL] A-Frame で glTF 2.0形式のデータを表示してみるテスト（その１５）（調整中）

---
+++ cx20/YuEt/index.js
@@ -0,0 +1,23 @@
+// forked from cx20's "[WebGL] A-Frame で glTF 2.0形式のデータを表示してみるテスト（その１４）（調整中）" http://jsdo.it/cx20/KgFg
+// forked from cx20's "[WebGL] A-Frame で glTF 2.0形式のデータを表示してみるテスト（その１３）（調整中）" http://jsdo.it/cx20/kLKw
+// forked from cx20's "[WebGL] A-Frame で glTF 2.0形式のデータを表示してみるテスト（その１２）（調整中）" http://jsdo.it/cx20/Cdbo
+// forked from cx20's "[WebGL] A-Frame で glTF 2.0形式のデータを表示してみるテスト（その１１）（調整中）" http://jsdo.it/cx20/Ydw4
+// forked from cx20's "[WebGL] A-Frame で glTF 2.0形式のデータを表示してみるテスト（その１０）（調整中）" http://jsdo.it/cx20/udGG
+// forked from cx20's "[WebGL] A-Frame で glTF 2.0形式のデータを表示してみるテスト（その９）（調整中）" http://jsdo.it/cx20/qHxP
+// forked from cx20's "[WebGL] A-Frame で glTF 2.0形式のデータを表示してみるテスト（その８）" http://jsdo.it/cx20/IAYh
+// forked from cx20's "[WebGL] A-Frame で glTF 2.0形式のデータを表示してみるテスト（その７）" http://jsdo.it/cx20/K9F1
+// forked from cx20's "[WebGL] A-Frame で glTF 2.0形式のデータを表示してみるテスト（その６）" http://jsdo.it/cx20/42Db
+// forked from cx20's "[WebGL] A-Frame で glTF 2.0形式のデータを表示してみるテスト（その５）" http://jsdo.it/cx20/ov9L
+// forked from cx20's "[WebGL] A-Frame で glTF 2.0形式のデータを表示してみるテスト（その４）（調整中）" http://jsdo.it/cx20/yCwk
+// forked from cx20's "[WebGL] A-Frame で glTF 2.0形式のデータを表示してみるテスト（その３）" http://jsdo.it/cx20/O3gd
+// forked from cx20's "[WebGL] A-Frame で glTF 2.0形式のデータを表示してみるテスト（その２）" http://jsdo.it/cx20/G1CS
+// forked from cx20's "[WebGL] A-Frame で glTF 2.0形式のデータを表示してみるテスト" http://jsdo.it/cx20/Aoc0
+// forked from cx20's "[WebGL] A-Frame + glTF ファイルを試してみるテスト" http://jsdo.it/cx20/wHVp
+// forked from cx20's "[WebGL] A-Frame を試してみるテスト（その４）" http://jsdo.it/cx20/k7B2
+// forked from cx20's "[WebGL] A-Frame を試してみるテスト（その３）" http://jsdo.it/cx20/qgOl
+// forked from cx20's "[WebGL] A-Frame を試してみるテスト（その２）" http://jsdo.it/cx20/OclK
+// forked from cx20's "[WebGL] A-Frame を試してみるテスト" http://jsdo.it/cx20/AOwl
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+// nothing
+
+document.querySelector("a-scene").renderer.gammaOutput = true;
---
+++ cx20/YuEt/readme.txt
@@ -0,0 +1,17 @@
+﻿[WebGL] A-Frame で glTF 2.0形式のデータを表示してみるテスト（その１５）（調整中）
+
+＜対応した点＞
+・glTF Draco拡張のモデルを表示するよう対応。
+
+＜対応できていない点＞
+・A-Frame で Draco 形式のファイルを表示させる方法が分かっていない。
+
+＜使用モデル＞
+■ Littlest Tokyo - Sketchfab
+https://sketchfab.com/models/94b24a60dc1b48248de50bf087c0f042
+■ LittlestTokyo.glb（Draco形式）
+https://github.com/mrdoob/three.js/blob/dev/examples/models/gltf/LittlestTokyo.glb
+
+＜他ライブラリ比較＞
+■ [WebGL] three.js で glTF 2.0形式のデータを表示してみるテスト（その１５）（調整中）
+http://jsdo.it/cx20/IviI
