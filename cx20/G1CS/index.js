d1eb71cbf6ceb87e9237ad3c0b063a414c1d3fae
cx20 <y-shimano@hondarer-soft.com>
2019-10-31 05:28:34 +0900

    [WebGL] A-Frame で glTF 2.0形式のデータを表示してみるテスト（その２）

---
+++ cx20/G1CS/index.js
@@ -0,0 +1,8 @@
+// forked from cx20's "[WebGL] A-Frame で glTF 2.0形式のデータを表示してみるテスト" http://jsdo.it/cx20/Aoc0
+// forked from cx20's "[WebGL] A-Frame + glTF ファイルを試してみるテスト" http://jsdo.it/cx20/wHVp
+// forked from cx20's "[WebGL] A-Frame を試してみるテスト（その４）" http://jsdo.it/cx20/k7B2
+// forked from cx20's "[WebGL] A-Frame を試してみるテスト（その３）" http://jsdo.it/cx20/qgOl
+// forked from cx20's "[WebGL] A-Frame を試してみるテスト（その２）" http://jsdo.it/cx20/OclK
+// forked from cx20's "[WebGL] A-Frame を試してみるテスト" http://jsdo.it/cx20/AOwl
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+// nothing
---
+++ cx20/G1CS/readme.txt
@@ -0,0 +1,10 @@
+﻿[WebGL] A-Frame で glTF 2.0形式のデータを表示してみるテスト（その２）
+
+A-Frame は WebVR に対応したタグベースの WebGL フレームワークです。レンダリングには three.js が使用されています。
+
+＜対応した点＞
+・表示モデルを TextureSettingsTest.gltf に変更
+
+＜参考＞
+■ aframevr/aframe
+https://github.com/aframevr/aframe
