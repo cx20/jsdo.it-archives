2bab6a5e11da461205f5198d0142eb0fedc493df
cx20 <y-shimano@hondarer-soft.com>
2019-11-02 23:03:37 +0900

    [WebGL] WWG.js を試してみるテスト（その２）

---
+++ cx20/gDNn/index.js
@@ -0,0 +1,63 @@
+// forked from cx20's "[WebGL] WWG.js を試してみるテスト" http://jsdo.it/cx20/E96S
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+var render = {
+    env: {
+        clear_color: [1.0, 1.0, 1.0, 1.0]
+    },
+    vshader: {
+        text: document.getElementById('vshader').innerText
+    },
+    fshader: {
+        text: document.getElementById('fshader').innerText
+    },
+    vs_uni: {
+        mvpMatrix: [
+            1, 0, 0, 0,
+            0, 1, 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1
+        ]
+    },
+    model: [{
+        geo: {
+            mode: "tri_strip",
+            vtx_at: ["position", "color"],
+            // 正方形の座標データを用意
+            //             1.0 y 
+            //              ^  -1.0 
+            //              | / z
+            //              |/       x
+            // -1.0 -----------------> +1.0
+            //            / |
+            //      +1.0 /  |
+            //           -1.0
+            // 
+            //        [0]------[1]
+            //         |        |
+            //         |        |
+            //         |        |
+            //        [2]------[3]
+            //
+            vtx: [
+                // position      color
+                // ------------  -------------------
+                -0.5, 0.5, 0.0,  1.0, 0.0, 0.0, 1.0,
+                 0.5, 0.5, 0.0,  0.0, 1.0, 0.0, 1.0,
+                -0.5,-0.5, 0.0,  0.0, 0.0, 1.0, 1.0,
+                 0.5,-0.5, 0.0,  1.0, 1.0, 0.0, 1.0
+            ],
+            idx: [
+                0, 1, 2, 3
+            ]
+        }
+    }]
+};
+var wwg = new WWG();
+wwg.init(document.getElementById('screen1'));
+var r = wwg.createRender();
+r.setRender(render).then(function() {
+    console.log(r);
+    r.draw();
+}).catch(function(err) {
+    console.log(err);
+});
---
+++ cx20/gDNn/readme.txt
@@ -0,0 +1,19 @@
+﻿[WebGL] WWG.js を試してみるテスト（その２）
+
+WWG.js は wakufactory さんによるシンプルな WebGL ラッパーライブラリです。
+
+＜対応した点＞
+・色設定用の頂点バッファを追加。
+・GLSL のシェーダに色設定用変数を追加
+　＜頂点シェーダ＞
+　attribute vec4 color
+　varying vec4 vColor;
+　＜フラグメントシェーダ＞
+　varying vec4 vColor;
+
+＜参考＞
+■ wakufactory/wwg
+https://github.com/wakufactory/wwg
+
+■ 各種 WebGL ライブラリで基本図形を表示してみる - Qiita
+http://qiita.com/cx20/items/0fa19c96aa6470d98807
