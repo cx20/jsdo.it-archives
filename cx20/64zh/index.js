33251b90c7aed6ab8ef7cefae864b67c122a02e0
cx20 <y-shimano@hondarer-soft.com>
2019-11-02 23:08:01 +0900

    [WebGL] WWG.js を試してみるテスト（その４）

---
+++ cx20/64zh/index.js
@@ -0,0 +1,133 @@
+// forked from cx20's "[WebGL] WWG.js を試してみるテスト（その３）（調整中）" http://jsdo.it/cx20/EZAw
+// forked from cx20's "[WebGL] WWG.js を試してみるテスト（その２）" http://jsdo.it/cx20/gDNn
+// forked from cx20's "[WebGL] WWG.js を試してみるテスト" http://jsdo.it/cx20/E96S
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+
+var render = {
+    env: {
+        clear_color: [1.0, 1.0, 1.0, 1.0]
+    },
+    vshader: {
+        text: document.getElementById('vshader').innerText
+    },
+    fshader: {
+        text: document.getElementById('fshader').innerText
+    },
+    vs_uni: {
+        mvpMatrix: [
+            1, 0, 0, 0,
+            0, 1, 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1
+        ]
+    },
+    fs_uni:{
+        tex: 0
+    },
+    texture: [{
+        src: "../../assets/A/k/w/j/AkwjW.jpg", // frog.jpg
+        opt: {
+            flevel: 2,
+            anisotropy: 4
+        }
+    }],
+    model: [{
+        geo: {
+            mode: "tri", // TRIANGLES
+            vtx_at: ["position", "textureCoord"],
+            // 立方体の座標データを用意
+            //             1.0 y 
+            //              ^  -1.0 
+            //              | / z
+            //              |/       x
+            // -1.0 -----------------> +1.0
+            //            / |
+            //      +1.0 /  |
+            //           -1.0
+            // 
+            //         [7]------[6]
+            //        / |      / |
+            //      [3]------[2] |
+            //       |  |     |  |
+            //       | [4]----|-[5]
+            //       |/       |/
+            //      [0]------[1]
+            //
+            vtx: [
+                // Front face
+                -0.5, -0.5,  0.5,   0.0, 0.0,
+                 0.5, -0.5,  0.5,   1.0, 0.0,
+                 0.5,  0.5,  0.5,   1.0, 1.0,
+                -0.5,  0.5,  0.5,   0.0, 1.0,
+                // Back face
+                -0.5, -0.5, -0.5,   1.0, 0.0,
+                 0.5, -0.5, -0.5,   1.0, 1.0,
+                 0.5,  0.5, -0.5,   0.0, 1.0,
+                -0.5,  0.5, -0.5,   0.0, 0.0,
+                // Top face
+                 0.5,  0.5,  0.5,   0.0, 1.0,
+                -0.5,  0.5,  0.5,   0.0, 0.0,
+                -0.5,  0.5, -0.5,   1.0, 0.0,
+                 0.5,  0.5, -0.5,   1.0, 1.0,
+                // Bottom face
+                -0.5, -0.5,  0.5,   1.0, 1.0,
+                 0.5, -0.5,  0.5,   0.0, 1.0,
+                 0.5, -0.5, -0.5,   0.0, 0.0,
+                -0.5, -0.5, -0.5,   1.0, 0.0,
+                 // Right face
+                 0.5, -0.5,  0.5,   1.0, 0.0,
+                 0.5,  0.5,  0.5,   1.0, 1.0,
+                 0.5,  0.5, -0.5,   0.0, 1.0,
+                 0.5, -0.5, -0.5,   0.0, 0.0,
+                 // Left face
+                -0.5, -0.5,  0.5,   0.0, 0.0,
+                -0.5,  0.5,  0.5,   1.0, 0.0,
+                -0.5,  0.5, -0.5,   1.0, 1.0,
+                -0.5, -0.5, -0.5,   0.0, 1.0
+            ],
+            idx: [
+                 0,  1,  2,    0,  2 , 3,  // Front face
+                 4,  5,  6,    4,  6 , 7,  // Back face
+                 8,  9, 10,    8, 10, 11,  // Top face
+                12, 13, 14,   12, 14, 15,  // Bottom face
+                16, 17, 18,   16, 18, 19,  // Right face
+                20, 21, 22,   20, 22, 23   // Left face
+            ]
+        }
+    }]
+};
+
+var wwg = new WWG();
+var can = document.getElementById('screen1');
+wwg.init(can);
+var r = wwg.createRender();
+var p = {
+    camRX: 30,
+    camRY: -30,
+    rotX: 0,
+    rotY: 0,
+    ofsY: 0
+};
+r.setRender(render).then(function() {
+    var st = new Date().getTime();
+    var lt = st;
+    (function loop() {
+        window.requestAnimationFrame(loop);
+        var ct = new Date().getTime();
+        var tint = (ct - st);
+        //if ((ct - lt) < 60) return;
+
+        r.draw({
+            vs_uni: {
+                mvpMatrix: new CanvasMatrix4().
+                    rotate(tint / 20 - 90, 1, 1, 0).    // rotate(angle,x,y,z)
+                    lookat(0, 0, 3, 0, 0, 0, 0, 1, 0).  // lookat(eyex, eyey, eyez, centerx, centery, centerz, upx, upy, upz)
+                    perspective(40, 1.0, 0.1, 1000).    // perspective(fovy, aspect, zNear, zFar)
+                    getAsWebGLFloatArray()
+            }
+        });
+        lt = ct;
+    })();
+}).catch(function(err) {
+    console.log(err);
+});
---
+++ cx20/64zh/readme.txt
@@ -0,0 +1,14 @@
+﻿[WebGL] WWG.js を試してみるテスト（その４）
+
+WWG.js は wakufactory さんによるシンプルな WebGL ラッパーライブラリです。
+
+＜対応した点＞
+・テクスチャ画像を立方体に貼り付けるよう対応
+・ライブラリの行列演算関数を用いて回転させるよう対応。
+
+＜参考＞
+■ wakufactory/wwg
+https://github.com/wakufactory/wwg
+
+■ 各種 WebGL ライブラリで基本図形を表示してみる - Qiita
+http://qiita.com/cx20/items/0fa19c96aa6470d98807
