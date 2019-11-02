2418bee3a870b0945359daba02d6a8b64cc24713
cx20 <y-shimano@hondarer-soft.com>
2019-11-02 22:29:55 +0900

    [WebGL] glTips.js を試してみるテスト（その２）

---
+++ cx20/q7tP/index.js
@@ -0,0 +1,102 @@
+// forked from cx20's "[WebGL] glTips.js を試してみるテスト" http://jsdo.it/cx20/OKDl
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+
+var c, gl;
+var aLoc = [];
+var fps = 1000 / 30;
+
+function initWebGL() {
+    c = document.getElementById("c");
+    // WebGL1 コンテキストを取得
+    gl = getWebGL1Context(c);                               // glTips#getWebGL1Context()
+    resizeCanvas();
+    window.addEventListener("resize", function(){
+        resizeCanvas();
+    });
+}
+
+function resizeCanvas() {
+    c.width = window.innerWidth;
+    c.height = window.innerHeight;
+    gl.viewport(0, 0, c.width, c.height);
+}
+
+function initShaders() {
+    var vsText = document.getElementById("vs").textContent;
+    var fsText = document.getElementById("fs").textContent;
+/*
+    // WebGLShader オブジェクトを文字列を元に準備
+    var vs = compileShader(gl, gl.VERTEX_SHADER, vsText);     // glTips#compileShader()
+    var fs = compileShader(gl, gl.FRAGMENT_SHADER, fsText);   // glTips#compileShader()
+    // WebGLProgram オブジェクトをセットアップ
+    var p = setupShaderProgram(gl, vs, fs);                   // glTips#setupShaderProgram()
+*/
+    // WebGLProgram オブジェクトを文字列を元にセットアップ
+    var p = setupShaderProgramFromSource(gl, vsText, fsText); // glTips#setupShaderProgramFromSource()
+
+    // WebGLProgram オブジェクトをアクティブにする
+    gl.useProgram(p);
+    aLoc[0] = gl.getAttribLocation(p, "position");
+    gl.enableVertexAttribArray(aLoc[0]);
+    aLoc[1] = gl.getAttribLocation(p, "color");
+    gl.enableVertexAttribArray(aLoc[1]);
+}
+
+function initBuffers() {
+    // 正方形の座標データを用意
+    //             1.0 y 
+    //              ^  -1.0 
+    //              | / z
+    //              |/       x
+    // -1.0 -----------------> +1.0
+    //            / |
+    //      +1.0 /  |
+    //           -1.0
+    // 
+    //        [0]------[1]
+    //         |        |
+    //         |        |
+    //         |        |
+    //        [2]------[3]
+    //
+    var positions = [ 
+        -0.5, 0.5, 0.0, // v0
+         0.5, 0.5, 0.0, // v1 
+        -0.5,-0.5, 0.0, // v2
+         0.5,-0.5, 0.0  // v3
+    ];
+    
+    var colors = [ 
+         1.0, 0.0, 0.0, 1.0, // v0
+         0.0, 1.0, 0.0, 1.0, // v1
+         0.0, 0.0, 1.0, 1.0, // v2
+         1.0, 1.0, 0.0, 1.0  // v3
+    ];
+    
+    gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+    gl.vertexAttribPointer(aLoc[0], 3, gl.FLOAT, false, 0, 0);
+    
+    gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+    gl.vertexAttribPointer(aLoc[1], 4, gl.FLOAT, false, 0, 0);
+}
+
+function render() {
+    draw();
+    requestAnimationFrame(render);
+}
+
+function draw() {
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+    gl.flush();
+}
+
+window.onload = function() {
+    glTips(); // glTips をグローバル呼び出し可能とする
+
+    initWebGL();
+    initShaders();
+    initBuffers();
+    render();
+};
---
+++ cx20/q7tP/readme.txt
@@ -0,0 +1,20 @@
+﻿[WebGL] glTips.js を試してみるテスト（その２）
+
+glTips.js は WebGL の便利コードをまとめた、CC0 ライセンスな WebGL ルーチンコードスニペット集です。
+WebGL のルーチン的な API 呼び出しの手順について簡素化することができます。
+
+＜対応した点＞
+・色設定用の頂点バッファを追加。
+・GLSL のシェーダに色設定用変数を追加
+　＜頂点シェーダ＞
+　attribute vec4 color
+　varying vec4 vColor;
+　＜フラグメントシェーダ＞
+　varying vec4 vColor;
+
+＜参考＞
+■ glTips.js
+https://github.com/emadurandal/glTips
+
+■ WebGL 軽量ライブラリ比較
+http://qiita.com/cx20/items/0fa19c96aa6470d98807
