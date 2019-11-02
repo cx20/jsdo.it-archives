b6fd195c9eee51979087d949124a677898f7d607
cx20 <y-shimano@hondarer-soft.com>
2019-11-03 00:34:11 +0900

    [WebGL] webgl-utils.js を試してみるテスト（その３）

---
+++ cx20/6GRf/index.js
@@ -0,0 +1,142 @@
+// forked from cx20's "[WebGL] webgl-utils.js を試してみるテスト（その２）" http://jsdo.it/cx20/hSkH
+// forked from cx20's "[WebGL] webgl-utils.js を試してみるテスト" http://jsdo.it/cx20/d23X
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+
+var c = document.getElementById("c");
+var gl = getWebGLContext(c);
+var programInfo = createProgramInfo(gl, ["vs", "fs"]);
+
+// 立方体の座標データを用意
+//             1.0 y 
+//              ^  -1.0 
+//              | / z
+//              |/       x
+// -1.0 -----------------> +1.0
+//            / |
+//      +1.0 /  |
+//           -1.0
+// 
+//         [7]------[6]
+//        / |      / |
+//      [3]------[2] |
+//       |  |     |  |
+//       | [4]----|-[5]
+//       |/       |/
+//      [0]------[1]
+//
+var arrays = {
+    position: { 
+        numComponents: 3,
+        data: [ 
+            // Front face
+            -0.5, -0.5,  0.5, // v0
+             0.5, -0.5,  0.5, // v1
+             0.5,  0.5,  0.5, // v2
+            -0.5,  0.5,  0.5, // v3
+            // Back face
+            -0.5, -0.5, -0.5, // v4
+             0.5, -0.5, -0.5, // v5
+             0.5,  0.5, -0.5, // v6
+            -0.5,  0.5, -0.5, // v7
+            // Top face
+             0.5,  0.5,  0.5, // v2
+            -0.5,  0.5,  0.5, // v3
+            -0.5,  0.5, -0.5, // v7
+             0.5,  0.5, -0.5, // v6
+            // Bottom face
+            -0.5, -0.5,  0.5, // v0
+             0.5, -0.5,  0.5, // v1
+             0.5, -0.5, -0.5, // v5
+            -0.5, -0.5, -0.5, // v4
+             // Right face
+             0.5, -0.5,  0.5, // v1
+             0.5,  0.5,  0.5, // v2
+             0.5,  0.5, -0.5, // v6
+             0.5, -0.5, -0.5, // v5
+             // Left face
+            -0.5, -0.5,  0.5, // v0
+            -0.5,  0.5,  0.5, // v3
+            -0.5,  0.5, -0.5, // v7
+            -0.5, -0.5, -0.5  // v4
+        ]
+    },
+    color: {
+        numComponents: 4,
+        data: [ 
+            1.0, 0.0, 0.0, 1.0, // Front face
+            1.0, 0.0, 0.0, 1.0, // Front face
+            1.0, 0.0, 0.0, 1.0, // Front face
+            1.0, 0.0, 0.0, 1.0, // Front face
+            1.0, 1.0, 0.0, 1.0, // Back face
+            1.0, 1.0, 0.0, 1.0, // Back face
+            1.0, 1.0, 0.0, 1.0, // Back face
+            1.0, 1.0, 0.0, 1.0, // Back face
+            0.0, 1.0, 0.0, 1.0, // Top face
+            0.0, 1.0, 0.0, 1.0, // Top face
+            0.0, 1.0, 0.0, 1.0, // Top face
+            0.0, 1.0, 0.0, 1.0, // Top face
+            1.0, 0.5, 0.5, 1.0, // Bottom face
+            1.0, 0.5, 0.5, 1.0, // Bottom face
+            1.0, 0.5, 0.5, 1.0, // Bottom face
+            1.0, 0.5, 0.5, 1.0, // Bottom face
+            1.0, 0.0, 1.0, 1.0, // Right face
+            1.0, 0.0, 1.0, 1.0, // Right face
+            1.0, 0.0, 1.0, 1.0, // Right face
+            1.0, 0.0, 1.0, 1.0, // Right face
+            0.0, 0.0, 1.0, 1.0, // Left face
+            0.0, 0.0, 1.0, 1.0, // Left face
+            0.0, 0.0, 1.0, 1.0, // Left face
+            0.0, 0.0, 1.0, 1.0  // Left face
+        ]
+    },
+    indices: [ 
+         0,  1,  2,    0,  2 , 3,  // Front face
+         4,  5,  6,    4,  6 , 7,  // Back face
+         8,  9, 10,    8, 10, 11,  // Top face
+        12, 13, 14,   12, 14, 15,  // Bottom face
+        16, 17, 18,   16, 18, 19,  // Right face
+        20, 21, 22,   20, 22, 23   // Left face
+    ]
+};
+
+var mapping = {
+    position: "position",
+    color: "color"
+};
+
+var uniforms = {
+};
+
+var bufferInfo = createBufferInfoFromArrays(gl, arrays, mapping);
+
+var projection = makePerspective(30 * Math.PI / 180, c.clientWidth / c.clientHeight, 0.5, 1000);
+var eye = [0, 0, -4];
+var target = [0, 0, 0];
+var up = [0, 1, 0];
+
+var camera = makeLookAt(eye, target, up);
+var view = makeInverse(camera);
+var viewProjection = matrixMultiply(view, projection);
+    
+var rad = 0;
+function render() {
+    rad += Math.PI * 1.0 / 180.0;
+
+    gl.enable(gl.DEPTH_TEST);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    var world = makeIdentity();
+    world = matrixMultiply(world, makeXRotation(rad));
+    world = matrixMultiply(world, makeYRotation(rad));
+    world = matrixMultiply(world, makeZRotation(rad));
+    uniforms.u_worldViewProjection = matrixMultiply(world, viewProjection);
+
+    gl.useProgram(programInfo.program);
+    setBuffersAndAttributes(gl, programInfo.attribSetters, bufferInfo);
+    setUniforms(programInfo.uniformSetters, uniforms);
+    gl.drawElements(gl.TRIANGLES, bufferInfo.numElements, gl.UNSIGNED_SHORT, 0);
+
+    requestAnimationFrame(render);
+}
+
+render();
---
+++ cx20/6GRf/readme.txt
@@ -0,0 +1,23 @@
+﻿[WebGL] webgl-utils.js を試してみるテスト（その３）
+
+＜対応した点＞
+・立方体を webgl-3d-math.js の行列演算関数を用いて回転させるよう対応。
+
+＜対応できていない点＞
+・クォータニオンには対応していません。
+
+＜参考＞
+■ WebGL Fundamentals
+http://webglfundamentals.org/
+
+■ greggman/webgl-fundamentals
+https://github.com/greggman/webgl-fundamentals/tree/master/webgl/resources
+
+■ [簡易版] WebGL で立方体に色を付けてみるテスト
+http://jsdo.it/cx20/FPzz
+
+■ [WebGL] 行列演算ライブラリを使用してみるテスト（glMatrix.js v2.2.2編）
+http://jsdo.it/cx20/fCkX
+
+■ [WebGL] TWGL.js を試してみるテスト（その３）
+http://jsdo.it/cx20/tiBJ
