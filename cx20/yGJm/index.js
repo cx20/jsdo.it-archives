0cc7fc79835b24bb8ae5aab563425867a3cb6750
cx20 <y-shimano@hondarer-soft.com>
2019-11-03 18:22:21 +0900

    [WebGL] CubicVR.js を試してみるテスト（その３）

---
+++ cx20/yGJm/index.js
@@ -0,0 +1,100 @@
+// forked from cx20's "[WebGL] CubicVR.js を試してみるテスト（その２）" http://jsdo.it/cx20/rhim
+// forked from cx20's "[WebGL] CubicVR.js を試してみるテスト" http://jsdo.it/cx20/bOkq
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+
+function buildMesh() {
+
+
+    // 立方体の座標データを用意
+    //             1.0 y 
+    //              ^  -1.0 
+    //              | / z
+    //              |/       x
+    // -1.0 -----------------> +1.0
+    //            / |
+    //      +1.0 /  |
+    //           -1.0
+    // 
+    //         [6]------[5]
+    //        / |      / |
+    //      [2]------[1] |
+    //       |  |     |  |
+    //       | [7]----|-[4]
+    //       |/       |/
+    //      [3]------[0]
+    //
+    var mesh = new CubicVR.Mesh({
+        material: {
+            textures: {
+            },
+            colorMap: true
+        },
+        points: [
+            [ 0.5,-0.5, 0.5], // v0
+            [ 0.5, 0.5, 0.5], // v1
+            [-0.5, 0.5, 0.5], // v2
+            [-0.5,-0.5, 0.5], // v3
+            [ 0.5,-0.5,-0.5], // v4
+            [ 0.5, 0.5,-0.5], // v5
+            [-0.5, 0.5,-0.5], // v6
+            [-0.5,-0.5,-0.5]  // v7
+        ],
+        faces: [
+            [0, 1, 2, 3], // Front face
+            [7, 6, 5, 4], // Back face
+            [4, 5, 1, 0], // Right face
+            [5, 6, 2, 1], // Top face
+            [6, 7, 3, 2], // Left face
+            [7, 4, 0, 3]  // Buttom face
+        ],
+        uv: [
+            [[0, 1],[1, 1],[1, 0],[0, 0]], // Front face
+            [[0, 1],[1, 1],[1, 0],[0, 0]], // Back face
+            [[0, 1],[1, 1],[1, 0],[0, 0]], // Right face
+            [[0, 1],[1, 1],[1, 0],[0, 0]], // Top face
+            [[0, 1],[1, 1],[1, 0],[0, 0]], // Left face
+            [[0, 1],[1, 1],[1, 0],[0, 0]]  // Buttom face
+        ],
+        color: [
+            [[1, 0, 0],[1, 0, 0],[1, 0, 0],[1, 0, 0]],         // Front face
+            [[1, 1, 0],[1, 1, 0],[1, 1, 0],[1, 1, 0]],         // Back face
+            [[0, 1, 0],[0, 1, 0],[0, 1, 0],[0, 1, 0]],         // Right face
+            [[1,0.5,0.5],[1,0.5,0.5],[1,0.5,0.5],[1,0.5,0.5]], // Top face
+            [[1, 0, 1],[1, 0, 1],[1, 0, 1],[1, 0, 1]],         // Left face
+            [[0, 0, 1],[0, 0, 1],[0, 0, 1],[0, 0, 1]]          // Buttom face
+        ],
+        compile: true
+    });
+    
+    return mesh;
+}
+
+function webGLStart(gl, canvas) {
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+    
+    var scene = new CubicVR.Scene({
+        camera: {
+            width: canvas.width,
+            height: canvas.height,
+            fov: 30,
+            position: [0, 0, 4],
+            target: [0, 0, 0]
+        },
+        sceneObject: {
+            name: "cube",
+            mesh: buildMesh
+        }
+    });
+
+    var mvc = new CubicVR.MouseViewController(canvas, scene.camera);
+    var sphereObject = scene.getSceneObject("cube");
+    var angle = 0;
+
+    CubicVR.MainLoop(function(timer, gl) {
+        angle += 1.0;
+        sphereObject.rotation[0] = angle;
+        sphereObject.rotation[1] = angle;
+        sphereObject.rotation[2] = angle;
+        scene.render();
+    });
+}
---
+++ cx20/yGJm/readme.txt
@@ -0,0 +1,21 @@
+﻿[WebGL] CubicVR.js を試してみるテスト（その３）
+
+＜対応した点＞
+・立方体を組み込み型（primitive / type: "box"） でなく、自前で作成するよう対応。
+・立方体を CubicVR.js の行列演算関数を用いて回転させるよう対応。
+
+＜対応していない点＞
+・シェーダはライブラリに組み込みのものを使用しています。
+
+＜参考＞
+■ cjcliffe/CubicVR.js
+https://github.com/cjcliffe/CubicVR.js
+
+■ CubicVR 3D Engine
+http://www.cubicvr.org/
+
+■ CubicVR.js/samples/mesh_build/cube_arrays.html
+https://github.com/cjcliffe/CubicVR.js/blob/master/samples/mesh_build/cube_arrays.html
+
+■ [簡易版] WebGL で立方体に色を付けてみるテスト
+http://jsdo.it/cx20/FPzz
