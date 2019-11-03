2225849d5a06cd454c61aaebd361fdd29d7585fe
cx20 <y-shimano@hondarer-soft.com>
2019-11-03 19:06:01 +0900

    [WebGl] TinyGL.js を試してみるテスト（その２）

---
+++ cx20/c200/index.js
@@ -0,0 +1,36 @@
+// forked from cx20's "[WebGl] TinyGL.js を試してみるテスト" http://jsdo.it/cx20/eZz6
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+
+var canvas = document.getElementById("c");
+var gl = canvas.getContext("experimental-tinygl");
+
+gl.viewport(0, 0, canvas.width, canvas.height);
+
+gl.clearColor(1.0, 1.0, 1.0, 1.0);
+gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+gl.matrixMode(gl.PROJECTION);
+gl.loadIdentity();
+gl.frustum(-1.0, 1.0, -1.0, 1.0, 1, 100.0);
+gl.matrixMode(gl.MODELVIEW);
+gl.loadIdentity();
+gl.translatef(0.0, 0.0, -1.0);
+
+/*
+gl.begin(gl.TRIANGLE_STRIP)
+    gl.color3f(1.0, 0.0, 0.0); gl.vertex3f(-0.5, 0.5, 0.0);
+    gl.color3f(0.0, 1.0, 0.0); gl.vertex3f( 0.5, 0.5, 0.0);
+    gl.color3f(0.0, 0.0, 1.0); gl.vertex3f(-0.5,-0.5, 0.0);
+    gl.color3f(1.0, 1.0, 0.0); gl.vertex3f( 0.5,-0.5, 0.0);
+gl.end();
+*/
+gl.begin(gl.QUAD_STRIP)
+    gl.color3f(1.0, 0.0, 0.0); gl.vertex3f(-0.5, 0.5, 0.0);
+    gl.color3f(0.0, 1.0, 0.0); gl.vertex3f( 0.5, 0.5, 0.0);
+    gl.color3f(0.0, 0.0, 1.0); gl.vertex3f(-0.5,-0.5, 0.0);
+    gl.color3f(1.0, 1.0, 0.0); gl.vertex3f( 0.5,-0.5, 0.0);
+gl.end();
+
+gl.flush();
+
+gl.swapBuffers();
---
+++ cx20/c200/readme.txt
@@ -0,0 +1,23 @@
+﻿[WebGl] TinyGL.js を試してみるテスト（その２）
+
+TinyGL.js は OpenGL 1.1 互換 API の JavaScript ライブラリとのことです。
+TinyGL.jsは、C言語版の OpenGL 実装である TinyGL を Emscripten で JavaScript にコンパイルしたものをベースにしているようです。
+ソフトウェアレンダリングを行っている為、WebGL が使えない環境でも動作するようです。
+
+＜対応した点＞
+・TinyGL.js で色付きの四角形を描画するよう対応。
+・TinyGL.js は OpenGL のサブセットである為、WebGL では対応していないハズの gl.QUADS や gl.QUADS_STRIP を用いることが出来るようです。
+
+＜参考＞
+■ TinyGL.js
+https://github.com/humu2009/tinygl.js
+■ TinyGL.js Examples
+https://github.com/humu2009/tinygl.js/wiki/Examples
+■ TinyGL : a Small, Free and Fast Subset of OpenGL*
+http://www.bellard.org/TinyGL/
+
+＜関連＞
+■ [WebGL] lightgl.js を試してみるテスト（その２）
+http://jsdo.it/cx20/ejpI
+■ 各種 WebGL ライブラリで基本図形を表示してみる
+http://qiita.com/cx20/items/0fa19c96aa6470d98807
