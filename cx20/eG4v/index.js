72143a687b350c0797536ca86a01d76372905c7d
cx20 <y-shimano@hondarer-soft.com>
2019-11-03 17:19:20 +0900

    [WebGL] CZPG.js を試してみるテスト

---
+++ cx20/eG4v/index.js
@@ -0,0 +1,17 @@
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+
+    const renderer = new CZPG.Renderer('c').setSize('100%', '100%').clear(1.0, 1.0, 1.0, 1.0);
+    const context = renderer.context;
+
+    const positions = [ 
+        0.0,  0.5, 0.0, 
+       -0.5, -0.5, 0.0, 
+        0.5, -0.5, 0.0 
+    ];
+    const attribArrays = {};
+    attribArrays['position'] = { data: positions, numComponents: 3 };
+    var mesh = new CZPG.Mesh('triangle', attribArrays);
+    var model = new CZPG.Model(mesh);
+    var shader = new CZPG.Shader(context, 'vs', 'fs');
+
+    shader.renderModel(model);
