ad67670a9d32fe7d5e127b014bcfd8b7aa7ce4b1
cx20 <y-shimano@hondarer-soft.com>
2019-11-03 19:17:00 +0900

    [WebGL] three.js を試してみるテスト（組み込み関数編）（その３）

---
+++ cx20/kwGs/index.js
@@ -0,0 +1,73 @@
+// forked from cx20's "[WebGL] three.js を試してみるテスト（組み込み関数編）（その２）" http://jsdo.it/cx20/d11S
+// forked from cx20's "[WebGL] three.js を試してみるテスト（組み込み関数編）" http://jsdo.it/cx20/vvCa
+// forked from cx20's "[WebGL] three.js を試してみるテスト（BufferGeometry編）" http://jsdo.it/cx20/yCyD
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+
+var container;
+var camera, scene, renderer;
+var mesh;
+
+init();
+animate();
+
+function init() {
+    container = document.getElementById('container');
+    camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 10);
+    camera.position.z = 3.5;
+    scene = new THREE.Scene();
+    
+    // 立方体の各面に色を設定
+    //             1.0 y 
+    //              ^  -1.0 
+    //              | / z
+    //              |/       x
+    // -1.0 -----------------> +1.0
+    //            / |
+    //      +1.0 /  |
+    //           -1.0
+    // 
+    //         [7]------[6]
+    //        / |      / |
+    //      [3]------[2] |
+    //       |  |     |  |
+    //       | [4]----|-[5]
+    //       |/       |/
+    //      [0]------[1]
+    //
+    var geometry = new THREE.BoxGeometry(1, 1, 1);
+    var vertexColors = [
+        [1.0, 0.0, 0.0, 1.0], // Front face
+        [1.0, 1.0, 0.0, 1.0], // Back face
+        [0.0, 1.0, 0.0, 1.0], // Top face
+        [1.0, 0.5, 0.5, 1.0], // Bottom face
+        [1.0, 0.0, 1.0, 1.0], // Right face
+        [0.0, 0.0, 1.0, 1.0], // Left face
+    ];
+    for ( var i = 0; i < geometry.faces.length; i += 2 ) {
+        var rgb = vertexColors[i/2];
+        geometry.faces[ i + 0 ].color.setRGB( rgb[0], rgb[1], rgb[2] );
+        geometry.faces[ i + 1 ].color.setRGB( rgb[0], rgb[1], rgb[2] );
+    }
+    var material = new THREE.MeshBasicMaterial({vertexColors: THREE.VertexColors});
+    mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setClearColor(0xffffff);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    container.appendChild(renderer.domElement);
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    render();
+}
+
+var rad = 0.0;
+function render() {
+    rad += Math.PI * 1.0 / 180.0
+    mesh.rotation.x = rad;
+    mesh.rotation.y = rad;
+    mesh.rotation.z = rad;
+    renderer.render(scene, camera);
+}
