899bed1f5f037d781cb21f1503dc8bf397d6ed98
cx20 <y-shimano@hondarer-soft.com>
2019-11-03 18:55:35 +0900

    [WebGL] xml3d.js を試してみるテスト（その４）

---
+++ cx20/ueDz/index.js
@@ -0,0 +1,19 @@
+// forked from cx20's "[WebGL] xml3d.js を試してみるテスト（その３）" http://jsdo.it/cx20/EwC8
+// forked from cx20's "[WebGL] xml3d.js を試してみるテスト（その２）" http://jsdo.it/cx20/E4Jd
+// forked from cx20's "[WebGL] xml3d.js を試してみるテスト" http://jsdo.it/cx20/Y75u
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+// nothing
+
+var anim = {};
+function initAnim() {
+    anim.xfm = document.getElementById("myxfm");
+    anim.lastTime = (new Date()).getTime();
+    anim.rotAngle = 0.0;
+    anim.intervalID = window.setInterval(function () {
+        var currentTime = (new Date).getTime();
+        var delta = currentTime - anim.lastTime;
+        anim.rotAngle += (30 * delta) / 1000.0;
+        anim.xfm.setAttribute("rotation", "1.0 0.0 1.0 "+(anim.rotAngle * Math.PI / 180));
+        anim.lastTime = currentTime;
+    }, 20);
+}
---
+++ cx20/ueDz/readme.txt
@@ -0,0 +1,30 @@
+﻿[WebGL] xml3d.js を試してみるテスト（その４）
+
+xml3d.js は XML の 3D 規格である XML3D の WebGL 実装のようです。
+
+＜対応した点＞
+・xml3d.js で立方体にテクスチャを貼り付けるよう対応。
+
+＜参考＞
+■ xml3d/xml3d.js
+https://github.com/xml3d/xml3d.js
+■ Welcome to the xml3d.js wiki!
+https://github.com/xml3d/xml3d.js/wiki
+■ xml3d/xml3d-examples
+https://github.com/xml3d/xml3d-examples
+■ 各種 WebGL ライブラリで基本図形を表示してみる
+http://qiita.com/cx20/items/0fa19c96aa6470d98807
+
+＜マークアップするタイプの WebGL ライブラリの比較＞
+■ [WebGL] GLAM を試してみるテスト（その４）
+http://jsdo.it/cx20/qK0x
+■ [WebGL] X3DOM を試してみるテスト（その４）
+http://jsdo.it/cx20/MGpZ
+■ [WebGL] xml3d.js を試してみるテスト（その４）
+http://jsdo.it/cx20/ueDz
+■ [WebGL] jThree を試してみるテスト（その４）
+http://jsdo.it/cx20/QIUP
+■ [WebGL] Grimoire.js を試してみるテスト（その４）
+http://jsdo.it/cx20/yNCh
+■ [WebGL] A-Frame を試してみるテスト（その４）
+http://jsdo.it/cx20/k7B2
