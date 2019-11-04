c48e81859c8071dda30a1ca661ac78383d80da76
cx20 <y-shimano@hondarer-soft.com>
2019-11-04 13:20:26 +0900

    [WebGL] Grimoire.js + OimoPhysics.js をドット絵を落下させてみるテスト

---
+++ cx20/giob/index.js
@@ -0,0 +1,132 @@
+// forked from cx20's "[WebGL] Grimoire.js で OimoPhysics.js を試してみるテスト" http://jsdo.it/cx20/Ep4p
+// forked from cx20's "[WebGL] Grimoire.js で Oimo.js を試してみるテスト（その３）" http://jsdo.it/cx20/c1O1
+// forked from cx20's "[WebGL] Grimoire.js で Oimo.js を試してみるテスト（その２）" http://jsdo.it/cx20/sVc4
+// forked from cx20's "[WebGL] Grimoire.js で Oimo.js を試してみるテスト" http://jsdo.it/cx20/kQ0F
+// forked from cx20's "[WebGL] Grimoire.js で Cannon.js を試してみるテスト" http://jsdo.it/cx20/4xwo
+// forked from cx20's "[WebGL] Grimoire.js を試してみるテスト（その４）（VBO編）" http://jsdo.it/cx20/swSy
+// forked from cx20's "[WebGL] Grimoire.js を試してみるテスト（その３）（VBO編）" http://jsdo.it/cx20/YiRx
+// forked from cx20's "[WebGL] Grimoire.js を試してみるテスト（その２）（VBO編）" http://jsdo.it/cx20/e3YN
+// forked from cx20's "[WebGL] Grimoire.js を試してみるテスト（VBO編）" http://jsdo.it/cx20/iUdQ
+// forked from cx20's "[WebGL] Grimoire.js を試してみるテスト" http://jsdo.it/cx20/4ZEB
+// forked from cx20's "[WebGL] jThree を試してみるテスト" http://jsdo.it/cx20/Go5s
+
+// ‥‥‥‥‥‥‥‥‥‥‥‥‥□□□
+// ‥‥‥‥‥‥〓〓〓〓〓‥‥□□□
+// ‥‥‥‥‥〓〓〓〓〓〓〓〓〓□□
+// ‥‥‥‥‥■■■□□■□‥■■■
+// ‥‥‥‥■□■□□□■□□■■■
+// ‥‥‥‥■□■■□□□■□□□■
+// ‥‥‥‥■■□□□□■■■■■‥
+// ‥‥‥‥‥‥□□□□□□□■‥‥
+// ‥‥■■■■■〓■■■〓■‥‥‥
+// ‥■■■■■■■〓■■■〓‥‥■
+// □□■■■■■■〓〓〓〓〓‥‥■
+// □□□‥〓〓■〓〓□〓〓□〓■■
+// ‥□‥■〓〓〓〓〓〓〓〓〓〓■■
+// ‥‥■■■〓〓〓〓〓〓〓〓〓■■
+// ‥■■■〓〓〓〓〓〓〓‥‥‥‥‥
+// ‥■‥‥〓〓〓〓‥‥‥‥‥‥‥‥
+var dataSet = [
+    "BK","BK","BK","BK","BK","BK","BK","BK","BK","BK","BK","BK","BK","BG","BG","BG",
+    "BK","BK","BK","BK","BK","BK","RD","RD","RD","RD","RD","BK","BK","BG","BG","BG",
+    "BK","BK","BK","BK","BK","RD","RD","RD","RD","RD","RD","RD","RD","RD","BG","BG",
+    "BK","BK","BK","BK","BK","BR","BR","BR","BG","BG","BR","BG","BK","RD","RD","RD",
+    "BK","BK","BK","BK","BR","BG","BR","BG","BG","BG","BR","BG","BG","RD","RD","RD",
+    "BK","BK","BK","BK","BR","BG","BR","BR","BG","BG","BG","BR","BG","BG","BG","RD",
+    "BK","BK","BK","BK","BR","BR","BG","BG","BG","BG","BR","BR","BR","BR","RD","BK",
+    "BK","BK","BK","BK","BK","BK","BG","BG","BG","BG","BG","BG","BG","RD","BK","BK",
+    "BK","BK","RD","RD","RD","RD","RD","BL","RD","RD","RD","BL","RD","BK","BK","BK",
+    "BK","RD","RD","RD","RD","RD","RD","RD","BL","RD","RD","RD","BL","BK","BK","BR",
+    "BG","BG","RD","RD","RD","RD","RD","RD","BL","BL","BL","BL","BL","BK","BK","BR",
+    "BG","BG","BG","BK","BL","BL","RD","BL","BL","YL","BL","BL","YL","BL","BR","BR",
+    "BK","BG","BK","BR","BL","BL","BL","BL","BL","BL","BL","BL","BL","BL","BR","BR",
+    "BK","BK","BR","BR","BR","BL","BL","BL","BL","BL","BL","BL","BL","BL","BR","BR",
+    "BK","BR","BR","BR","BL","BL","BL","BL","BL","BL","BL","BK","BK","BK","BK","BK",
+    "BK","BR","BK","BK","BL","BL","BL","BL","BK","BK","BK","BK","BK","BK","BK","BK"
+];
+
+function getRgbColor(colorType)
+{
+    var colorHash = {
+        "BK":"#dcaa6b", // black
+        "WH":"#FFFFFF", // white
+        "BG":"#FFCCCC", // beige
+        "BR":"#800000", // brown
+        "RD":"#FF0000", // red
+        "YL":"#FFFF00", // yellow
+        "GN":"#00FF00", // green
+        "WT":"#00FFFF", // water
+        "BL":"#0000FF", // blue
+        "PR":"#800080"  // purple
+    };
+    return colorHash[colorType];
+}
+
+const Quaternion = gr.lib.math.Quaternion;
+gr(function() {
+    const scene = gr("#main")("scene").single();
+    for (var i = 0; i < dataSet.length; i++) {
+        var x = i % 16 - 8;
+        var y = 16 - Math.floor(i / 16);
+        var z = 0;
+        const n = scene.addChildByName("rigid-cube", {
+            albedo: getRgbColor(dataSet[i]),
+            scale: 0.5,
+            position: [x * 1.2, y * 1.2, 0]
+        });
+    }
+
+});
+gr.register(() => {
+    gr.registerComponent("OimoScene", {
+        attributes: {
+        },
+        $awake: function() {
+            this.world = new OIMO.World();
+            this.world.gravity = new OIMO.Vec3(0, -9.80665, 0);
+        },
+        $update: function() {
+            this.world.step(1/30);
+        }
+    });
+    gr.overrideDeclaration("scene", ["OimoScene"]);
+    gr.registerComponent("Rigid", {
+        attributes: {
+            shape: {
+                default: "box",
+                converter: "String"
+            },
+            move: {
+                converter: "Boolean",
+                default: true
+            }
+        },
+        $mount: function() {
+            this.__bindAttributes();
+            this.transform = this.node.getComponent("Transform");
+            const p = this.transform.position;
+            const r = this.transform.rotation;
+            const s = this.transform.scale;
+            const oimoScene = this.node.getComponentInAncestor("OimoScene");
+            const shapec = new OIMO.ShapeConfig();
+            shapec.geometry = new OIMO.BoxGeometry(new OIMO.Vec3(s.X, s.Y, s.Z));
+            const bodyc = new OIMO.RigidBodyConfig();
+            bodyc.type = this.move ? OIMO.RigidBodyType.DYNAMIC : OIMO.RigidBodyType.STATIC;
+            bodyc.position = new OIMO.Vec3(p.X, p.Y, p.Z);
+            this.body = new OIMO.RigidBody(bodyc);
+            this.body.setRotationXyz(new OIMO.Vec3(r.X, r.Y, r.Z));
+            this.body.addShape(new OIMO.Shape(shapec));
+            oimoScene.world.addRigidBody(this.body);
+        },
+        $update: function() {
+            const p = this.body.getPosition();
+            this.transform.setAttribute("position", [p.x, p.y, p.z]);
+            const r = this.body.getOrientation();
+            this.transform.setAttribute("rotation", new Quaternion([r.x, r.y, r.z, r.w]));
+        }
+    });
+    gr.registerNode("rigid-cube", ["Rigid"], {
+        geometry: "cube",
+        scale: 0.5
+    }, "mesh");
+});
---
+++ cx20/giob/readme.txt
@@ -0,0 +1,14 @@
+﻿[WebGL] Grimoire.js + OimoPhysics.js をドット絵を落下させてみるテスト
+
+＜対応した点＞
+・立方体をドット絵になるよう配置。
+
+＜参考＞
+■ GrimoireGL/GrimoireJS
+https://github.com/GrimoireGL/GrimoireJS
+
+■ saharan/OimoPhysics
+https://github.com/saharan/OimoPhysics
+
+■ WebGLと3D物理演算ライブラリの組み合わせを試してみる
+https://qiita.com/cx20/items/3ebed669fb9c9e797935
