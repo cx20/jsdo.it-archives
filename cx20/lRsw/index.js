11c8b6490eb877c25196ab3bbb72f950a3b09d17
cx20 <y-shimano@hondarer-soft.com>
2019-11-06 01:16:58 +0900

    [WebGL] glCubic.js を試してみるテスト

--- cx20/lRsw/index.js
+++ cx20/lRsw/index.js
@@ -1,9 +1,9 @@
 // forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
 
-var prg;
-var VBO;
+let prg;
+let VBO;
 
-var position = [ 
+let position = [ 
      0.0, 0.5, 0.0, // v0
     -0.5,-0.5, 0.0, // v1
      0.5,-0.5, 0.0  // v2
@@ -23,6 +23,18 @@ function init() {
     VBO = [
         gl3.createVbo(position)
     ];
+
+    resizeCanvas();
+
+    window.addEventListener("resize", function(){
+        resizeCanvas();
+    });
+}
+
+function resizeCanvas() {
+    gl3.canvas.width = window.innerWidth;
+    gl3.canvas.height = window.innerHeight;
+    gl3.gl.viewport(0, 0, gl3.canvas.width, gl3.canvas.height);
 }
 
 function render(){
