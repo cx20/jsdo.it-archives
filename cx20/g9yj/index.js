9ac1d6e6a7df5c9d20278c3e8b994b7ca415cd7e
cx20 <y-shimano@hondarer-soft.com>
2019-11-16 03:40:23 +0900

    [WebGL] GLBoost を試してみるテスト（組み込み関数編）

--- cx20/g9yj/index.js
+++ cx20/g9yj/index.js
@@ -3,29 +3,33 @@
 // forked from cx20's "[WebGL] GLBoost を試してみるテスト" http://jsdo.it/cx20/SWec
 // forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
 
-var canvas = document.getElementById("world");
-var glBoostContext = new GLBoost.GLBoostMiddleContext(canvas);
-var renderer = glBoostContext.createRenderer({ canvas: canvas, clearColor: {red:1, green:1, blue:1, alpha:1}});
+let width = window.innerWidth;
+let height = window.innerHeight;
 
-var scene = glBoostContext.createScene();
+let canvas = document.getElementById("world");
+let glBoostContext = new GLBoost.GLBoostMiddleContext(canvas);
+let renderer = glBoostContext.createRenderer({ canvas: canvas, clearColor: {red:1, green:1, blue:1, alpha:1}});
+renderer.resize(width, height);
 
-var positions = [];
+let scene = glBoostContext.createScene();
+
+let positions = [];
 positions.push(new GLBoost.Vector3( 0.0,  0.5, 0.0));
 positions.push(new GLBoost.Vector3(-0.5, -0.5, 0.0));
 positions.push(new GLBoost.Vector3( 0.5, -0.5, 0.0));
 
-var geometry = glBoostContext.createGeometry();
-var material = glBoostContext.createClassicMaterial();
+let geometry = glBoostContext.createGeometry();
+let material = glBoostContext.createClassicMaterial();
 material.baseColor = new GLBoost.Vector4(0, 0, 1, 1);
 
 geometry.setVerticesData({
     position: positions
 });
 
-var mesh = glBoostContext.createMesh(geometry, material);
+let mesh = glBoostContext.createMesh(geometry, material);
 scene.addChild(mesh);
 
-var expression = glBoostContext.createExpressionAndRenderPasses(1);
+let expression = glBoostContext.createExpressionAndRenderPasses(1);
 expression.renderPasses[0].scene = scene;
 expression.prepareToRender();
 
