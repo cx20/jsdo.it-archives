434061abd50f2228e01ddd1abbbc20d51cfca7b0
cx20 <y-shimano@hondarer-soft.com>
2019-11-16 03:41:32 +0900

    [WebGL] GLBoost を試してみるテスト（組み込み関数編）（その２）

--- cx20/8PA0/index.js
+++ cx20/8PA0/index.js
@@ -4,13 +4,17 @@
 // forked from cx20's "[WebGL] GLBoost を試してみるテスト" http://jsdo.it/cx20/SWec
 // forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
 
-var canvas = document.getElementById("world");
-var glBoostContext = new GLBoost.GLBoostMiddleContext(canvas);
-var renderer = glBoostContext.createRenderer({ canvas: canvas, clearColor: {red:1, green:1, blue:1, alpha:1}});
+let width = window.innerWidth;
+let height = window.innerHeight;
 
-var scene = glBoostContext.createScene();
+let canvas = document.getElementById("world");
+let glBoostContext = new GLBoost.GLBoostMiddleContext(canvas);
+let renderer = glBoostContext.createRenderer({ canvas: canvas, clearColor: {red:1, green:1, blue:1, alpha:1}});
+renderer.resize(width, height);
 
-var material = glBoostContext.createClassicMaterial();
+let scene = glBoostContext.createScene();
+
+let material = glBoostContext.createClassicMaterial();
 material.baseColor = new GLBoost.Vector4(1, 1, 1, 1);
 
 // 正方形の座標データを用意
@@ -29,19 +33,19 @@ material.baseColor = new GLBoost.Vector4(1, 1, 1, 1);
 //         |        |
 //        [2]------[3]
 //
-var uSpan = 1;
-var vSpan = 1;
-var geometry = glBoostContext.createPlane(1, 1, uSpan, vSpan);
-var colors = [
+let uSpan = 1;
+let vSpan = 1;
+let geometry = glBoostContext.createPlane(1, 1, uSpan, vSpan);
+let colors = [
     new GLBoost.Vector4(1.0, 0.0, 0.0, 1.0),  // v0
     new GLBoost.Vector4(0.0, 1.0, 0.0, 1.0),  // v1
     new GLBoost.Vector4(0.0, 0.0, 1.0, 1.0),  // v2
     new GLBoost.Vector4(1.0, 1.0, 0.0, 1.0)   // v3
 ];
-var mesh = glBoostContext.createMesh(geometry, material);
+let mesh = glBoostContext.createMesh(geometry, material);
 scene.addChild(mesh);
 
-var camera = glBoostContext.createPerspectiveCamera({
+let camera = glBoostContext.createPerspectiveCamera({
     eye: new GLBoost.Vector3(0.0, 2.5, 0.0),
     center: new GLBoost.Vector3(0.0, 0.0, 0.0),
     up: new GLBoost.Vector3(0.0, 0.0, 1.0)
@@ -54,7 +58,7 @@ var camera = glBoostContext.createPerspectiveCamera({
 
 scene.addChild(camera);
 
-var expression = glBoostContext.createExpressionAndRenderPasses(1);
+let expression = glBoostContext.createExpressionAndRenderPasses(1);
 expression.renderPasses[0].scene = scene;
 expression.prepareToRender();
 
