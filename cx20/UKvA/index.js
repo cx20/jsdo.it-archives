d8a5720bce98480f5c82d1226b8c2f39e58744a9
cx20 <y-shimano@hondarer-soft.com>
2019-11-25 02:47:53 +0900

    GLBoost.js + ammo.js でドミノっぽくドット絵を作るテスト

--- cx20/UKvA/index.js
+++ cx20/UKvA/index.js
@@ -19,7 +19,7 @@
 // ‥‥■■■〓〓〓〓〓〓〓〓〓■■
 // ‥■■■〓〓〓〓〓〓〓‥‥‥‥‥
 // ‥■‥‥〓〓〓〓‥‥‥‥‥‥‥‥
-var dataSet = [
+let dataSet = [
     "無","無","無","無","無","無","無","無","無","無","無","無","無","肌","肌","肌",
     "無","無","無","無","無","無","赤","赤","赤","赤","赤","無","無","肌","肌","肌",
     "無","無","無","無","無","赤","赤","赤","赤","赤","赤","赤","赤","赤","肌","肌",
@@ -40,8 +40,8 @@ var dataSet = [
 
 function getRgbColor( c )
 {
-    var colorHash = {
-        "無":{r:0xDC,g:0xAA,b:0x6B},    // 段ボール色
+    let colorHash = {
+        "無":{r:0xDC,g:0xAA,b:0x6B},
         "白":{r:0xff,g:0xff,b:0xff},
         "肌":{r:0xff,g:0xcc,b:0xcc},
         "茶":{r:0x80,g:0x00,b:0x00},
@@ -56,11 +56,11 @@ function getRgbColor( c )
 }
 
 Ammo().then(function(Ammo) {
-var glBoostContext;
-var scene;
-var dynamicsWorld;
-var objs = [];
-var numObjects = 0;
+let glBoostContext;
+let scene;
+let dynamicsWorld;
+let objs = [];
+let numObjects = 0;
 
 function Ball(x, y, z, r, m, color) {
     this.x = x;
@@ -82,35 +82,35 @@ Ball.prototype.destructor = function () {
 
 Ball.prototype.initGlboostObj = function () {
     // initialize Object3D
-    var material = glBoostContext.createClassicMaterial();
-    var texture = glBoostContext.createTexture("../../assets/s/s/X/x/ssXxc.png");  // Football.png
-    material.diffuseTexture = texture;
-    var color = this.color;
-    var color2 = new GLBoost.Vector4(color.r / 0xff, color.g / 0xff, color.b / 0xff, 1.0);
-    var sphere = glBoostContext.createSphere(this.r, 10, 10, color2);
-    var ball = glBoostContext.createMesh(sphere, material);
-    var translate = new GLBoost.Vector3(this.x, this.y, this.z);
+    let material = glBoostContext.createClassicMaterial();
+    let texture = glBoostContext.createTexture("../../assets/s/s/X/x/ssXxc.png");  // Football.png
+    material.setTexture(texture);
+    let color = this.color;
+    let color2 = new GLBoost.Vector4(color.r / 0xff, color.g / 0xff, color.b / 0xff, 1.0);
+    let sphere = glBoostContext.createSphere(this.r, 10, 10, color2);
+    let ball = glBoostContext.createMesh(sphere, material);
+    let translate = new GLBoost.Vector3(this.x, this.y, this.z);
     ball.translate = translate;
 
     this.glboostObj = ball;
 };
 
 Ball.prototype.initBulletObj = function (m) {
-    var startTransform = new Ammo.btTransform();
+    let startTransform = new Ammo.btTransform();
     startTransform.setIdentity();
-    var origin = startTransform.getOrigin();
+    let origin = startTransform.getOrigin();
     origin.setX(this.x);
     origin.setY(this.y);
     origin.setZ(this.z);
 
-    var shape = new Ammo.btSphereShape(this.r);
-    var localInertia = new Ammo.btVector3(0, 0, 0);
+    let shape = new Ammo.btSphereShape(this.r);
+    let localInertia = new Ammo.btVector3(0, 0, 0);
     shape.calculateLocalInertia(m, localInertia);
 
-    var motionState = new Ammo.btDefaultMotionState(startTransform);
-    var rbInfo = new Ammo.btRigidBodyConstructionInfo(m, motionState, shape, localInertia);
+    let motionState = new Ammo.btDefaultMotionState(startTransform);
+    let rbInfo = new Ammo.btRigidBodyConstructionInfo(m, motionState, shape, localInertia);
     rbInfo.set_m_restitution(0.6); // 反発係数（0～1）
-    var body = new Ammo.btRigidBody(rbInfo);
+    let body = new Ammo.btRigidBody(rbInfo);
 
     Ammo.destroy(startTransform);
     Ammo.destroy(localInertia);
@@ -120,16 +120,16 @@ Ball.prototype.initBulletObj = function (m) {
 };
 
 Ball.prototype.move = function () {
-    var pos = [0, 0, 0];
+    let pos = [0, 0, 0];
 
     this.bulletObj.getMotionState().getWorldTransform(this._trans);
-    var origin = this._trans.getOrigin();
+    let origin = this._trans.getOrigin();
     pos[0] = origin.x();
     pos[1] = origin.y();
     pos[2] = origin.z();
-    var rotation = this._trans.getRotation();
-    var quat = new GLBoost.Quaternion(rotation.x(), rotation.y(), rotation.z(), rotation.w());
-    var translate = new GLBoost.Vector3(pos[0], pos[1], pos[2]);
+    let rotation = this._trans.getRotation();
+    let quat = new GLBoost.Quaternion(rotation.x(), rotation.y(), rotation.z(), rotation.w());
+    let translate = new GLBoost.Vector3(pos[0], pos[1], pos[2]);
     
     this.glboostObj.translate = translate;
     // クォータニオン（ammo.js） → クォータニオン（GLBoost.js）
@@ -156,41 +156,41 @@ Box.prototype.destructor = function () {
 };
 
 Box.prototype.initGlboostObj = function () {
-    var w = this.w;
-    var h = this.h;
-    var d = this.d;
-    var material = glBoostContext.createClassicMaterial();
+    let w = this.w;
+    let h = this.h;
+    let d = this.d;
+    let material = glBoostContext.createClassicMaterial();
     material.shaderClass = GLBoost.PhongShader;
-    var color = this.color;
-    var color2 = new GLBoost.Vector4(color.r / 0xff, color.g / 0xff, color.b / 0xff, 1.0);
-    var box = glBoostContext.createCube(new GLBoost.Vector3(w, h, d), color2);
-    var mesh = glBoostContext.createMesh(box, material);
-    var translate = new GLBoost.Vector3(this.x, this.y, this.z);
+    let color = this.color;
+    let color2 = new GLBoost.Vector4(color.r / 0xff, color.g / 0xff, color.b / 0xff, 1.0);
+    let box = glBoostContext.createCube(new GLBoost.Vector3(w, h, d), color2);
+    let mesh = glBoostContext.createMesh(box, material);
+    let translate = new GLBoost.Vector3(this.x, this.y, this.z);
     mesh.translate = translate;
 
     this.glboostObj = mesh;
 };
 
 Box.prototype.initBulletObj = function (m) {
-    var startTransform = new Ammo.btTransform();
+    let startTransform = new Ammo.btTransform();
     startTransform.setIdentity();
-    var origin = startTransform.getOrigin();
+    let origin = startTransform.getOrigin();
     origin.setX(this.x);
     origin.setY(this.y);
     origin.setZ(this.z);
 
-    var w = this.w;
-    var h = this.h;
-    var d = this.d;
-    var tmpVec = new Ammo.btVector3(w / 2, h / 2, d / 2);
-    var shape = new Ammo.btBoxShape(tmpVec);
-    var localInertia = new Ammo.btVector3(0, 0, 0);
+    let w = this.w;
+    let h = this.h;
+    let d = this.d;
+    let tmpVec = new Ammo.btVector3(w / 2, h / 2, d / 2);
+    let shape = new Ammo.btBoxShape(tmpVec);
+    let localInertia = new Ammo.btVector3(0, 0, 0);
     shape.calculateLocalInertia(m, localInertia);
 
-    var motionState = new Ammo.btDefaultMotionState(startTransform);
-    var rbInfo = new Ammo.btRigidBodyConstructionInfo(m, motionState, shape, localInertia);
+    let motionState = new Ammo.btDefaultMotionState(startTransform);
+    let rbInfo = new Ammo.btRigidBodyConstructionInfo(m, motionState, shape, localInertia);
     rbInfo.set_m_restitution(0.2); // 反発係数（0～1）
-    var body = new Ammo.btRigidBody(rbInfo);
+    let body = new Ammo.btRigidBody(rbInfo);
 
     Ammo.destroy(startTransform);
     Ammo.destroy(localInertia);
@@ -200,16 +200,16 @@ Box.prototype.initBulletObj = function (m) {
 };
 
 Box.prototype.move = function () {
-    var pos = [0, 0, 0];
+    let pos = [0, 0, 0];
 
     this.bulletObj.getMotionState().getWorldTransform(this._trans);
-    var origin = this._trans.getOrigin();
+    let origin = this._trans.getOrigin();
     pos[0] = origin.x();
     pos[1] = origin.y();
     pos[2] = origin.z();
-    var rotation = this._trans.getRotation();
-    var quat = new GLBoost.Quaternion(rotation.x(), rotation.y(), rotation.z(), rotation.w());
-    var translate = new GLBoost.Vector3(pos[0], pos[1], pos[2]);
+    let rotation = this._trans.getRotation();
+    let quat = new GLBoost.Quaternion(rotation.x(), rotation.y(), rotation.z(), rotation.w());
+    let translate = new GLBoost.Vector3(pos[0], pos[1], pos[2]);
     
     this.glboostObj.translate = translate;
     // クォータニオン（ammo.js） → クォータニオン（GLBoost.js）
@@ -233,32 +233,32 @@ Plane.prototype.destructor = function () {
 };
 
 Plane.prototype.initGlboostObj = function () {
-    var s = this.s;
-    var material = glBoostContext.createClassicMaterial();
-    var texture = glBoostContext.createTexture("../../assets/u/y/G/y/uyGy9.jpg"); // grass.jpg
-    material.diffuseTexture = texture;
-    var planeGeometry = glBoostContext.createPlane(s, s, 5, 5, null, true);
-    var ground = glBoostContext.createMesh(planeGeometry, material);
+    let s = this.s;
+    let material = glBoostContext.createClassicMaterial();
+    let texture = glBoostContext.createTexture("../../assets/u/y/G/y/uyGy9.jpg"); // grass.jpg
+    material.setTexture(texture);
+    let planeGeometry = glBoostContext.createPlane(s, s, 5, 5, null, true);
+    let ground = glBoostContext.createMesh(planeGeometry, material);
 
     this.glboostObj = ground;
 };
 
 Plane.prototype.initBulletObj = function (m) {
-    var s = this.s;
-    var tmpVec = new Ammo.btVector3(s / 2, 1 / 2, s / 2);
-    var shape = new Ammo.btBoxShape(tmpVec);
+    let s = this.s;
+    let tmpVec = new Ammo.btVector3(s / 2, 1 / 2, s / 2);
+    let shape = new Ammo.btBoxShape(tmpVec);
     Ammo.destroy(tmpVec);
-    var startTransform = new Ammo.btTransform();
+    let startTransform = new Ammo.btTransform();
     startTransform.setIdentity();
-    tmpVec = new Ammo.btVector3(this.x, this.y + 1, this.z);
+    tmpVec = new Ammo.btVector3(this.x, this.y, this.z);
     startTransform.setOrigin(tmpVec);
     Ammo.destroy(tmpVec);
 
-    var localInertia = new Ammo.btVector3(0, 0, 0);
-    var motionState = new Ammo.btDefaultMotionState(startTransform);
-    var rbInfo = new Ammo.btRigidBodyConstructionInfo(m, motionState, shape, localInertia);
+    let localInertia = new Ammo.btVector3(0, 0, 0);
+    let motionState = new Ammo.btDefaultMotionState(startTransform);
+    let rbInfo = new Ammo.btRigidBodyConstructionInfo(m, motionState, shape, localInertia);
     rbInfo.set_m_restitution(1);
-    var body = new Ammo.btRigidBody(rbInfo);
+    let body = new Ammo.btRigidBody(rbInfo);
 
     Ammo.destroy(startTransform);
     Ammo.destroy(localInertia);
@@ -272,13 +272,13 @@ Plane.prototype.move = function () {
 };
 
 function initPhysicsWorld() {
-    var gravity = new Ammo.btVector3(0, -200, 0);
+    let gravity = new Ammo.btVector3(0, -100, 0);
 
-    var collisionConfiguration = new Ammo.btDefaultCollisionConfiguration();
-    var dispatcher = new Ammo.btCollisionDispatcher(collisionConfiguration);
-    var overlappingPairCache = new Ammo.btDbvtBroadphase();
-    var solver = new Ammo.btSequentialImpulseConstraintSolver();
-    var dynamicsWorld = new Ammo.btDiscreteDynamicsWorld(
+    let collisionConfiguration = new Ammo.btDefaultCollisionConfiguration();
+    let dispatcher = new Ammo.btCollisionDispatcher(collisionConfiguration);
+    let overlappingPairCache = new Ammo.btDbvtBroadphase();
+    let solver = new Ammo.btSequentialImpulseConstraintSolver();
+    let dynamicsWorld = new Ammo.btDiscreteDynamicsWorld(
         dispatcher, overlappingPairCache, solver, collisionConfiguration);
     dynamicsWorld.setGravity(gravity);
 
@@ -286,42 +286,36 @@ function initPhysicsWorld() {
 }
 
 window.addEventListener("load", function () {
-    var width = 465;
-    var height = 465;
-    var deltaT = 30;
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+    let deltaT = 10;
 
     canvas = document.getElementById("world");
     glBoostContext = new GLBoost.GLBoostMiddleContext(canvas);
-    renderer = glBoostContext.createRenderer({
-        clearColor: {
-            red: 0.6,
-            green: 0.6,
-            blue: 0.6,
-            alpha: 1
-        }
-    });
-    
+    renderer = glBoostContext.createRenderer({canvas : canvas, clearColor: {red: 0, green: 0, blue: 0, alpha: 1}});
+    renderer.resize(width, height);
     scene = glBoostContext.createScene();
 
     camera = glBoostContext.createPerspectiveCamera({
-        eye: new GLBoost.Vector3(4.0, 10.0, 20.0),
+        eye: new GLBoost.Vector3(0.0, 10, 20),
         center: new GLBoost.Vector3(0.0, 0.0, 0.0),
         up: new GLBoost.Vector3(0.0, 1.0, 0.0)
     }, {
         fovy: 45.0,
-        aspect: 1.0,
-        zNear: 0.1,
+        aspect: window.innerWidth/window.innerHeight,
+        zNear: 0.01,
         zFar: 2000.0
     });
     camera.cameraController = glBoostContext.createCameraController();
     scene.addChild(camera);
 
-    var pointLight = glBoostContext.createPointLight(new GLBoost.Vector3(1.0, 1.0, 1.0));
-    pointLight.translate = new GLBoost.Vector3(10, 10, 10);
-    scene.addChild(pointLight);
+    let directionalLight1 = glBoostContext.createDirectionalLight(new GLBoost.Vector3(1, 1, 1), new GLBoost.Vector3(30, 30, 30));
+    scene.addChild( directionalLight1 );
+    let directionalLight2 = glBoostContext.createDirectionalLight(new GLBoost.Vector3(1, 1, 1), new GLBoost.Vector3(-30, -30, -30));
+    scene.addChild( directionalLight2 );
 
     dynamicsWorld = initPhysicsWorld();
-    var ground = new Plane(0, 0, 0, 500, 0, 0xdddddd);
+    let ground = new Plane(0, 0, 0, 50, 0, 0xdddddd);
     scene.addChild(ground.glboostObj);
     dynamicsWorld.addRigidBody(ground.bulletObj);
 
@@ -335,8 +329,8 @@ window.addEventListener("load", function () {
     function rendering() {
         dynamicsWorld.stepSimulation(deltaT / 1000);
 
-        for (var i = numObjects; i--;) {
-            var obj = objs[i];
+        for (let i = numObjects; i--;) {
+            let obj = objs[i];
             obj.move();
         }
 
@@ -350,14 +344,14 @@ window.addEventListener("load", function () {
 }, false);
 
 function createDominos() {
-    var box_size = 1;
-    for ( var y = 0; y < 16; y++ ) {
-        for ( var x = 0; x < 16; x++ ) {
-            var x1 = -6 + x * box_size * 0.95;
-            var y1 = box_size * 2;
-            var z1 = -8 + y * box_size * 1.2;
-            var color = getRgbColor( dataSet[y * 16 + x] );
-            var box = new Box(x1, y1, z1, 0.2, 1.2, 1, 10, color);
+    let box_size = 1;
+    for ( let y = 0; y < 16; y++ ) {
+        for ( let x = 0; x < 16; x++ ) {
+            let x1 = -6 + x * box_size * 0.95;
+            let y1 = box_size * 1;
+            let z1 = -8 + y * box_size * 1.2;
+            let color = getRgbColor( dataSet[y * 16 + x] );
+            let box = new Box(x1, y1, z1, 0.2, 1.2, 1, 10, color);
             scene.addChild(box.glboostObj);
             dynamicsWorld.addRigidBody(box.bulletObj);
             objs.push(box);
@@ -367,14 +361,14 @@ function createDominos() {
 }
 
 function createShapes() {
-    var ball_size = 0.5;
-    var box_size = 1;
-    for ( var y = 0; y < 16; y++ ) {
-        var x1 = -6.2;
-        var y1 = 4;
-        var z1 = -8 + (15 - y) * box_size * 1.2;
-        var color = getRgbColor("白");
-        var ball = new Ball(x1, y1, z1, ball_size, 10, color);
+    let ball_size = 0.5;
+    let box_size = 1;
+    for ( let y = 0; y < 16; y++ ) {
+        let x1 = -6.2;
+        let y1 = 4;
+        let z1 = -8 + (15 - y) * box_size * 1.2;
+        let color = getRgbColor("白");
+        let ball = new Ball(x1, y1, z1, ball_size, 10, color);
         scene.addChild(ball.glboostObj);
         dynamicsWorld.addRigidBody(ball.bulletObj);
         objs.push(ball);
