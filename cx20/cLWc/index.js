89fad67fb4e8a9e427811e555cba4934592dda51
cx20 <y-shimano@hondarer-soft.com>
2019-11-24 15:49:32 +0900

    [WebGL] GLBoost で PBR を試してみるテスト（glTF編）（調整中）

---
+++ cx20/cLWc/index.js
@@ -0,0 +1,105 @@
+// forked from cx20's "[WebGL] GLBoost で glTF 2.0 モデルを表示してみるテスト（その６）（調整中）" http://jsdo.it/cx20/O3eo
+// forked from cx20's "[WebGL] GLBoost で glTF 2.0 モデルを表示してみるテスト（その５）（調整中）" http://jsdo.it/cx20/E9Ph
+// forked from cx20's "[WebGL] GLBoost で glTF 2.0 モデルを表示してみるテスト（その４）（調整中）" http://jsdo.it/cx20/4bHc
+// forked from cx20's "[WebGL] GLBoost で glTF 2.0 モデルを表示してみるテスト（その３）（調整中）" http://jsdo.it/cx20/89Fn
+// forked from cx20's "[WebGL] GLBoost で glTF 2.0 モデルを表示してみるテスト（その２）（調整中）" http://jsdo.it/cx20/Gldx
+// forked from cx20's "[WebGL] GLBoost で glTF 2.0 モデルを表示してみるテスト（調整中）" http://jsdo.it/cx20/Qr7c
+// forked from cx20's "[WebGL] GLBoost + glTF Loader を試してみるテスト（その１）" http://jsdo.it/cx20/G5nJ
+// forked from cx20's "[WebGL] GLBoost + ObjLoader を試してみるテスト" http://jsdo.it/cx20/8qQl
+// forked from cx20's "[WebGL] GLBoost + vox.js を試してみるテスト" http://jsdo.it/cx20/2Nrf
+// forked from cx20's "[WebGL] GLBoost を試してみるテスト（standalone編）（その３）" http://jsdo.it/cx20/Elgc
+// forked from cx20's "[WebGL] GLBoost を試してみるテスト（standalone編）（その２）" http://jsdo.it/cx20/Uvah
+// forked from cx20's "[WebGL] GLBoost を試してみるテスト（standalone編）" http://jsdo.it/cx20/KXXM
+// forked from cx20's "[WebGL] GLBoost を試してみるテスト（phina.js編）" http://jsdo.it/cx20/SmrF
+// forked from cx20's "[WebGL] GLBoost を試してみるテスト" http://jsdo.it/cx20/SWec
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+
+let canvas = document.getElementById("world");
+let width = window.innerWidth;
+let height = window.innerHeight;
+
+let glBoostContext = new GLBoost.GLBoostMiddleContext(canvas);
+let renderer = glBoostContext.createRenderer({
+    clearColor: {
+        red: 0.0,
+        green: 0.0,
+        blue: 0.0,
+        alpha: 1
+    }
+});
+
+renderer.resize(width, height);
+
+let scene = glBoostContext.createScene();
+
+let pointLight = glBoostContext.createPointLight(new GLBoost.Vector3(1.0, 1.0, 1.0));
+pointLight.translate = new GLBoost.Vector3(10, 10, 10);
+scene.addChild(pointLight);
+let camera = glBoostContext.createPerspectiveCamera({
+    //eye: new GLBoost.Vector3(2.0, 1.0, 2.0),
+    eye: new GLBoost.Vector3(0, 0, 5.5),
+    center: new GLBoost.Vector3(0.0, 0.0, 0.0),
+    up: new GLBoost.Vector3(0.0, 1.0, 0.0)
+}, {
+    fovy: 30.0,
+    aspect: width/height,
+    zNear: 0.1,
+    zFar: 3000.0
+});
+camera.cameraController = glBoostContext.createCameraController();
+//camera.cameraController.zFarAdjustingFactorBasedOnAABB = 3;
+scene.addChild(camera);
+
+//let gridGizmo = glBoostContext.createGridGizmo(16, 16, true, true, false, new GLBoost.Vector4(1, 1, 1, 1));
+//scene.addChild(gridGizmo);
+
+let axisGizmo = glBoostContext.createAxisGizmo(4);
+scene.addChild(axisGizmo);
+
+let gtime = 0;
+let glTF2Loader = GLBoost.GLTF2Loader.getInstance();
+let modelConverter = GLBoost.ModelConverter.getInstance();
+let scale = 1*2;
+//let url = "https://cdn.rawgit.com/cx20/gltf-test/e63efa65/tutorialModels/FlightHelmet/glTF/FlightHelmet.gltf";
+//let url = "https://cdn.rawgit.com/cx20/jsdo-static-contents/05a21973/models/gltf/2.0/PbrSimpleTest/glTF-Binary/PbrSimpleTest.glb";
+let url = "https://cdn.rawgit.com/cx20/jsdo-static-contents/05a21973/models/gltf/2.0/PbrSimpleTest/glTF/PbrSimpleTest.gltf";
+
+let promise = glTF2Loader.loadGLTF(url, {
+      extensionLoader: null,
+      //defaultShader: GLBoost.PhongShader,
+      defaultShader: GLBoost.PBRPrincipledShader,
+      isNeededToMultiplyAlphaToColorOfPixelOutput: true,
+      isTextureImageToLoadPreMultipliedAlpha: false,
+      isExistJointGizmo: false,
+      isBlend: false,
+      isDepthTest: true,
+      isAllMeshesTransparent: false
+    });
+      
+promise.then(function(gltfObj) {
+    let group = modelConverter.convertToGLBoostModel(glBoostContext, gltfObj);
+    //camera.cameraController.target = group;
+    console.log(group);
+    group.scale = new GLBoost.Vector3(scale, scale, scale);
+    scene.addChild(group);
+
+    let expression = glBoostContext.createExpressionAndRenderPasses(1);
+    expression.renderPasses[0].scene = scene;
+    expression.prepareToRender();
+    
+    let render = function() {
+        scene.setCurrentAnimationValue('time', gtime);
+        renderer.clearCanvas();
+        renderer.update(expression); 
+        renderer.draw(expression);
+        gtime += 0.03;
+        if (gtime > 5) {
+            gtime = 0.0;
+        }
+        let rotateMatrix = GLBoost.Matrix33.rotateY(-0.5);
+        let rotatedVector = rotateMatrix.multiplyVector(camera.eye);
+        //camera.eye = rotatedVector;
+        requestAnimationFrame(render);
+    };
+    render();
+});
---
+++ cx20/cLWc/readme.txt
@@ -0,0 +1,21 @@
+﻿[WebGL] GLBoost で PBR を試してみるテスト（glTF編）（調整中）
+
+＜他ライブラリ比較＞
+■ [WebGL] three.jsで PBR を試してみるテスト（glTF編）（調整中）
+http://jsdo.it/cx20/mvgp
+■ [WebGL] Babylon.js で PBR を試してみるテスト（glTF編）（調整中）
+http://jsdo.it/cx20/IJzp
+■ [WebGL] Grimoire.jsで PBR を試してみるテスト（glTF編）（調整中）
+http://jsdo.it/cx20/k9ho
+■ [WebGL] Cesium.jsで PBR を試してみるテスト（glTF編）（調整中）
+http://jsdo.it/cx20/ghCa
+■ [WebGL] ClayGL で PBR を試してみるテスト（glTF編）（調整中）
+http://jsdo.it/cx20/OReR
+■ [WebGL] Hilo3d で PBR を試してみるテスト（glTF編）（調整中）
+http://jsdo.it/cx20/08FR
+■ [WebGL] PlayCanvas で PBR を試してみるテスト（glTF編）（調整中）
+http://jsdo.it/cx20/sY2n6
+■ [WebGL] X3DOM で PBR を試してみるテスト（glTF編）（調整中）
+http://jsdo.it/cx20/2TlY
+■ [WebGL] GLBoost で PBR を試してみるテスト（glTF編）（調整中）
+http://jsdo.it/cx20/cLWc
