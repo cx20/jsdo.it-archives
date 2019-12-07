56cf5d246d914a16f5ee3088eee7202bb06917a2
cx20 <y-shimano@hondarer-soft.com>
2019-12-07 19:12:19 +0900

    [WebGL] PlayCanvas で glTF 2.0 モデルを表示してみるテスト（その５）（調整中）

--- cx20/MNU4/index.js
+++ cx20/MNU4/index.js
@@ -9,13 +9,18 @@
 // forked from cx20's "[WebGL] PlayCanvas Engine を試してみるテスト" http://jsdo.it/cx20/enuS
 // forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
 
+// create a PlayCanvas application
 var canvas = document.getElementById('application');
-document.body.appendChild(canvas);
-var app = new pc.Application(canvas, {});
+var app = new pc.Application(canvas, {
+    mouse: new pc.Mouse(document.body),
+    keyboard: new pc.Keyboard(window)
+});
 app.start();
 // fill the available space at full resolution
 app.setCanvasFillMode(pc.FILLMODE_FILL_WINDOW);
 app.setCanvasResolution(pc.RESOLUTION_AUTO);
+app.scene.gammaCorrection = pc.GAMMA_SRGB;
+app.scene.toneMapping = pc.TONEMAP_ACES;
 // ensure canvas is resized when window changes size
 window.addEventListener('resize', function() {
     app.resizeCanvas();
@@ -23,9 +28,30 @@ window.addEventListener('resize', function() {
 // create camera entity
 var camera = new pc.Entity('camera');
 camera.addComponent('camera');
+camera.addComponent('script');
 app.root.addChild(camera);
-camera.setLocalPosition(0, 0, 100);
+camera.setLocalPosition(0.0, 0, 1);
 
+// make the camera interactive
+app.assets.loadFromUrl('https://rawcdn.githack.com/cx20/gltf-test/aeae18583e4d3695744f98661314fd0249c82736/libs/playcanvas/v1.25.0-dev/orbit-camera.js', 'script', function (err, asset) {
+    camera.script.create('orbitCamera', {
+        attributes: {
+            inertiaFactor: 0,
+            distanceMin: 0,
+            distanceMax: 0,
+            pitchAngleMax: 90,
+            pitchAngleMin: -90,
+            frameOnStart: true
+        }
+    });
+    camera.script.create('keyboardInput');
+    camera.script.create('mouseInput', {
+        attributes: {
+            orbitSensitivity: 0.3,
+            distanceSensitivity: 0.15
+        }
+    });
+});
 // set a prefiltered cubemap as the skybox
 var cubemapAsset = new pc.Asset('helipad', 'cubemap', {
     url: "https://cdn.rawgit.com/playcanvas/playcanvas-gltf/5489ff62/viewer/cubemap/6079289/Helipad.dds"
@@ -52,32 +78,67 @@ cubemapAsset.ready(function () {
     app.scene.setSkybox(cubemapAsset.resources);
 });
 
-// create directional light entity
-var light = new pc.Entity('light');
-light.addComponent('light');
-light.setEulerAngles(45, 0, 0);
-app.root.addChild(light);
-// rotator script
-var Rotate = pc.createScript('rotate');
-Rotate.prototype.update = function (deltaTime) {
-    this.entity.rotate(0, deltaTime * 20, 0);
-};
-// glTF scene root that rotates
-var gltfRoot = new pc.Entity();
-gltfRoot.addComponent('script');
-gltfRoot.script.create('rotate');
-var url = "https://cdn.rawgit.com/cx20/jsdo-static-contents/94bb7090/models/gltf/2.0/VoxelCorgi/glTF_merge/VoxelCorgi.gltf";
-var basePath = url.substring(0, url.lastIndexOf("/")) + "/";
-
+// root entity for loaded gltf scenes which can have more than one root entity
+var gltfRoot = new pc.Entity('gltf');
 app.root.addChild(gltfRoot);
-app.assets.loadFromUrl(url, 'json', function (err, asset) {
-    var json = asset.resource;
-    var gltf = JSON.parse(json);
-    loadGltf(gltf, app.graphicsDevice, function (err, res) {
-        // add the loaded scene to the hierarchy
-        gltfRoot.addComponent('model');
-        gltfRoot.model.model = res.model;
-    }, {
-        basePath: basePath
-    });
-});
+ 
+function init(){
+    var url = "https://cdn.rawgit.com/cx20/jsdo-static-contents/94bb7090/models/gltf/2.0/VoxelCorgi/glTF_merge/VoxelCorgi.gltf";
+
+    var scale = 1;
+    var basePath = url.substring(0, url.lastIndexOf("/")) + "/";
+    var ext = url.split(".").pop();
+    var isGlb = (ext == "glb") ? true : false;
+
+    // create directional light entity
+    var light = new pc.Entity('light');
+    light.addComponent('light',);
+    app.root.addChild(light);
+    light.setEulerAngles(45, 0, 45);
+ 
+    // rotator script
+    var Rotate = pc.createScript('rotate');
+    Rotate.prototype.update = function (deltaTime) {
+        //this.entity.rotate(0, -deltaTime * 20, 0);
+    };
+    // glTF scene root that rotates
+    var gltfRoot = new pc.Entity();
+    gltfRoot.addComponent('script');
+    gltfRoot.script.create('rotate');
+    app.root.addChild(gltfRoot);
+
+    if ( isGlb ) {
+        var req = new XMLHttpRequest();
+        req.open("get", url, true);
+        req.responseType = isGlb ? "arraybuffer" : "";
+        req.send(null);
+
+        req.onload = function(){
+            var arrayBuffer = req.response;
+            loadGlb(arrayBuffer, app.graphicsDevice, function (model) {
+                // add the loaded scene to the hierarchy
+                gltfRoot.addComponent('model');
+                gltfRoot.model.model = model;
+
+                // focus the camera on the newly loaded scene
+                camera.script.orbitCamera.focusEntity = gltfRoot;
+            });
+        }
+    } else {
+        app.assets.loadFromUrl(url, 'json', function (err, asset) {
+            var json = asset.resource;
+            var gltf = JSON.parse(json);
+            loadGltf(gltf, app.graphicsDevice, function (err, res) {
+                // add the loaded scene to the hierarchy
+                gltfRoot.addComponent('model');
+                gltfRoot.model.model = res.model;
+                // focus the camera on the newly loaded scene
+                camera.script.orbitCamera.focusEntity = gltfRoot;
+            }, {
+                basePath: basePath
+            });
+        });
+    }
+}
+
+init();
