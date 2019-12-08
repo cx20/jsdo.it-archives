0d8792c26cdc97850819b1187820936a40f56e0c
cx20 <y-shimano@hondarer-soft.com>
2019-12-09 00:01:36 +0900

    Three.js + Cannon.js でドミノっぽくドット絵を作るテスト

--- cx20/qM1k/index.js
+++ cx20/qM1k/index.js
@@ -21,7 +21,7 @@
 // ‥‥■■■〓〓〓〓〓〓〓〓〓■■
 // ‥■■■〓〓〓〓〓〓〓‥‥‥‥‥
 // ‥■‥‥〓〓〓〓‥‥‥‥‥‥‥‥
-var dataSet = [
+let dataSet = [
     "無","無","無","無","無","無","無","無","無","無","無","無","無","肌","肌","肌",
     "無","無","無","無","無","無","赤","赤","赤","赤","赤","無","無","肌","肌","肌",
     "無","無","無","無","無","赤","赤","赤","赤","赤","赤","赤","赤","赤","肌","肌",
@@ -42,7 +42,7 @@ var dataSet = [
 
 function getRgbColor( c )
 {
-    var colorHash = {
+    let colorHash = {
         "無":0xDCAA6B,    // 段ボール色
         "白":0xffffff,
         "肌":0xffcccc,
@@ -57,36 +57,81 @@ function getRgbColor( c )
     return colorHash[ c ];
 }
 
-//var TIME_STEP = 1 / 60;
-var TIME_STEP = 1 / 30;
-var SCREEN_WIDTH = 465;
-var SCREEN_HEIGHT = 465;
-var VIEW_ANGLE = 60;
-var N = 256;
-var world, camera, scene, renderer, rendererElement;
-//var stats;
-    
+let TIME_STEP = 1 / 30;
+let N = 256;
+let world, camera, scene, renderer, rendererElement;
+let controls;
 
-var requestAnimationFrame = window.requestAnimationFrame  ||
-                       window.webkitRequestAnimationFrame ||
-                       window.mozRequestAnimationFrame    ||
-                       window.msRequestAnimationFrame     ||
-                       function(c) {
-                           window.setTimeout(c, 15);
-                       };
+//////////////////////////////////////////////
+//./cannon.js-0.4.3/src/math/Vec3.js
+//////////////////////////////////////////////
+/**
+ * @method tangents
+ * @memberof CANNON.Vec3
+ * @brief Compute two artificial tangents to the vector
+ * @param CANNON.Vec3 t1 Vector object to save the first tangent in
+ * @param CANNON.Vec3 t2 Vector object to save the second tangent in
+ */
+/*
+CANNON.Vec3.prototype.tangents = function(t1,t2){
+  var norm = this.norm();
+  if(norm>0.0){
+    var n = new CANNON.Vec3(this.x/norm,
+                this.y/norm,
+                this.z/norm);
+    if(n.x<0.9){
+      var rand = Math.random();
+      n.cross(new CANNON.Vec3(rand,0.0000001,0).unit(),t1);
+    } else
+      n.cross(new CANNON.Vec3(0.0000001,rand,0).unit(),t1);
+    n.cross(t1,t2);
+  } else {
+    // The normal length is zero, make something up
+    t1.set(1,0,0).normalize();
+    t2.set(0,1,0).normalize();
+  }
+};
+*/
+
+
+//////////////////////////////////////////////
+//./cannon.js-0.6.2/src/math/Vec3.js + modified
+//////////////////////////////////////////////
+/**
+ * @method tangents
+ * @memberof CANNON.Vec3
+ * @brief Compute two artificial tangents to the vector
+ * @param CANNON.Vec3 t1 Vector object to save the first tangent in
+ * @param CANNON.Vec3 t2 Vector object to save the second tangent in
+ */
+var Vec3_tangents_n = new CANNON.Vec3();
+var Vec3_tangents_randVec = new CANNON.Vec3();
+CANNON.Vec3.prototype.tangents = function(t1,t2){
+    var norm = this.norm();
+    if(norm>0.0){
+        var n = Vec3_tangents_n;
+        var inorm = 1/norm;
+        n.set(this.x*inorm,this.y*inorm,this.z*inorm);
+        var randVec = Vec3_tangents_randVec;
+        if(Math.abs(n.x) < 0.9){
+            randVec.set(1,0,0);
+            n.cross(randVec,t1);
+        } else {
+            //randVec.set(0,1,0);
+            randVec.set(0,0,0); // 1→0 に変更
+            n.cross(randVec,t1);
+        }
+        n.cross(t1,t2);
+    } else {
+        // The normal length is zero, make something up
+        t1.set(1, 0, 0);
+        t2.set(0, 1, 0);
+    }
+};
 
 function init() {
     // Stats
-/*
-    stats = new Stats();
-
-    // 左上に設定
-    stats.getDomElement().style.position = "fixed";
-    stats.getDomElement().style.left = "0px";
-    stats.getDomElement().style.top = "0px";
-    document.body.appendChild(stats.getDomElement());
-*/
-    var parentElement = document.body;
+    let parentElement = document.body;
 
     // initialize cannon.js's world
     world = new CANNON.World();
@@ -95,41 +140,30 @@ function init() {
     world.solver.iterations = 10;
 
     // initialize three.js's scene, camera and renderer
-    if (isWebgl()) {
         renderer = new THREE.WebGLRenderer();
-    } else {
-        renderer = new THREE.CanvasRenderer();
-    }
-    renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+    renderer.setSize(window.innerWidth, window.innerHeight);
     parentElement.appendChild(renderer.domElement);
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(VIEW_ANGLE, SCREEN_WIDTH / SCREEN_HEIGHT, 0.1, 1000);
-    camera.position.x = 4;
-    camera.position.y = 10;
-    camera.position.z = 20;
-    camera.lookAt(new THREE.Vector3(0, 0, 0));
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.x = 8;
+    camera.position.y = 20;
+    camera.position.z = 50;
+    camera.lookAt(new THREE.Vector3(0, 10, 0));
 
     initLights();
     initGround();
 
-    //createShapes();
     createDominos();
-    
     createShapes();
-}
 
+    controls = new THREE.OrbitControls(camera);
+    controls.autoRotate = true;
 
-function isWebgl() {
-    try {
-        return !!window.WebGLRenderingContext && !! document.createElement('canvas').getContext('experimental-webgl');
-    } catch (e) {
-        return false;
-    }
 }
 
 // initialize lights
 function initLights() {
-    var directionalLight, ambientLight;
+    let directionalLight, ambientLight;
     directionalLight = new THREE.DirectionalLight(0xffffff, 1);
     directionalLight.position.set(0.4, 1, 0.3);
     scene.add(directionalLight);
@@ -139,37 +173,41 @@ function initLights() {
 
 // ground
 function initGround() {
-    var groundShape = new CANNON.Plane(new CANNON.Vec3(0, 1, 0));
-    var groundBody = new CANNON.RigidBody(0, groundShape);
+    let groundShape = new CANNON.Plane(new CANNON.Vec3(0, 1, 0));
+    let groundBody = new CANNON.Body({mass: 0});
+    groundBody.addShape(groundShape);
+    groundBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2);
     world.add(groundBody);
 
     // initialize Object3D
-    var plane = createPlane(100, 100);
+    let plane = createPlane(100, 100);
     plane.rotation.x = -Math.PI / 2;
     plane.position.y = 0;
     scene.add(plane);
 }
 
 function createPlane(w, h) {
-    //var texture = THREE.ImageUtils.loadTexture("grass.jpg"); // grass.jpg
-    var texture = THREE.ImageUtils.loadTexture("../../assets/u/y/G/y/uyGy9.jpg"); // grass.jpg
+    let loader = new THREE.TextureLoader();
+    let texture = loader.load('../../assets/u/y/G/y/uyGy9.jpg'); // grass.jpg
     texture.wrapS   = texture.wrapT = THREE.RepeatWrapping;
-    texture.repeat.set( 10, 10 );  
-    var material = new THREE.MeshLambertMaterial( { color: 0x777777, map: texture } );
-    var geometry = new THREE.PlaneGeometry( w, h );
-    var mesh = new THREE.Mesh(geometry, material);
+    texture.repeat.set( 5, 5 );  
+    let material = new THREE.MeshLambertMaterial( { color: 0x777777, map: texture } );
+    let geometry = new THREE.PlaneGeometry( w, h );
+    let mesh = new THREE.Mesh(geometry, material);
 
     return mesh;
 }
 
 // create a shape
 function createShape(x, y, z, w, h, d, mass, color) {
-    var geometry, material, mesh, shape, body;
+    let geometry, material, mesh, shape, body;
 
     // initialize rigid body
     //shape = new CANNON.Box(new CANNON.Vec3(w, h, d));
     shape = new CANNON.Sphere(w);
-    body = new CANNON.RigidBody(mass, shape);
+    body = new CANNON.Body({mass: mass});
+    body.addShape(shape);
+
     //body.angularVelocity.set(0, 10, 0);
     body.position.x = x + Math.random()/10;
     body.position.y = y + Math.random()/10;
@@ -178,92 +216,100 @@ function createShape(x, y, z, w, h, d, mass, color) {
     world.add(body);
 
     // initialize Object3D
-    //var texture = THREE.ImageUtils.loadTexture('Football.png')  // Football.png
-    var texture = THREE.ImageUtils.loadTexture('../../assets/s/s/X/x/ssXxc.png')  // Football.png
+    let loader = new THREE.TextureLoader();
+    let texture = loader.load('../../assets/s/s/X/x/ssXxc.png');  // Football.png
     geometry = new THREE.SphereGeometry(w, 10, 10);
     material = new THREE.MeshLambertMaterial({
         color: Math.round(color),
         map: texture
     });
     mesh = new THREE.Mesh(geometry, material);
-    mesh.useQuaternion = true;
-    mesh.rigidBody = body; // THREE.Object3D#rigidBody has a field of CANNON.RigidBody
+    mesh.rigidBody = body;
     scene.add(mesh);
 }
 
+// create a shape
 function createDomino(x, y, z, w, h, d, mass, color) {
-    var geometry, material, mesh, shape, body;
+    let geometry, material, mesh, shape, body;
 
     // initialize rigid body
     shape = new CANNON.Box(new CANNON.Vec3(w/2, h/2, d/2));
     //shape = new CANNON.Sphere(w);
-    body = new CANNON.RigidBody(mass, shape);
-    //body.angularVelocity.set(0, 10, 0);
+    body = new CANNON.Body({mass: mass});
+    body.addShape(shape);
     body.position.x = x;
     body.position.y = y;
     body.position.z = z;
-    //body.quaternion.set(Math.random()/50, Math.random()/50, Math.random()/50, 0.2);
     world.add(body);
 
     // initialize Object3D
-    var texture = THREE.ImageUtils.loadTexture('../../assets/s/s/X/x/ssXxc.png')  // Football.png
-    //geometry = new THREE.SphereGeometry(w, 10, 10);
     geometry = new THREE.CubeGeometry(w, h, d);
     material = new THREE.MeshLambertMaterial({
         color: Math.round(color)
     });
     mesh = new THREE.Mesh(geometry, material);
-    mesh.useQuaternion = true;
-    mesh.rigidBody = body; // THREE.Object3D#rigidBody has a field of CANNON.RigidBody
+    mesh.rigidBody = body;
     scene.add(mesh);
 }
 
+
+// sphere
 function createDominos() {
-    var box_size = 1;
-    for ( var y = 0; y < 16; y++ ) {
-        for ( var x = 0; x < 16; x++ ) {
-            var x1 = -5 + x * box_size * 0.95;
-            var y1 = box_size * 2;
-            var z1 = -5 + y * box_size * 1.2;
-            var color = getRgbColor( dataSet[y * 16 + x] );
-            createDomino(x1, y1, z1, 0.2, 1, 1, 1, color);
+    let box_size = 2;
+    let w = box_size * 0.15;
+    let h = box_size * 1.5;
+    let d = box_size * 1.0;
+    let mass = 1;
+    for ( let y = 0; y < 16; y++ ) {
+        for ( let x = 0; x < 16; x++ ) {
+            let x1 = -8 * box_size + x * box_size * 1.0;
+            let y1 = box_size;
+            let z1 = -8 * box_size + y * box_size * 1.2;
+            let color = getRgbColor( dataSet[y * 16 + x] );
+            createDomino(x1, y1, z1, w, h, d, mass, color);
         }
     }
 }
 
 function createShapes() {
-    var box_size = 1;
-    for ( var y = 0; y < 16; y++ ) {
-        var x1 = -5.5;
-        var y1 = 3;
-        var z1 = -5 + (15 - y) * box_size * 1.2;
-        var color = getRgbColor("白");
-        createShape(x1, y1, z1, box_size/2, box_size/2, box_size/2, 1, color);
+    let box_size = 2;
+    let w = box_size / 2;
+    let h = box_size / 2;
+    let d = box_size / 2;
+    let mass = 1;
+    for ( let y = 0; y < 16; y++ ) {
+        let x1 = -8 * box_size - 0.5;
+        let y1 = 8;
+        let z1 = -8 * box_size + (15 - y) * box_size * 1.2;
+        let color = getRgbColor("白");
+        createShape(x1, y1, z1, w, h, d, mass, color);
     }
 }
 
 function animate() {
+    controls.update();
     // step physical simulation
     world.step(TIME_STEP);
 
     // position graphical object on physical object recursively
-    (function updateObject3D(obj3d) {
-        if (obj3d.rigidBody) {
-            obj3d.rigidBody.position.copy(obj3d.position);
-            obj3d.rigidBody.quaternion.copy(obj3d.quaternion);
+    (function updateObject3D(mesh) {
+        if (mesh.rigidBody) {
+            mesh.position.x = mesh.rigidBody.position.x;
+            mesh.position.y = mesh.rigidBody.position.y;
+            mesh.position.z = mesh.rigidBody.position.z;
+            mesh.quaternion.x = mesh.rigidBody.quaternion.x;
+            mesh.quaternion.y = mesh.rigidBody.quaternion.y;
+            mesh.quaternion.z = mesh.rigidBody.quaternion.z;
+            mesh.quaternion.w = mesh.rigidBody.quaternion.w;
         }
-        if (obj3d.children) {
-            obj3d.children.map(updateObject3D);
+        if (mesh.children) {
+            mesh.children.map(updateObject3D);
         }
     })(scene);
 
     // render graphical object
     renderer.render(scene, camera);
 
-/*
-    // stats 更新
-    stats.update();
-*/
     // request next frame
     requestAnimationFrame(animate);
 }
