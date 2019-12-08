ee3e4ea7536e87a6b68d7284936bb6ceb58e442d
cx20 <y-shimano@hondarer-soft.com>
2019-12-09 00:25:45 +0900

    Three.js + Cannon.js でサッカーボールを落下させてみるテスト

--- cx20/59Ij/index.js
+++ cx20/59Ij/index.js
@@ -2,7 +2,9 @@
 // forked from cx20's "Stats.js で cannon.js の FPS を計測してみるテスト" http://jsdo.it/cx20/nEVns
 // forked from Kon's "俺のキャノン砲を試してみるかい？" http://jsdo.it/Kon/1ksj
 
-"use strict";
+let loader;
+let texture_grass;
+let texture_football;
 
 // ‥‥‥‥‥‥‥‥‥‥‥‥‥□□□
 // ‥‥‥‥‥‥〓〓〓〓〓‥‥□□□
@@ -20,7 +22,7 @@
 // ‥‥■■■〓〓〓〓〓〓〓〓〓■■
 // ‥■■■〓〓〓〓〓〓〓‥‥‥‥‥
 // ‥■‥‥〓〓〓〓‥‥‥‥‥‥‥‥
-var dataSet = [
+let dataSet = [
     "無","無","無","無","無","無","無","無","無","無","無","無","無","肌","肌","肌",
     "無","無","無","無","無","無","赤","赤","赤","赤","赤","無","無","肌","肌","肌",
     "無","無","無","無","無","赤","赤","赤","赤","赤","赤","赤","赤","赤","肌","肌",
@@ -41,8 +43,8 @@ var dataSet = [
 
 function getRgbColor( c )
 {
-    var colorHash = {
-        "無":0xDCAA6B,    // 段ボール色
+    let colorHash = {
+        "無":0xDCAA6B,
         "白":0xffffff,
         "肌":0xffcccc,
         "茶":0x800000,
@@ -56,67 +58,45 @@ function getRgbColor( c )
     return colorHash[ c ];
 }
 
-var TIME_STEP = 1 / 30;
-var SCREEN_WIDTH = 465;
-var SCREEN_HEIGHT = 465;
-var VIEW_ANGLE = 60;
-var N = 256;
-var world, camera, scene, renderer, rendererElement;
-//var stats;
-    
+let TIME_STEP = 1 / 30;
+let N = 256;
+let world, camera, scene, renderer, rendererElement;
+let controls;
 
 function init() {
-    // Stats
-/*
-    stats = new Stats();
+    loader = new THREE.TextureLoader();
+    texture_grass = loader.load('../../assets/u/y/G/y/uyGy9.jpg'); // grass.jpg
+    texture_football = loader.load('../../assets/s/s/X/x/ssXxc.png'); // Football.png
 
-    // 左上に設定
-    stats.getDomElement().style.position = "fixed";
-    stats.getDomElement().style.left = "0px";
-    stats.getDomElement().style.top = "0px";
-    document.body.appendChild(stats.getDomElement());
-*/
-    var parentElement = document.body;
+    let parentElement = document.body;
 
-    // initialize cannon.js's world
     world = new CANNON.World();
     world.gravity.set(0, -10, 0);
     world.broadphase = new CANNON.NaiveBroadphase();
     world.solver.iterations = 10;
 
-    // initialize three.js's scene, camera and renderer
-    if (isWebgl()) {
-        renderer = new THREE.WebGLRenderer();
-    } else {
-        renderer = new THREE.CanvasRenderer();
-    }
-    renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
     parentElement.appendChild(renderer.domElement);
+
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(VIEW_ANGLE, SCREEN_WIDTH / SCREEN_HEIGHT, 0.1, 1000);
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.x = 8;
     camera.position.y = 20;
-    camera.position.z = 25;
+    camera.position.z = 50;
     camera.lookAt(new THREE.Vector3(0, 10, 0));
 
     initLights();
     initGround();
 
-    createShapes();
+    createBalls();
+
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.autoRotate = true;
 }
 
-
-function isWebgl() {
-    try {
-        return !!window.WebGLRenderingContext && !! document.createElement('canvas').getContext('experimental-webgl');
-    } catch (e) {
-        return false;
-    }
-}
-
-// initialize lights
 function initLights() {
-    var directionalLight, ambientLight;
+    let directionalLight, ambientLight;
     directionalLight = new THREE.DirectionalLight(0xffffff, 1);
     directionalLight.position.set(0.4, 1, 0.3);
     scene.add(directionalLight);
@@ -124,96 +104,91 @@ function initLights() {
     scene.add(ambientLight);
 }
 
-// ground
 function initGround() {
-    var groundShape = new CANNON.Plane(new CANNON.Vec3(0, 1, 0));
-    var groundBody = new CANNON.RigidBody(0, groundShape);
+    let groundShape = new CANNON.Box(new CANNON.Vec3(50/2, 1/2, 50/2));
+    let groundBody = new CANNON.Body({mass: 0});
+    groundBody.addShape(groundShape);
     world.add(groundBody);
 
-    // initialize Object3D
-    var plane = createPlane(100, 100);
-    plane.rotation.x = -Math.PI / 2;
-    plane.position.y = 0;
-    scene.add(plane);
+    let box = createBox(50, 1, 50);
+    scene.add(box);
 }
 
-function createPlane(w, h) {
-    var texture = THREE.ImageUtils.loadTexture("../../assets/u/y/G/y/uyGy9.jpg"); // grass.jpg
-    texture.wrapS   = texture.wrapT = THREE.RepeatWrapping;
-    texture.repeat.set( 10, 10 );  
-    var material = new THREE.MeshLambertMaterial( { color: 0x777777, map: texture } );
-    var geometry = new THREE.PlaneGeometry( w, h );
-    var mesh = new THREE.Mesh(geometry, material);
-
+function createBox(w, h, d) {
+    let material = new THREE.MeshBasicMaterial( { map: texture_grass } );
+    let geometry = new THREE.CubeGeometry( w, h, d );
+    let mesh = new THREE.Mesh(geometry, material);
     return mesh;
 }
 
-// create a shape
-function createShape(x, y, z, w, h, d, mass, color) {
-    var geometry, material, mesh, shape, body;
+function createBall(x, y, z, w, h, d, mass, color) {
+    let geometry, material, mesh, shape, body;
 
-    // initialize rigid body
-    //shape = new CANNON.Box(new CANNON.Vec3(w, h, d));
-    shape = new CANNON.Sphere(w);
-    body = new CANNON.RigidBody(mass, shape);
-    //body.angularVelocity.set(0, 10, 0);
-    body.position.x = x + Math.random()/10;
-    body.position.y = y + Math.random()/10;
-    body.position.z = z + Math.random()/10;
-    body.quaternion.set(Math.random()/50, Math.random()/50, Math.random()/50, 0.2);
+    shape = new CANNON.Sphere(w/2);
+    body = new CANNON.Body({mass: mass});
+    body.addShape(shape);
+    body.position.x = x;
+    body.position.y = y;
+    body.position.z = z;
     world.add(body);
 
-    // initialize Object3D
-    var texture = THREE.ImageUtils.loadTexture('../../assets/s/s/X/x/ssXxc.png')  // Football.png
-    geometry = new THREE.SphereGeometry(w, 10, 10);
+    geometry = new THREE.SphereGeometry(w/2, 36, 36);
     material = new THREE.MeshLambertMaterial({
         color: Math.round(color),
-        map: texture
+        map: texture_football
     });
     mesh = new THREE.Mesh(geometry, material);
-    mesh.useQuaternion = true;
-    mesh.rigidBody = body; // THREE.Object3D#rigidBody has a field of CANNON.RigidBody
+    mesh.rigidBody = body;
     scene.add(mesh);
 }
 
-
-// sphere
-function createShapes() {
-    var box_size = 0.7;
-    for ( var y = 0; y < 16; y++ ) {
-        for ( var x = 0; x < 16; x++ ) {
-            var x1 = -15 + x * box_size * 2.5;
-            var y1 = (15 - y) * box_size * 3.0;
-            var z1 = 0;
-            var color = getRgbColor( dataSet[y * 16 + x] );
-            createShape(x1, y1, z1, box_size, box_size, box_size, 1, color);
+function createBalls() {
+    const BALL_SIZE = 1;
+    for (let x = 0; x < 16; x++) {
+        for (let y = 0; y < 16; y++) {
+            i = x + (15 - y) * 16;
+            let z = 0;
+            let x1 = -10 + x * BALL_SIZE * 1.5 + Math.random() * 0.1;
+            let y1 = 0 + (15 - y) * BALL_SIZE * 1.2 + Math.random() * 0.1;
+            let z1 = z * BALL_SIZE * 1 + Math.random() * 0.1;
+            let color = getRgbColor(dataSet[y * 16 + x]);
+            let w = BALL_SIZE * 1;
+            let h = BALL_SIZE * 1;
+            let d = BALL_SIZE * 1;
+            let mass = 1;
+            createBall(x1, y1, z1, w, h, d, mass, color);
         }
     }
 }
 
 function animate() {
-    // step physical simulation
+    controls.update();
     world.step(TIME_STEP);
 
-    // position graphical object on physical object recursively
-    (function updateObject3D(obj3d) {
-        if (obj3d.rigidBody) {
-            obj3d.rigidBody.position.copy(obj3d.position);
-            obj3d.rigidBody.quaternion.copy(obj3d.quaternion);
+    (function updateObject3D(mesh) {
+        if (mesh.rigidBody) {
+            mesh.position.x = mesh.rigidBody.position.x;
+            mesh.position.y = mesh.rigidBody.position.y;
+            mesh.position.z = mesh.rigidBody.position.z;
+            mesh.quaternion.x = mesh.rigidBody.quaternion.x;
+            mesh.quaternion.y = mesh.rigidBody.quaternion.y;
+            mesh.quaternion.z = mesh.rigidBody.quaternion.z;
+            mesh.quaternion.w = mesh.rigidBody.quaternion.w;
+
+            if (mesh.position.y < -10) {
+                let x = -5 + Math.random() * 10;
+                let y = 20 + Math.random() * 10;
+                let z = -5 + Math.random() * 10;
+                mesh.rigidBody.position.set(x, y, z);
+                mesh.rigidBody.velocity.set(0, 0, 0);
+            }
         }
-        if (obj3d.children) {
-            obj3d.children.map(updateObject3D);
+        if (mesh.children) {
+            mesh.children.map(updateObject3D);
         }
     })(scene);
 
-    // render graphical object
     renderer.render(scene, camera);
-
-/*
-    // stats 更新
-    stats.update();
-*/
-    // request next frame
     requestAnimationFrame(animate);
 }
 
