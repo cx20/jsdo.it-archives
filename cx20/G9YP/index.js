5b956eb9bd6172fb783fed5abc5ed3df5e278dc5
cx20 <y-shimano@hondarer-soft.com>
2019-12-15 17:07:08 +0900

    SpectorJS を試してみるテスト（Babylon.js編）

---
+++ cx20/G9YP/index.js
@@ -0,0 +1,164 @@
+// forked from cx20's "[WebGL] Babylon.js を試してみるテスト（その４）" http://jsdo.it/cx20/wNbW
+// forked from cx20's "[WebGL] Babylon.js を試してみるテスト（その３）" http://jsdo.it/cx20/o79P
+// forked from cx20's "[WebGL] Babylon.js を試してみるテスト（その２）" http://jsdo.it/cx20/mJuge
+// forked from cx20's "[WebGL] Babylon.js を試してみるテスト" http://jsdo.it/cx20/84AP
+// forked from cx20's "[簡易版] 30行で WebGL を試してみるテスト" http://jsdo.it/cx20/oaQC
+
+var spector = new SPECTOR.Spector();
+var cube;
+var createScene = function(engine) {
+    var scene = new BABYLON.Scene(engine);
+    var camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(0, 0, -3), scene);
+    cube = new BABYLON.Mesh('cube', scene);
+    scene.clearColor = new BABYLON.Color3(1, 1, 1);
+
+    // 立方体の座標データを用意
+    //             1.0 y 
+    //              ^  -1.0 
+    //              | / z
+    //              |/       x
+    // -1.0 -----------------> +1.0
+    //            / |
+    //      +1.0 /  |
+    //           -1.0
+    // 
+    //         [7]------[6]
+    //        / |      / |
+    //      [3]------[2] |
+    //       |  |     |  |
+    //       | [4]----|-[5]
+    //       |/       |/
+    //      [0]------[1]
+    //
+    var positions = [
+            // Front face
+            -0.5, -0.5,  0.5, // v0
+             0.5, -0.5,  0.5, // v1
+             0.5,  0.5,  0.5, // v2
+            -0.5,  0.5,  0.5, // v3
+            // Back face
+            -0.5, -0.5, -0.5, // v4
+             0.5, -0.5, -0.5, // v5
+             0.5,  0.5, -0.5, // v6
+            -0.5,  0.5, -0.5, // v7
+            // Top face
+             0.5,  0.5,  0.5, // v2
+            -0.5,  0.5,  0.5, // v3
+            -0.5,  0.5, -0.5, // v7
+             0.5,  0.5, -0.5, // v6
+            // Bottom face
+            -0.5, -0.5,  0.5, // v0
+             0.5, -0.5,  0.5, // v1
+             0.5, -0.5, -0.5, // v5
+            -0.5, -0.5, -0.5, // v4
+            // Right face
+             0.5, -0.5,  0.5, // v1
+             0.5,  0.5,  0.5, // v2
+             0.5,  0.5, -0.5, // v6
+             0.5, -0.5, -0.5, // v5
+            // Left face
+            -0.5, -0.5,  0.5, // v0
+            -0.5,  0.5,  0.5, // v3
+            -0.5,  0.5, -0.5, // v7
+            -0.5, -0.5, -0.5  // v4
+    ];
+    var colors = [
+            1.0, 0.0, 0.0, 1.0, // Front face
+            1.0, 0.0, 0.0, 1.0, // Front face
+            1.0, 0.0, 0.0, 1.0, // Front face
+            1.0, 0.0, 0.0, 1.0, // Front face
+            1.0, 1.0, 0.0, 1.0, // Back face
+            1.0, 1.0, 0.0, 1.0, // Back face
+            1.0, 1.0, 0.0, 1.0, // Back face
+            1.0, 1.0, 0.0, 1.0, // Back face
+            0.0, 1.0, 0.0, 1.0, // Top face
+            0.0, 1.0, 0.0, 1.0, // Top face
+            0.0, 1.0, 0.0, 1.0, // Top face
+            0.0, 1.0, 0.0, 1.0, // Top face
+            1.0, 0.5, 0.5, 1.0, // Bottom face
+            1.0, 0.5, 0.5, 1.0, // Bottom face
+            1.0, 0.5, 0.5, 1.0, // Bottom face
+            1.0, 0.5, 0.5, 1.0, // Bottom face
+            1.0, 0.0, 1.0, 1.0, // Right face
+            1.0, 0.0, 1.0, 1.0, // Right face
+            1.0, 0.0, 1.0, 1.0, // Right face
+            1.0, 0.0, 1.0, 1.0, // Right face
+            0.0, 0.0, 1.0, 1.0, // Left face
+            0.0, 0.0, 1.0, 1.0, // Left face
+            0.0, 0.0, 1.0, 1.0, // Left face
+            0.0, 0.0, 1.0, 1.0  // Left face
+    ];
+    var uv = [
+        // Front face
+        1, 0,
+        0, 0,
+        0, 1,
+        1, 1,
+        // Back face
+        1, 0,
+        0, 0,
+        0, 1,
+        1, 1,
+        // Top face
+        1, 0,
+        0, 0,
+        0, 1,
+        1, 1,
+        // Bottom face
+        1, 0,
+        0, 0,
+        0, 1,
+        1, 1,
+        // Right face
+        1, 0,
+        0, 0,
+        0, 1,
+        1, 1,
+        // Left face
+        1, 0,
+        0, 0,
+        0, 1,
+        1, 1
+    ];
+    var indices = [
+         0,  1,  2,    0,  2 , 3,  // Front face
+         4,  5,  6,    4,  6 , 7,  // Back face
+         8,  9, 10,    8, 10, 11,  // Top face
+        12, 13, 14,   12, 14, 15,  // Bottom face
+        16, 17, 18,   16, 18, 19,  // Right face
+        20, 21, 22,   20, 22, 23   // Left face
+    ];
+
+    cube.setVerticesData(BABYLON.VertexBuffer.PositionKind, positions, true);
+    cube.setVerticesData(BABYLON.VertexBuffer.UVKind, uv);
+    cube.setIndices(indices);
+
+    var material = new BABYLON.ShaderMaterial("material", scene, {
+        vertexElement: "vs",
+        fragmentElement: "fs",
+    }, {
+        attributes: ["position", "uv"],
+        uniforms: ["worldViewProjection"]
+    });
+    
+    material.setTexture("texture", new BABYLON.Texture("../../assets/A/k/w/j/AkwjW.jpg", scene)); // frog.jpg
+
+    cube.material = material;
+    cube.material.backFaceCulling = false;
+
+    return scene;
+};
+
+var canvas = document.querySelector("#c");
+var engine = new BABYLON.Engine(canvas, true);
+var scene = createScene(engine);
+spector.displayUI();
+
+var rad = 0.0;
+engine.runRenderLoop(function () {
+    rad += Math.PI * 1.0 / 180.0;
+    cube.rotation.x = rad;
+    cube.rotation.y = rad;
+    cube.rotation.z = rad;
+    scene.render();
+});
