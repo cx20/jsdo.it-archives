4a5c349204a290cc6aa5280f0bead1a3b1e6f8d3
cx20 <y-shimano@hondarer-soft.com>
2020-01-07 01:53:48 +0900

    Updated three.js version from r97 to r110

--- cx20/84wX/index.js
+++ cx20/84wX/index.js
@@ -66,8 +66,9 @@ function init() {
     let loader = new THREE.GLTFLoader();
     loader.setCrossOrigin( 'anonymous' ); // r84 以降は明示的に setCrossOrigin() を指定する必要がある
 
-    THREE.DRACOLoader.setDecoderPath( 'https://cdn.rawgit.com/cx20/gltf-test/a63d4941/libs/three.js/r95dev/draco/gltf/' );
-    loader.setDRACOLoader( new THREE.DRACOLoader() );
+    var dracoLoader = new THREE.DRACOLoader();
+    dracoLoader.setDecoderPath( 'https://rawcdn.githack.com/mrdoob/three.js/r110/examples/js/libs/draco/gltf/' );
+    loader.setDRACOLoader( dracoLoader );
     
     let scale = 20;
     //let url = "https://cdn.rawgit.com/ft-lab/ft-lab.github.io/c56ef016/gltf/grass/rocks_trees_ao.glb";
@@ -129,7 +130,8 @@ function init() {
     controls.autoRotateSpeed = -2.0;
 
     renderer.setSize( width, height );
-    renderer.gammaOutput = true;
+    //renderer.gammaOutput = true; // if >r88, models are dark unless you activate gammaOutput
+    //renderer.gammaFactor = 2.2;
     document.body.appendChild( renderer.domElement );
 }
 
