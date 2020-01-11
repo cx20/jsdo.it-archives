f03445e611a6d577cbf137c5d08e06724e243ccc
cx20 <y-shimano@hondarer-soft.com>
2020-01-11 17:53:28 +0900

    Changed from rawgit to githack

--- cx20/mtpa/index.js
+++ cx20/mtpa/index.js
@@ -46,7 +46,7 @@ app.root.addChild(camera);
 camera.setLocalPosition(0.0, 0, 1);
 
 // make the camera interactive
-app.assets.loadFromUrl('https://cdn.rawgit.com/cx20/gltf-test/6f94897a/libs/playcanvas/v1.9.0-dev/orbit-camera.js', 'script', function (err, asset) {
+app.assets.loadFromUrl('https://rawcdn.githack.com/cx20/gltf-test/08f35fd423b432a87b22679bdda11365b5d1ac22/libs/playcanvas/v1.9.0-dev/orbit-camera.js', 'script', function (err, asset) {
     camera.script.create('orbitCamera', {
         attributes: {
             inertiaFactor: 0,
