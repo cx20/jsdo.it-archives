a8db184276357e1ceaf306bad4304892d61b6c6d
cx20 <y-shimano@hondarer-soft.com>
2020-03-19 22:57:34 +0900

    Adjust SVG size to Window width

--- cx20/6n9Y/index.js
+++ cx20/6n9Y/index.js
@@ -1,7 +1,8 @@
 // forked from cx20's "Snap.svg でドット絵を描いてみるテスト" http://jsdo.it/cx20/kCUy
 
-var X_MAX = 465;
-var Y_MAX = 465;
+var X_MAX = window.innerWidth;
+var Y_MAX = window.innerHeight;
+var SCALE = Y_MAX / 465;
 var s = Snap(X_MAX, Y_MAX);
 
 var thetas = [
@@ -44,25 +45,25 @@ function drawMatrix() {
     s.rect(0, 0, X_MAX, Y_MAX).attr({fill: "#fff"});
     var x1, y1, x2, y2, title;
     for ( var col = 0; col <= cols; col++ ) {
-        x1 = col * 40 + 60;
-        y1 = 60;
-        x2 = col * 40 + 60;
-        y2 = rows * 40 + 60;
+        x1 = (col * 40 + 60) * SCALE;
+        y1 = (60) * SCALE;
+        x2 = (col * 40 + 60) * SCALE;
+        y2 = (rows * 40 + 60) * SCALE;
         s.line(x1, y1, x2, y2).attr({stroke: "#369", strokeWidth: 1, fill: "none"});
         if ( col < cols ) {
             title = thetas[col].title;
-            s.text(x2 + 5, 55, title ).attr({stroke: "#369", "font-size":12});
+            s.text(x2 + 5 * SCALE, 55 * SCALE, title ).attr({stroke: "#369", "font-size":12 * SCALE});
         }
     }
     for ( var row = 0; row <= rows; row++ ) {
-        x1 = 60;
-        y1 = row * 40 + 60;
-        x2 = cols * 40 + 60;
-        y2 = row * 40 + 60;
+        x1 = (60) * SCALE;
+        y1 = (row * 40 + 60) * SCALE;
+        x2 = (cols * 40 + 60) * SCALE;
+        y2 = (row * 40 + 60) * SCALE;
         s.line(x1, y1, x2, y2).attr({stroke: "#369", strokeWidth: 1, fill: "none"});
         if ( row < rows ) {
             title = ratios[row].title;
-            s.text(35, y2 + 25, title ).attr({stroke: "#369", "font-size":12});
+            s.text(35 * SCALE, y2 + 25 * SCALE, title ).attr({stroke: "#369", "font-size":12 * SCALE});
         }
     }
 }
@@ -74,11 +75,11 @@ function draw(angle) {
     for ( var row = 0; row < rows; row++ ) {
         for ( var col = 0; col < cols; col++ ) {
             var theta = thetas[col].theta + angle; 
-            var base_x = col * 40 + 80;
-            var base_y = row * 40 + 80;
+            var base_x = (col * 40 + 80) * SCALE;
+            var base_y = (row * 40 + 80) * SCALE;
             var a = ratios[row].a;
             var b = ratios[row].b;
-            var scale = 15;
+            var scale = 15 * SCALE;
             var times = ratios[row].times;
             drawLissajous(base_x, base_y, theta, a, b, scale, times);
         }
