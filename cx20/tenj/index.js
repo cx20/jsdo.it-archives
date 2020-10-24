f2b8b92a438f4538e3d651496ebe58a4ba720827
cx20 <y-shimano@hondarer-soft.com>
2020-10-24 13:57:33 +0900

    Added Pluto sample of Three.js

---
+++ cx20/tenj/index.js
@@ -0,0 +1,50 @@
+// forked from cx20's "Three.js で地球を回してみるテスト" http://jsdo.it/cx20/tv0T
+var container;
+var camera;
+var scene;
+var geometry;
+var material;
+var mesh;
+var renderer;
+var light;
+var light2;
+
+init();
+animate();
+
+function init() {
+    container = document.getElementById('container');
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+    camera.position.z = 400;
+
+    scene = new THREE.Scene();
+    light = new THREE.PointLight( new THREE.Color(0x808080), 1.5);
+    light2 = new THREE.PointLight( new THREE.Color(0x101010), 1);
+    light.position.set(0, -100, 1000);
+    light2.position.set(50, 50, 1000);
+    scene.add(light);
+    scene.add(light2);
+    geometry = new THREE.SphereGeometry(140, 30, 30);
+    material = new THREE.MeshPhongMaterial({
+        map: THREE.ImageUtils.loadTexture('./pluto.jpg'),
+        bumpMap: THREE.ImageUtils.loadTexture('./pluto-bump.jpg'),
+        bumpScale: 0.2
+    });
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.overdraw = true;
+    scene.add(mesh);
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    container.appendChild(renderer.domElement);
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    render();
+}
+
+function render() {
+    mesh.rotation.y += 0.005;
+    renderer.render(scene, camera);
+}
---
+++ cx20/tenj/readme.txt
@@ -0,0 +1,21 @@
+﻿Three.js で冥王星を表示させてみるテスト
+
+惑星探査機「ニューホライズンズ」の冥王星到着を記念して、冥王星を回してみました。テクスチャ画像は2009年時点の想像図になります。あしからず。
+
+＜対応した点＞
+・テクスチャを冥王星に変更
+・バンプマップを使用するよう対応
+・Three.js のバージョンを「r60」→「r71」に更新
+
+＜参考＞
+■ Detail | NASA 3D Resources (Beta)
+http://nasa3d.arc.nasa.gov/detail/plu0rss1
+
+■ wgld.org | canvas2D: 法線マップの生成
+http://wgld.org/d/canvas2d/c003.html
+
+■ NASAの惑星探査機「ニューホライズンズ」が冥王星の写真撮影に成功、人類は太陽系全ての惑星への到達を完了 - GIGAZINE
+http://gigazine.net/news/20150715-pluto-new-horizons/
+
+■ 冥王星の地形が分かってきました。これってすごいこと : ギズモード・ジャパン.
+http://www.gizmodo.jp/2015/07/post_17643.html
