f0954a65b16d26517ee7368ca39220bbfd037da3
cx20 <y-shimano@hondarer-soft.com>
2021-05-29 16:07:06 +0900

    Added Three.js + Cannon.js sample

---
+++ cx20/CeNf/index.js
@@ -0,0 +1,276 @@
+// forked from cx20's "Three.js + Cannon.js でドミノっぽくドット絵を作るテスト" http://jsdo.it/cx20/qM1k
+// forked from cx20's "Three.js + Cannon.js でサッカーボールを落下させてみるテスト" http://jsdo.it/cx20/59Ij
+// forked from cx20's "Three.js + cannon.js でドット絵を落下させてみるテスト" http://jsdo.it/cx20/7Zay
+// forked from cx20's "Stats.js で cannon.js の FPS を計測してみるテスト" http://jsdo.it/cx20/nEVns
+// forked from Kon's "俺のキャノン砲を試してみるかい？" http://jsdo.it/Kon/1ksj
+
+"use strict";
+
+// ‥‥‥‥‥‥‥‥‥‥‥‥‥□□□
+// ‥‥‥‥‥‥〓〓〓〓〓‥‥□□□
+// ‥‥‥‥‥〓〓〓〓〓〓〓〓〓□□
+// ‥‥‥‥‥■■■□□■□‥■■■
+// ‥‥‥‥■□■□□□■□□■■■
+// ‥‥‥‥■□■■□□□■□□□■
+// ‥‥‥‥■■□□□□■■■■■‥
+// ‥‥‥‥‥‥□□□□□□□■‥‥
+// ‥‥■■■■■〓■■■〓■‥‥‥
+// ‥■■■■■■■〓■■■〓‥‥■
+// □□■■■■■■〓〓〓〓〓‥‥■
+// □□□‥〓〓■〓〓□〓〓□〓■■
+// ‥□‥■〓〓〓〓〓〓〓〓〓〓■■
+// ‥‥■■■〓〓〓〓〓〓〓〓〓■■
+// ‥■■■〓〓〓〓〓〓〓‥‥‥‥‥
+// ‥■‥‥〓〓〓〓‥‥‥‥‥‥‥‥
+let dataSet = [
+    "無","無","無","無","無","無","無","無","無","無","無","無","無","肌","肌","肌",
+    "無","無","無","無","無","無","赤","赤","赤","赤","赤","無","無","肌","肌","肌",
+    "無","無","無","無","無","赤","赤","赤","赤","赤","赤","赤","赤","赤","肌","肌",
+    "無","無","無","無","無","茶","茶","茶","肌","肌","茶","肌","無","赤","赤","赤",
+    "無","無","無","無","茶","肌","茶","肌","肌","肌","茶","肌","肌","赤","赤","赤",
+    "無","無","無","無","茶","肌","茶","茶","肌","肌","肌","茶","肌","肌","肌","赤",
+    "無","無","無","無","茶","茶","肌","肌","肌","肌","茶","茶","茶","茶","赤","無",
+    "無","無","無","無","無","無","肌","肌","肌","肌","肌","肌","肌","赤","無","無",
+    "無","無","赤","赤","赤","赤","赤","青","赤","赤","赤","青","赤","無","無","無",
+    "無","赤","赤","赤","赤","赤","赤","赤","青","赤","赤","赤","青","無","無","茶",
+    "肌","肌","赤","赤","赤","赤","赤","赤","青","青","青","青","青","無","無","茶",
+    "肌","肌","肌","無","青","青","赤","青","青","黄","青","青","黄","青","茶","茶",
+    "無","肌","無","茶","青","青","青","青","青","青","青","青","青","青","茶","茶",
+    "無","無","茶","茶","茶","青","青","青","青","青","青","青","青","青","茶","茶",
+    "無","茶","茶","茶","青","青","青","青","青","青","青","無","無","無","無","無",
+    "無","茶","無","無","青","青","青","青","無","無","無","無","無","無","無","無"
+];
+
+function getRgbColor( c )
+{
+    let colorHash = {
+        "無":0xDCAA6B,    // 段ボール色
+        "白":0xffffff,
+        "肌":0xffcccc,
+        "茶":0x800000,
+        "赤":0xff0000,
+        "黄":0xffff00,
+        "緑":0x00ff00,
+        "水":0x00ffff,
+        "青":0x0000ff,
+        "紫":0x800080
+    };
+    return colorHash[ c ];
+}
+
+//let TIME_STEP = 1 / 60;
+let TIME_STEP = 1 / 30;
+let SCREEN_WIDTH = 465;
+let SCREEN_HEIGHT = 465;
+let VIEW_ANGLE = 60;
+let N = 256;
+let world, camera, scene, renderer, rendererElement;
+//let stats;
+    
+
+let requestAnimationFrame = window.requestAnimationFrame  ||
+                       window.webkitRequestAnimationFrame ||
+                       window.mozRequestAnimationFrame    ||
+                       window.msRequestAnimationFrame     ||
+                       function(c) {
+                           window.setTimeout(c, 15);
+                       };
+
+function init() {
+    // Stats
+/*
+    stats = new Stats();
+
+    // 左上に設定
+    stats.getDomElement().style.position = "fixed";
+    stats.getDomElement().style.left = "0px";
+    stats.getDomElement().style.top = "0px";
+    document.body.appendChild(stats.getDomElement());
+*/
+    let parentElement = document.body;
+
+    // initialize cannon.js's world
+    world = new CANNON.World();
+    world.gravity.set(0, -10, 0);
+    world.broadphase = new CANNON.NaiveBroadphase();
+    world.solver.iterations = 10;
+
+    // initialize three.js's scene, camera and renderer
+    if (isWebgl()) {
+        renderer = new THREE.WebGLRenderer();
+    } else {
+        renderer = new THREE.CanvasRenderer();
+    }
+    renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+    parentElement.appendChild(renderer.domElement);
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(VIEW_ANGLE, SCREEN_WIDTH / SCREEN_HEIGHT, 0.1, 1000);
+    camera.position.x = 4;
+    camera.position.y = 10;
+    camera.position.z = 20;
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+    initLights();
+    initGround();
+
+    //createShapes();
+    createDominos();
+    
+    createShapes();
+}
+
+
+function isWebgl() {
+    try {
+        return !!window.WebGLRenderingContext && !! document.createElement('canvas').getContext('experimental-webgl');
+    } catch (e) {
+        return false;
+    }
+}
+
+// initialize lights
+function initLights() {
+    let directionalLight, ambientLight;
+    directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    directionalLight.position.set(0.4, 1, 0.3);
+    scene.add(directionalLight);
+    ambientLight = new THREE.AmbientLight(0x101020);
+    scene.add(ambientLight);
+}
+
+// ground
+function initGround() {
+    let groundShape = new CANNON.Plane(new CANNON.Vec3(0, 1, 0));
+    let groundBody = new CANNON.Body({mass: 0});
+    groundBody.addShape(groundShape);
+    groundBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2);
+    world.add(groundBody);
+
+    // initialize Object3D
+    let plane = createPlane(100, 100);
+    plane.rotation.x = -Math.PI / 2;
+    plane.position.y = 0;
+    scene.add(plane);
+}
+
+function createPlane(w, h) {
+    let loader = new THREE.TextureLoader();
+    let texture = loader.load('../../assets/u/y/G/y/uyGy9.jpg'); // grass.jpg
+    texture.wrapS   = texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat.set( 10, 10 );  
+    let material = new THREE.MeshLambertMaterial( { color: 0x777777, map: texture } );
+    let geometry = new THREE.PlaneGeometry( w, h );
+    let mesh = new THREE.Mesh(geometry, material);
+
+    return mesh;
+}
+
+// create a shape
+function createShape(x, y, z, w, h, d, mass, color) {
+    let geometry, material, mesh, shape, body;
+
+    // initialize rigid body
+    //shape = new CANNON.Box(new CANNON.Vec3(w, h, d));
+    shape = new CANNON.Sphere(w);
+    body = new CANNON.Body({mass: mass});
+    body.addShape(shape);
+    //body.angularVelocity.set(0, 10, 0);
+    body.position.x = x + Math.random()/10;
+    body.position.y = y + Math.random()/10;
+    body.position.z = z + Math.random()/10;
+    body.quaternion.set(Math.random()/50, Math.random()/50, Math.random()/50, 0.2);
+    world.add(body);
+
+    // initialize Object3D
+    let loader = new THREE.TextureLoader();
+    let texture = loader.load('../../assets/s/s/X/x/ssXxc.png');  // Football.png
+    geometry = new THREE.SphereGeometry(w, 10, 10);
+    material = new THREE.MeshLambertMaterial({
+        color: Math.round(color),
+        map: texture
+    });
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.useQuaternion = true;
+    mesh.rigidBody = body; // THREE.Object3D#rigidBody has a field of CANNON.RigidBody
+    scene.add(mesh);
+}
+
+function createDomino(x, y, z, w, h, d, mass, color) {
+    let geometry, material, mesh, shape, body;
+
+    // initialize rigid body
+    shape = new CANNON.Box(new CANNON.Vec3(w/2, h/2, d/2));
+    //shape = new CANNON.Sphere(w);
+    body = new CANNON.Body({mass: mass});
+    body.addShape(shape);
+    //body.angularVelocity.set(0, 10, 0);
+    body.position.x = x;
+    body.position.y = y;
+    body.position.z = z;
+    //body.quaternion.set(Math.random()/50, Math.random()/50, Math.random()/50, 0.2);
+    world.add(body);
+
+    // initialize Object3D
+    geometry = new THREE.CubeGeometry(w, h, d);
+    material = new THREE.MeshLambertMaterial({
+        color: Math.round(color)
+    });
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.useQuaternion = true;
+    mesh.rigidBody = body; // THREE.Object3D#rigidBody has a field of CANNON.RigidBody
+    scene.add(mesh);
+}
+
+function createDominos() {
+    let box_size = 1;
+    for ( let y = 0; y < 16; y++ ) {
+        for ( let x = 0; x < 16; x++ ) {
+            let x1 = -5 + x * box_size * 0.95;
+            let y1 = box_size * 2;
+            let z1 = -5 + y * box_size * 1.2;
+            let color = getRgbColor( dataSet[y * 16 + x] );
+            createDomino(x1, y1, z1, 0.2, 1, 1, 1, color);
+        }
+    }
+}
+
+function createShapes() {
+    let box_size = 1;
+    for ( let y = 0; y < 16; y++ ) {
+        let x1 = -5.5;
+        let y1 = 3;
+        let z1 = -5 + (15 - y) * box_size * 1.2;
+        let color = getRgbColor("白");
+        createShape(x1, y1, z1, box_size/2, box_size/2, box_size/2, 1, color);
+    }
+}
+
+function animate() {
+    // step physical simulation
+    world.step(TIME_STEP);
+
+    // position graphical object on physical object recursively
+    (function updateObject3D(mesh) {
+        if (mesh.rigidBody) {
+            mesh.position.x = mesh.rigidBody.position.x;
+            mesh.position.y = mesh.rigidBody.position.y;
+            mesh.position.z = mesh.rigidBody.position.z;
+            mesh.quaternion.x = mesh.rigidBody.quaternion.x;
+            mesh.quaternion.y = mesh.rigidBody.quaternion.y;
+            mesh.quaternion.z = mesh.rigidBody.quaternion.z;
+            mesh.quaternion.w = mesh.rigidBody.quaternion.w;
+        }
+        if (mesh.children) {
+            mesh.children.map(updateObject3D);
+        }
+    })(scene);
+
+    // render graphical object
+    renderer.render(scene, camera);
+
+    // request next frame
+    requestAnimationFrame(animate);
+}
+
+init();
+animate();
---
+++ cx20/CeNf/readme.txt
@@ -0,0 +1,20 @@
+﻿Three.js + Cannon.js でドミノっぽくドット絵を作るテスト（その２）（失敗）
+
+＜対応した点＞
+・各ライブラリを最新化
+　Three.js r56 → r82
+　Cannon.js 0.4.3 → 0.6.2
+
+＜対応できていない点＞
+・うまく倒れてくれない。。。
+　CANNON.Vec3.prototype.tangents() の実装が 0.4.3 → 0.5.0 に変更された際に挙動が変わっている模様。
+
+＜参考＞
+■ cannon.js
+https://github.com/schteppe/cannon.js
+
+■ Is new cannon.js weak in domino?
+https://github.com/schteppe/cannon.js/issues/310
+
+■ WebGLと3D物理演算ライブラリの組み合わせを試してみる
+http://qiita.com/cx20/items/3ebed669fb9c9e797935
