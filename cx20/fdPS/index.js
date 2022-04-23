1efda7fb50fca714ff464e0a90c329b4870d9fb2
cx20 <y-shimano@hondarer-soft.com>
2022-04-24 02:59:28 +0900

    Updated Babylon.js sample code

--- cx20/fdPS/index.js
+++ cx20/fdPS/index.js
@@ -1,43 +1,32 @@
-if (BABYLON.Engine.isSupported()) {
-    var canvas = document.getElementById("renderCanvas");
-    var engine = new BABYLON.Engine(canvas, true);
-    
-    var scene = new BABYLON.Scene(engine);
-    
-    var camera = new BABYLON.FreeCamera("Camera", new BABYLON.Vector3(0, 0, -10), scene);
-    var box = BABYLON.Mesh.CreateBox("Box", 3.0, scene);
-    
-    // Material
-    var material = new BABYLON.StandardMaterial("default", scene);
+const canvas = document.getElementById("renderCanvas");
+const engine = new BABYLON.Engine(canvas, true);
+
+const createScene = function() {
+    const scene = new BABYLON.Scene(engine);
+    const camera = new BABYLON.FreeCamera("Camera", new BABYLON.Vector3(0, 0, -10), scene);
+    const box = BABYLON.Mesh.CreateBox("Box", 3.0, scene);
+    const material = new BABYLON.StandardMaterial("default", scene);
     material.diffuseTexture = new BABYLON.Texture("../../assets/c/K/k/k/cKkkH.jpg", scene);
     material.emissiveColor = new BABYLON.Color3(1.0, 1.0, 1.0);
     material.backFaceCulling = false;
     box.material = material;
-    
-    // Render loop
-    var renderLoop = function () {
-        // Start new frame
-        engine.beginFrame();
-        
-        scene.render();
-        
-        // Present
-        engine.endFrame();
-        
-        // Register new frame
-        BABYLON.Tools.QueueNewFrame(renderLoop);
-    };
-    
-    BABYLON.Tools.QueueNewFrame(renderLoop);
-    
-    var alpha = 0;
-    box.scaling.x = 1.0;
-    box.scaling.y = 1.0;
-    box.scaling.z = 1.0;
-    scene.beforeRender = function() {
+
+    let alpha = 0;
+    engine.runRenderLoop(function () {
         box.rotation.x = alpha;
         box.rotation.y = alpha;
         
         alpha += 0.01;
-    };
-}
+
+        scene.render();
+    });
+
+    return scene;
+};
+
+const scene = createScene();
+
+window.addEventListener("resize", function () {
+        engine.resize();
+});
+
