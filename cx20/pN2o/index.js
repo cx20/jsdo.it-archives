8b990d5c896a9ee904ac97a5b8c50ff65efd1724
cx20 <y-shimano@hondarer-soft.com>
2022-05-05 01:25:49 +0900

    Update Babylon.js v2.0 to v5.4.0

--- cx20/pN2o/index.js
+++ cx20/pN2o/index.js
@@ -4,20 +4,20 @@
 // forked from cx20's "Babylon.js で立方体を表示するテスト" http://jsdo.it/cx20/fdPS
 
 // Canvas 取得
-var canvas = document.querySelector("#renderCanvas");
+const canvas = document.querySelector("#renderCanvas");
 
 // Babylon 3D Engine のロード
-var engine = new BABYLON.Engine(canvas, true);
+const engine = new BABYLON.Engine(canvas, true);
 
-var createScene = function () {
+const createScene = function () {
     // シーンオブジェクトの作成
-    var scene = new BABYLON.Scene(engine);
+    const scene = new BABYLON.Scene(engine);
     
     // 背景色
     scene.clearColor = new BABYLON.Color3(0.5, 0.7, 0.9);
 
     // カメラ
-    var camera = new BABYLON.ArcRotateCamera("Camera", 0, 1.2, 90, BABYLON.Vector3.Zero(), scene);
+    const camera = new BABYLON.ArcRotateCamera("Camera", 0, 1.2, 90, BABYLON.Vector3.Zero(), scene);
     camera.lowerBetaLimit = 0.1;
     camera.upperBetaLimit = (Math.PI / 2) * 0.9;
     camera.lowerRadiusLimit = 30;
@@ -26,33 +26,33 @@ var createScene = function () {
 
     // 平行光源
     // DirectionalLight(name, direction, scene)
-    var light = new BABYLON.DirectionalLight("dir01", new BABYLON.Vector3(-1, -2, -1), scene);
+    const light = new BABYLON.DirectionalLight("dir01", new BABYLON.Vector3(-1, -2, -1), scene);
     light.position = new BABYLON.Vector3(20, 40, 20);
     light.intensity = 1.0;  // 光の強さ
 
     // スポットライト
     // SpotLight(name, position, direction, angle, exponent, scene)
-    var light2 = new BABYLON.SpotLight("spot02", new BABYLON.Vector3(30, 40, 20), new BABYLON.Vector3(-1, -2, -1), 1.1, 1, scene);
+	const light2 = new BABYLON.SpotLight("spot02", new BABYLON.Vector3(30, 40, 20), new BABYLON.Vector3(-1, -2, -1), Math.PI / 2, 10, scene);
     light2.intensity = 1.0;  // 光の強さ
 
     // 地面
-    var ground = BABYLON.Mesh.CreateGroundFromHeightMap("ground", "../../assets/c/C/N/m/cCNmz.png", 100, 100, 100, 0, 20, scene, false); // heightMap.png
-    var material = new BABYLON.StandardMaterial("ground", scene);
-    var texture = new BABYLON.Texture("../../assets/y/x/R/f/yxRfY.png", scene); // fujimap.png
+    const ground = BABYLON.Mesh.CreateGroundFromHeightMap("ground", "../../assets/c/C/N/m/cCNmz.png", 100, 100, 100, 0, 20, scene, false); // heightMap.png
+    const material = new BABYLON.StandardMaterial("ground", scene);
+    const texture = new BABYLON.Texture("../../assets/y/x/R/f/yxRfY.png", scene); // fujimap.png
     material.diffuseTexture = texture;
     material.specularColor = new BABYLON.Color3(0, 0, 0);
     ground.material = material;
 
     // アニメーション
     scene.registerBeforeRender(function () {
-        scene.activeCamera.alpha += 0.01;
+        scene.activeCamera.alpha += 0.005 * scene.getAnimationRatio();
     });
 
     return scene;
 }
 
 // シーンの作成
-var scene = createScene();
+const scene = createScene();
 // 描画ループ
 engine.runRenderLoop(function () {
     scene.render();
