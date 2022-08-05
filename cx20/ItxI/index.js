98cfc629807e8fb2bf95d38db4d37bfa08ab16b1
cx20 <y-shimano@hondarer-soft.com>
2022-08-05 23:52:26 +0900

    Update PlayCanvas v1.54.0 to v1.55.0

--- cx20/ItxI/index.js
+++ cx20/ItxI/index.js
@@ -35,7 +35,7 @@ camera.addComponent('script');
 app.root.addChild(camera);
 camera.setLocalPosition(0, 0, 1);
 
-app.assets.loadFromUrl('https://cx20.github.io/gltf-test/libs/playcanvas/v1.54.0/orbit-camera.js', 'script', function (err, asset) {
+app.assets.loadFromUrl('https://cx20.github.io/gltf-test/libs/playcanvas/v1.55.0/orbit-camera.js', 'script', function (err, asset) {
     camera.script.create('orbitCamera');
     camera.script.create("orbitCameraInputMouse");
     camera.script.create("orbitCameraInputTouch");
