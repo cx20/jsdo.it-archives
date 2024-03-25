d1c3b5e7a9648b1e9abbddc8c8d7f32e801b2203
cx20 <y-shimano@hondarer-soft.com>
2024-03-26 07:37:39 +0900

    Update PlayCanvas v1.68.1 to v1.69.0

--- cx20/Q3iT/index.js
+++ cx20/Q3iT/index.js
@@ -52,7 +52,7 @@ camera.addComponent('script');
 app.root.addChild(camera);
 camera.setLocalPosition(1, 0.5, 1);
 
-app.assets.loadFromUrl('https://cx20.github.io/gltf-test/libs/playcanvas/v1.68.1/orbit-camera.js', 'script', function (err, asset) {
+app.assets.loadFromUrl('https://cx20.github.io/gltf-test/libs/playcanvas/v1.69.0/orbit-camera.js', 'script', function (err, asset) {
     camera.script.create('orbitCamera');
     camera.script.create("orbitCameraInputMouse");
     camera.script.create("orbitCameraInputTouch");
@@ -143,7 +143,7 @@ app.on("update", function (deltaTime) {
     }
 });
 
-const pcRoot = "https://cx20.github.io/gltf-test/libs/playcanvas/v1.68.1";
+const pcRoot = "https://cx20.github.io/gltf-test/libs/playcanvas/v1.69.0";
 
 function main(){
     pc.WasmModule.setConfig("DracoDecoderModule", {
